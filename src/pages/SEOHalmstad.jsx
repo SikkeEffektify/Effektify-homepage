12d79412194905ee1156787364226d97f8cf54cb
Mattias Sikmar <mattiassikmar@Mattiass-MacBook-Pro.local>
2023-06-22 11:08:33 +0200

    Added Sections  on Halmstad-seo

--- src/pages/SEOHalmstad.jsx
+++ src/pages/SEOHalmstad.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import bg from '../assets/SEO-sokmotoroptimering-halmstad.jpg'
+import stats from '../assets/seo-halmstad-stats.webp'
+import { Hero2 } from '../components/Hero2'
 
 export default function SEOHalmstad() {
   return (
@@ -41,8 +43,106 @@ export default function SEOHalmstad() {
     </div>
    </div>
     </section>
+
+    <section>
+    <div class='pt-20 heading-container text-center text-black bg-white'>
+    <p class="uppercase text-xs pb-5">Syns du inte så finns du inte</p>
+    <h1 class="text-lg uppercase">
+    Behöver även du hjälp med SEO i Halmstad?
+    </h1>
+    <p class="text-left pt-10 text-base pb-5">
+    SEO står för det engelska uttrycket Search Engine Optimization, vilket översatt till svenska är sökmotoroptimering. SEO består av många olika delar och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på tjänster eller produkter som du erbjuder.
+    </p>
+    <p class="text-center pb-5 text-sm">
+    Enkelt förklarat innebär 
+    <a class="text-rose-300" target="_blank" rel="noopener noreferrer" href="/seo-sokmotoroptimering/"> SEO </a>
+    att göra din webbplats synlig på Google och andra sökmotorer.
+    </p>
+    <img class="mx-auto pt-5 drop-shadow-lg" src={stats} alt="" />
+    </div>
+    </section>
+
+    <Hero2 />
     
+    <section class="py-20 heading-container">
+
+    <h1 class="text-lg text-white text-center pt-10 uppercase pb-5">För dig som vill veta mer om sökmotoroptimering</h1>
+    <p class="text-white text-center pb-5" >Vanliga frågor om SEO i Halmstad</p>
+   
+    <div id="accordion-flush" data-accordion="collapse " class='pb-10' >
+  <h2 id="accordion-flush-heading-1" class="button-no-back">
+    <button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-1" aria-expanded="false" aria-controls="accordion-flush-body-1">
+      <span class='uppercase'>Vad är lokal SEO- Halmstad?</span>
+      <i class="fa-solid fa-angles-down pr-2 text-rose-300"></i>
+    </button>
+  </h2>
+  <div id="accordion-flush-body-1" class="hidden" aria-labelledby="accordion-flush-heading-1">
+    <div class="py-5 border-b">
+      <p class="mb-2 text-sm tracking-wider  text-white">
+      Sökmotoroptimering (SEO) står för Search Engine Optimization och handlar om hur man optimerar en webbplats för att den ska rankas så högt som möjligt i sökresultatet. Om din webbplats är SEO-optimerad för Halmstad kommer fler lokala kunder att ha större chans att hitta till din hemsida.
+      </p> 
+    </div>
+  </div> 
+  <h2 id="accordion-flush-heading-2" class="button-no-back">
+    <button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-2" aria-expanded="false" aria-controls="accordion-flush-body-2">
+      <span class='uppercase'>Lokal SEO analys- Halmstad?</span>
+      <i class="fa-solid fa-angles-down pr-2 text-rose-300"></i>
+    </button>
+  </h2>
+  <div id="accordion-flush-body-2" class="hidden" aria-labelledby="accordion-flush-heading-2">
+    <div class="py-5 border-b">
+      <p class="mb-2 text-sm tracking-wider  text-white">
+      När man gör en analys som är lokal är det bra att fokusera på vad användarens intentioner med sökningen är. Om du har en firma som utför service av värmepumpar i Halmstad, vill man inte synas på sökresultat för människor som exempelvis bor i Stockholm. Man behöver inrikta sig till en lokal kundgrupp.
+      </p>
+    </div>
+  </div> 
+  <h2 id="accordion-flush-heading-3" class="button-no-back">
+    <button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-3" aria-expanded="false" aria-controls="accordion-flush-body-3">
+      <span class='uppercase'>Hur gör jag för att synas i det lokala sökresultatet i Halmstad?</span>
+      <i class="fa-solid fa-angles-down pr-2 text-rose-300"></i>
+    </button>
+  </h2>
+  <div id="accordion-flush-body-3" class="hidden" aria-labelledby="accordion-flush-heading-3">
+    <div class="py-5 border-b">
+      <p class="mb-2 text-sm tracking-wider  text-white">
+      För att synas i lokala sökresultat behöver du inrikta dig för ett specifikt lokalt område. Om du har en firma som utför service av värmepumpar i Halmstad gör man det med sökord som exempelvis ”service värmepump Halmstad”.
+      </p>
+    </div>
+  </div> 
+  <h2 id="accordion-flush-heading-4" class="button-no-back">
+    <button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-4" aria-expanded="false" aria-controls="accordion-flush-body-4">
+      <span class='uppercase'>Vad påverkar min rankning i Halmstad?</span>
+      <i class="fa-solid fa-angles-down pr-2 text-rose-300"></i>
+    </button>
+  </h2>
+  <div id="accordion-flush-body-4" class="hidden" aria-labelledby="accordion-flush-heading-4">
+    <div class="py-5 border-b text-white">
+      <p class="mb-2 text-sm tracking-wider  text-white">
+      Har du för ”breda” sökord som syns över hela Sverige istället för mer lokala sökord rankas du lägre för just din stad. Dina sökord behöver vara mer lokalt anpassade och då rankas man högre i sin stad.
+      </p>
+    
+    </div>
+  </div> 
+  <h2 id="accordion-flush-heading-5" class="button-no-back">
+    <button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-5" aria-expanded="false" aria-controls="accordion-flush-body-5">
+      <span class='uppercase'>Vad bör man tänka på inför en lokal SEO analys i Halmstad?</span>
+      <i class="fa-solid fa-angles-down pr-2 text-rose-300"></i>
+    </button>
+  </h2>
+  <div id="accordion-flush-body-5" class="hidden" aria-labelledby="accordion-flush-heading-5">
+    <div class="py-5 border-b">
+      <p class="mb-2 tracking-wider text-sm text-white">
+      När man vill synas på lokala sökresultat är det bra att på förhand fundera över vilka geografiska områden man vill synas på och om ni har några fördelar jämfört med era lokala konkurrenter. Ska man synas lokalt behöver man inrikta sina sökord. Om du har en frisörsalong i Halmstad, vill du inte synas på sökresultat för personer i Umeå. Välj då att inrikta dig på sökord som innehar den ort du vill synas i.
+      </p>
+    </div>
+  </div> 
  
+  
+  
+    </div>
+ 
+    </section>
+    
 
 
 
