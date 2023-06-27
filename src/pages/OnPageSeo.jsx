527e738a07bdf414f6947cf0e962747412d79183
Mattias Sikmar <mattiassikmar@Mattiass-MBP.localdomain>
2023-06-27 15:48:01 +0200

    On page seo halfway finnished

--- src/pages/OnPageSeo.jsx
+++ src/pages/OnPageSeo.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Google from '../assets/google-fung.webp'
+
 
 export default function OnPageSeo() {
   return (
@@ -79,10 +81,77 @@ export default function OnPageSeo() {
     Google tittar på sidans innehåll för att avgöra om det är relevant för sökningen som görs. En del av den här processen är att leta efter viktiga sökord. Om dessa sökord hittas på din sida, i rubriker eller i brödtext, anses informationen vara mer relevant för sökningen i fråga.
     </p>
 
+    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={Google} alt="" />
+
+    <p class="text-left text-sm pb-5">
+    Vad som gör on-page SEO så viktigt är att det hjälper sökmotorerna att förstå din webbplats och innehållet som finns där, för att identifiera om den är relevant för sökningen som görs. Vad Google gör är att helt enkelt förstå vad användare faktiskt letar efter när de gör sin sökning, även kallat search intent, och att därefter leverera ett sökresultat som uppfyller avsikten bakom sökningen.    
+    </p>
+    <p class="text-left text-sm pb-10">
+    Att hålla sig uppdaterad på vad kraven är för att Google ska anse dig vara relevant är oerhört viktigt, dels för att du ska fortsätta hålla dig synlig för användare, men även för att detta är den del av optimeringen som du faktiskt kan kontrollera, till skillnad från den del som styrs av faktorer utanför sidan.  
+    </p>
+
+    <h1 class="text-left text-4xl pb-5 ">
+    Korta och 
+    <br />
+    relevanta adresser
+    </h1>
+
+    <p class="text-left text-sm pb-5">
+    Webbadressen, eller URL:en, en den exakta länken till sidan. För att hålla all synlig information så relevant som möjligt är det bra att även här använda sig av sökord för att sökmotorerna ska få så bra bild av innehållet på sidan som möjligt.
+    </p>
+    <p class="text-left text-sm pb-5">
+    Tydliga och beskrivande URL:er hjälper användare att förstå vad sidan handlar om innan de klickat sig in på den. Oavsett om sidorna egentligen skulle handla om samma saker, framgår det av den första webbadressen att det är just vad sökaren är ute efter. Detta ger ett klart bättre resultat när det kommer till att öka trafiken till din sida.
+    </p>
+    <p class="text-left text-sm pb-5">
+    I de flesta fall kan du enkelt ändra URL-sluggen, den del efter domänen och eventuella undersidor, vilket är det enklaste sättet att anpassa webbadressen för det aktuella sökordet du vill synas på.
+    </p>
+    <p class="text-left text-sm pb-20">
+    Precis som med meta-beskrivning är det bäst att testa sig fram för att nå ett bra resultat.
+    </p>
+
+    <h1 class="text-center text-lg pb-5 ">
+    Tydliga och beskrivande URL:er hjälper besökare och
+    <br />
+    sökmotorer att enklare förstå vad sidan handlar om
+    </h1>
     </div>
     </section>
+
+    <div class="blacktopdivider fb-custom-color pb-20">
+    </div>
    
-   
+    <section>
+    <div class='pt-20 heading-container fb-custom-color text-white'>
+    <h1 class="text-left text-4xl pb-5 ">
+    Relevant
+    <br />
+    Content
+    </h1>
+    <p class="text-left text-sm pb-5">
+    Att fylla din hemsida med lättlästa och användarvänliga texter som innehåller sökord som du vill ranka på, hjälper Google och andra sökmotorer att anse sidan vara mer relevant. Då Google algoritmer läser igenom informationen hos en webbsida tittar den på textinnehållet för att ta reda på vad den handlar om, för att sedan matcha den till sökningarna som görs på ämnet i fråga. Skulle texten på sidan matcha sökningen som görs, anses sidan vara relevant. Ju fler kriterier som uppfylls på din hemsida, desto mer relevant är den och därmed rankar den högre.
+    </p>
+
+    <p class="text-left text-sm pb-10">
+    Vad som avgör hur bra innehållet på din hemsida står sig gentemot konkurrenters sidor beror på en rad element som alla väger olika tungt i Googles ögon. All text anses inte vara lika betydelsefull när det kommer till att öka din relevans, vilket gör det viktigt att veta vilka element som betyder mest.
+    </p>
+
+    <h1 class="text-left text-4xl pb-5 ">
+    Naturligt & användarvänligt innehåll   
+    </h1>
+    <p class="text-left text-sm pb-5">
+    Element som huvudrubrik (H1), underrubriker (H2, H3 osv.), listor och fetstilta ord påverkar alla hur hög relevans din sida har och genom att använda sig av sökord på dessa viktiga positioner, stärker du din webbsidas relevans betydligt.   
+    </p>
+    <p class="text-left text-sm pb-5">
+    Används sökord som du vill ranka på endast ett fåtal gånger på hemsidan anses det inte troligt att den handlar om det som sökningen avser. Ju mer text som finns, desto fler gånger behöver söktermen finnas med för att texten ska vara relevant. Om du däremot använder söktermer onaturligt mycket ser Google det som”keyword spamming” vilket kan göra att sidans ranking istället försämras.    
+    </p>
+    <p class="text-left text-sm pb-5">
+    Genom att använda sig av innehåll som är naturligt och användarvänligt håller du både besökare och sökmotorer nöjda, vilket oftast lönar sig i det långa loppet.   
+    </p>
+
+    </div>
+    </section>
+
+
    </>
   )
 }
