aae6a3b5a215ee489110ee1895e1ba0fccd5b26a
Mattias Sikmar <mattiassikmar@Mattiass-MBP.localdomain>
2023-06-28 11:56:07 +0200

    Tjanster page finnished

--- src/pages/Tjanster.jsx
+++ src/pages/Tjanster.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import ContactForm from '../components/ContactForm'
+import img from '../assets/effektify-digitalbyra-symbol.webp'
+import gif from '../assets/PONG_Light_version2.gif'
 
 export default function Tjanster() {
   return (
@@ -27,21 +30,21 @@ export default function Tjanster() {
     <div class="text-center pb-10">
     <a href="/google-ads">
     <i class="fa-solid fa-chart-column fa-2xl custom-icon"></i>
-    <p class="text-center text-sm"> Google Ads</p>
+    <p class="text-center text-sm pt-3"> Google Ads</p>
     </a>
     </div>
 
     <div class="text-center pb-10">
     <a href="/seo-sokmotoroptimering">
     <i class="fa-solid fa-magnifying-glass fa-flip-horizontal fa-2xl custom-icon"></i>
-    <p class="text-center text-sm"> Sökmotoroptimering</p>
+    <p class="text-center text-sm pt-3"> Sökmotoroptimering</p>
     </a>
     </div>
 
     <div class="text-center pb-10">
     <a href="/webbcopy">
     <i class="fa-solid fa-pen-to-square fa-flip-horizontal fa-2xl custom-icon"></i>
-    <p class="text-center text-sm"> Webbcopy</p>
+    <p class="text-center text-sm pt-3"> Webbcopy</p>
     </a>
     </div>
 
@@ -49,28 +52,79 @@ export default function Tjanster() {
     <div class="text-center pb-10">
     <a href="/facebook-ads-instagram-ads/">
     <i class="fa-solid fa-comments fa-2xl custom-icon"></i>
-    <p class="text-center text-sm"> Facebook Ads</p>
+    <p class="text-center text-sm pt-3"> Facebook Ads</p>
     </a>
     </div>
 
     <div class="text-center pb-10">
     <a href="/webb/">
     <i class="fa-solid fa-globe fa-2xl custom-icon"></i>
-    <p class="text-center text-sm"> Webbdesign</p>
+    <p class="text-center text-sm pt-3"> Webbdesign</p>
     </a>
     </div>
 
 
     <div class="text-center pb-10">
-    <a href="/on-page/">
+    <a href="/on-page-seo/">
     <i class="fa-solid fa-note-sticky fa-2xl custom-icon"></i>
-    <p class="text-center text-sm"> On-page</p>
+    <p class="text-center text-sm pt-3"> On-page</p>
     </a>
     </div>
+    </div>
+    </section>
 
+    <section>
+    <div class='pt-20 heading-container bg-black text-white'>
+    <h1 class="text-center text-3xl pb-5 ">
+    Öka försäljningen
+    med Google Ads
+    </h1>
+    <p class="text-sm text-left pb-10">
+    Google Ads är ett utmärkt verktyg för dig som vill
+    <em class='text-rose-300'> öka din försäljning, öka antalet människor som besöker din hemsida eller som vill ha fler återkommande kunder. </em>
+    Med Google Ads kan du specificera den målgrupp du riktar dig mot. Du har även möjlighet att rikta in annonserna geografiskt så att du syns på specifika sökord i specifika orter.
+    </p>  
+
+    <p class="text-lg text-left pb-20">
+    <a href="/google-ads"><span>Kom igång med Google Ads redan idag!</span></a>
+    </p>
+
+    <h1 class="text-right text-4xl pb-5 ">
+    Få fler besökare
+    till din hemsida
+    </h1>
+    <p class="text-sm text-right pb-10">
+    Att synas på Google ger ditt företag större möjlighet att nå ut till kunder som söker efter de produkter eller tjänster som just du erbjuder. Sökmotoroptimering eller SEO  är därför livsviktigt för att ditt företag ska synas online, få fler besökare och göra fler affärer som följd av detta.   
+    </p>
+
+    <p class="text-sm text-center pb-10">
+    <a href="/on-page-seo">Läs mer om just <em class="text-rose-300">Sökmotoroptimering</em></a>   
+    </p>
+
+    <h1 class="text-center text-6xl pb-10 ">
+    vi är
+    <span class="underline decoration-rose-300"> effektify</span>
+    </h1>
+
+    <h1 class="text-center text-4xl pb-5 ">
+    och vi är den
+    </h1>
+
+    <h1 class="text-center text-5xl pb-5 ">
+    kreativa digitalbyrån
+    du behöver
+    </h1>
+
+    <img src={img} class="bg-effect invert" alt="" />
 
     </div>
     </section>
+
+    <div class="whitedivider5 invert  pb-20">
+      </div>
+
+    <img src={gif} alt="" />
+    <ContactForm />
     </>
   )
 }
