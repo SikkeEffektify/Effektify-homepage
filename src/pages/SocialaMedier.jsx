00fb8e1aaa5095040133e79b0c46894a0790212b
Mattias Sikmar <mattiassikmar@Mattiass-MBP.localdomain>
2023-06-29 13:54:47 +0200

    Progress on Sociala medier page

--- src/pages/SocialaMedier.jsx
+++ src/pages/SocialaMedier.jsx
@@ -1,7 +1,106 @@
 import React from 'react'
+import { Hero6 } from '../components/Hero6'
+import eBackground from '../assets/effektify-white-digital.webp'
+import img2 from '../assets/digital-marknadsforing-drink.gif'
+import gif from '../assets/PONG_Light_version2.gif'
+import { Cards4 } from '../components/Cards4'
+import ContactForm from '../components/ContactForm'
 
 export default function SocialaMedier() {
   return (
-    <div>SocialaMedier</div>
+    <>
+    <section>
+   <div class='pt-40 pb-40 heading-container bg-cyan-700 text-white'>
+    <h1 class="text-left text-8xl text-rose-300 pb-10 ">
+    Sociala Medier
+    </h1>
+    <p class="text-lg text-left pb-10">
+    Med en av våra persoliga specialister har annonsering aldrig varit enklare. Vi skräddarsyr en strategi baserat på djupgående analyser av önskade sökord och konkurrens för att kunna erbjuda en lösning som passar din bransch och ditt företag.
+    </p>
+    </div>
+    </section>
+
+    <Hero6/>
+
+
+    <section>
+    <div class='pt-20  heading-container bg-white text-black'>
+    <h1 class="text-left text-3xl pb-5 ">
+    Rätt strategi – rätt resultat.
+    </h1>
+    <p class="text-left text-sm pb-5">
+    Med hjälp av väl optimerade annonser kommer det att vara betydligt större chans att dina potentiella kunder hittar dig när söker efter produkter eller tjänster som ditt företag erbjuder. När det kommer till digital marknadsföring har vi på Effektify både kunskapen och erfarenheten som krävs för att synas online idag.   
+    </p>
+    <p class="text-left text-sm pb-20">
+    Vare sig det rör sig om annonsering på 
+    <a class="text-rose-300" href="/digital-annonsering"> sociala medier </a>
+    eller på
+    <a class="text-rose-300" href="/digital-annonsering"> Google ads</a>
+    , erbjuder vi både den strategi som passar just ditt företag, samt rapporter med mätbara resultat så att du enkelt kan följa utvecklingen av dina kampanjer.
+    </p>
+
+    <img src={eBackground} class="bg-effect " alt="" />
+
+    <h1 class="text-right text-5xl pt-10 pb-5 ">
+    på rätt plats
+    <br />
+    vid rätt tid
+    </h1>
+    <p class="text-right text-sm pb-10">
+    Google är Sveriges i särklass största sökmotor och 95% av alla sökningar görs idag via deras plattform. Att synas högt upp på Google ger företag möjlighet att nå ut till potentiella kunder när dessa själva gör en sökning. Sökmotoroptimering eller SEO är med andra ord ett av de bästa sätten att nå nya kunder.   
+    </p>
+    <p class="text-left text-sm pb-5">
+    Det finns olika sätt att synas på Google, man kan betala för att synas genom att annonsera via Google Ads eller genom att synas organiskt när potentiella kunder söker (sökmotoroptimering). Bägge sätten är viktiga delar av en modern digital marknadsföringsstrategi.
+    </p>
+    <p class="text-left text-sm pb-5">
+    En av fördelarna med att annonsera med Google Ads är att det inte tar lika lång tid att synas som när man arbetar med sökmotoroptimering då du betalar för annonser och besökare. Nackdelen med annonsering är att så fort du slutar lägga pengar på annonser slutar du synas och får inga fler besök till hemsidan. Med sökmotoroptimering syns du varje gång nya potentiella kunder googlar efter det du säljer, dygnet runt året om.   
+    </p>
+
+
+    </div>
+    </section>
+        
+
+    <div class="bg-white  ">
+      <p class="text-right text-xl pb-3 pr-2">Luta dig tillbaka och låt oss 
+        <br />
+        göra det vi är bra på.</p>
+    <img class="flower mx-auto invert " src={img2} alt="" />
+    </div>
+
+    <Cards4 />
+    <img src={gif} alt="" />
+    <ContactForm />
+
+
+    <section>
+    <div class='pt-10  heading-container text-white'>
+    <section class="cards-google4">
+       <div class="pt-5 cards-container2 pb-10">
+     <div class="custom-back max-w-sm pb-10 border border-black shado mx-auto pt-10">
+        <h1 class="pt-40 cards2-title text-center mb-2 font-semibold tracking-tight uppercase text-rose-300"><span class="text-white"></span></h1>
+        <h2 class="pb-20 text-center cards2-under-title text-white"><span class="pl-4"></span></h2>
+    </div>
+    </div>
+    </section>
+
+    <h1 class="text-left text-5xl pt-10 pb-5 ">
+    Öka din synlighet
+    <br />
+    med Google Ads
+    </h1>
+    <p class="text-left text-sm pb-10">
+    Google är Sveriges i särklass största sökmotor och 95% av alla sökningar görs idag via deras plattform. I Sverige görs det varje dag mer än 50 miljoner sökningar på Google. Med siffror som dessa inser du säkert vikten av att synas högt upp i sökresultatet som Google visar upp. Konkurrensen mellan företag som vill ranka högt upp på Google är idag oerhört stor då många idag förstår betydelsen av vad en bra position innebär. Med andra ord, syns du inte så finns du inte.
+    </p>
+
+    <p class="text-right text-sm pb-10">
+    Google är Sveriges i särklass största sökmotor och 95% av alla sökningar görs idag via deras plattform. I Sverige görs det varje dag mer än 50 miljoner sökningar på Google. Med siffror som dessa inser du säkert vikten av att synas högt upp i sökresultatet som Google visar upp. Konkurrensen mellan företag som vill ranka högt upp på Google är idag oerhört stor då många idag förstår betydelsen av vad en bra position innebär. Med andra ord, syns du inte så finns du inte.    
+    </p>
+
+
+
+    </div>     
+    </section>
+    </>
   )
 }
