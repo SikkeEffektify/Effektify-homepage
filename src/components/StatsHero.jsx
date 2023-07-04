39761e809f0a22326a023471e0a75cc953877e82
Mattias Sikmar <mattiassikmar@Mattiass-MBP.localdomain>
2023-07-04 16:17:21 +0200

    Made StatsHero Nonstatic and renamed

--- src/components/StatsHero.jsx
+++ src/components/StatsHero.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 import './Hero.css'
 import videoBg from '../assets/City-By-Night-low.mp4'
 import divider from '../assets/whitedivider.svg'
-export const Hero7 = () => {
+export const StatsHero = ({videoLink, title1, title2, desc, title3, title4, stats1, stats2, stats3, underStats1, underStats2, underStats3}) => {
   return (
 <section>
    <div className='hero'>
     <div className="vidOverlay"></div>
-    <video class="min-w-full min-h-full absolute object-cover" src={videoBg} autoPlay loop muted />
+    <video class="min-w-full min-h-full absolute object-cover" src={videoLink} autoPlay loop muted />
     <div class="vidContent pt-20 text-center ">
-        <h1>Det krävs en strategi för att växa</h1>
+        <h1>{title1}</h1>
         <a class="pt-40" href="#testtyp"><i class="fa-solid fa-circle-chevron-down fa-lg"></i></a>
     </div>
    </div>
    <section class="bg-white pt-10 text-lg font-black text-black heading-container pb-10">
-    <h2 class="uppercase">MED TYDLIGA STRATEGIER OCH TRANSPARANS EFFEKTIVISERAR vi TILLVÄXT OCH LÖNSAMHET</h2>
+    <h2 class="uppercase">{title2}</h2>
     <p class="font-light pt-5 text-sm">
-    Vi hjälper idag över 250 företag i Sverige, övriga Norden, Tyskland och Nederländerna. Ert mål är vår drivkraft, och utifrån full transparens i alla led och med dagens teknik, strävar vi på effektify mot att skapa tillväxt och lönsamhet för er som har en vision och en dröm. 
+    {desc}
     </p>
    </section>
    <section>
@@ -26,20 +26,20 @@ export const Hero7 = () => {
     <div className="vidOverlay"></div>
     <video class="min-w-full min-h-full absolute object-cover" src="https://player.vimeo.com/progressive_redirect/playback/714795502/rendition/1080p/file.mp4?loc=external&signature=f10571525f87060048a4f457365b416995804594ef77eb283e82fe660968d63e" autoPlay loop muted />
     <div class="vidContent2 text-center heading-container ">
-        <p class="text-base uppercase font-light">Siffrorna talar sitt tydliga språk</p>
-        <h1 class="uppercase text-2xl pt-5 pb-5">Med strategier som fungerar har vi hjälpt företag att synas med</h1>
+        <p class="text-base uppercase font-light">{title3}</p>
+        <h1 class="uppercase text-2xl pt-5 pb-5">{title4}</h1>
         <div class="ring-4 ring-yellow-400 pr-5 pl-5 pt-10 pb-10 rounded-full"> 
-        <span>1047</span>
+        <span>{stats1}</span>
         </div>
-        <h3 class='text-2xl pt-5 pb-5'>Sökord i top 3</h3>
+        <h3 class='text-2xl pt-5 pb-5'>{underStats1}</h3>
         <div class="ring-4 ring-green-400 pr-5 pl-5 pt-10 pb-10 rounded-full"> 
-        <span>1832</span>
+        <span>{stats2}</span>
         </div>
-        <h3 class='text-2xl pt-5 pb-5'>Sökord i top 10</h3>
+        <h3 class='text-2xl pt-5 pb-5'>{underStats2}</h3>
         <div class="ring-4 ring-pink-500 pr-5 pl-5 pt-10 pb-10 rounded-full"> 
-        <span>2408</span>
+        <span>{stats3}</span>
         </div>
-        <h3 class='text-2xl pt-5 pb-5'>Sökord i top 20</h3>
+        <h3 class='text-2xl pt-5 pb-5'>{underStats3}</h3>
     </div>
    </div>
    </section>
--- src/pages/Erbjuder.jsx
+++ src/pages/Erbjuder.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
-import { Hero7 } from '../components/Hero7'
+import videoHeroButton from '../assets/spaceloop2.mp4'
 import { Heading7 } from '../components/Heading7'
 import { HeroButton } from '../components/HeroButton'
 import { Headinglist } from '../components/Headinglist'
+import { StatsHero } from '../components/StatsHero'
+import videoStatsHero from '../assets/City-By-Night-low.mp4'
 
 export default function Erbjuder() {
   return (
     <>
-    <Hero7 />
+    <StatsHero 
+      videoLink={videoStatsHero}
+      title1={"Det krävs en strategi för att växa"}
+      title2={"MED TYDLIGA STRATEGIER OCH TRANSPARANS EFFEKTIVISERAR vi TILLVÄXT OCH LÖNSAMHET"}
+      title3={"Siffrorna talar sitt tydliga språk"}
+      title4={"Med strategier som fungerar har vi hjälpt företag att synas med"}
+      desc={"Vi hjälper idag över 250 företag i Sverige, övriga Norden, Tyskland och Nederländerna. Ert mål är vår drivkraft, och utifrån full transparens i alla led och med dagens teknik, strävar vi på effektify mot att skapa tillväxt och lönsamhet för er som har en vision och en dröm."}
+
+      stats1={"1047"}
+      underStats1={"Sökord i top 3"}
+      stats2={"1832"}
+      underStats2={"sökord i top 10"}
+      stats3={"2408"}
+      underStats3={"sökord i top 20"}
+    
+    
+    />
     <Heading7/>
     <Headinglist />
-    <Hero2 />
+    <HeroButton 
+      title={"We're ready for takeoff"}
+      title2={"are you?"}
+      buttonLink={"/kontakt"}
+      buttonText={"Launch"}
+      videoLink={videoHeroButton}
+    />
     </>
   )
 }
