63389482ac94d859bee303a62780da3d6638da04
Mattias Sikmar <mattiassikmar@Mattiass-MBP.localdomain>
2023-07-06 16:17:59 +0200

    HeadingSEO5  nonstatic

--- src/pages/SEOSokopt.jsx
+++ src/pages/SEOSokopt.jsx
@@ -553,7 +553,81 @@ export default function SEOSokopt() {
         buttonStyle5={"pl-5 pr-5"}
         buttonLink5={"/seo-sokmotoroptimering-kungsbacka"}
       />
-      <HeadingSEO5 />
+      <HeadingSEO5 
+        mainTitle1={"Hur hittar Google din content och indexerar det?"}
+        mainDesc1={`
+          <p class="text-sm text-left pb-5">Optimering av crawl är viktigt när det kommer till sökmotoroptimering. Det handlar i stort sätt om saker du kan göra för att hjälpa Google att enkelt upptäcka, genomsöka och indexera det innehåll du förmedlar på din sajt.</p>
+          <p class="text-sm text-left pb-5">Sökmotorbotar letar ständigt efter relevant innehåll. När din nya text är live behöver Google sedan hitta den och indexera din artikel för att den ska göra någon nytta. Det betyder dock inte att botarna kommer att hitta den, crawla och indexera den utan din insats. Speciellt inte om du vill att det ska gå snabbt. Utan att hjälpa sökspindlarna på traven kan det ta upp till två dagar innan ditt ämne.</p>
+          <p class="text-sm text-left pb-5">För att allt ska gå fortare kan du börja med att installera ett pluginet Google XML-sitemapgenerator för WordPress. Du behöver inte göra någonting när du väl har installerat insticksprogrammet. I samma ögonblick som du trycker på knappen ”publicera”, genereras en ny XML-sitemap som skickas till sökmotorerna och katalogerna. Detta hjälper Google att indexera ditt nya innehåll så snabbt som möjligt, samtidigt som din befintliga länkprofil söks igenom och indexeras.</p>
+        `}
+
+        cardTitle1={"ANVÄND DIG AV SOCIALA MEDIER FÖR ATT HITTAS SNABBARE"}
+        cardDesc1={`
+          <p class="text-sm text-white pb-5">Genom att uppdatera dina sociala profiler får du också ditt nya innehåll indexerat snabbare. Genom att dela länken på din Facebook-sida eller på Twitter, skapar du fler vägar in till din hemsida, och gör att sökmotorerna enklare kan hitta, crawla och indexera den.</p>    
+        `}
+
+        afterCardText={"Ett annat enkelt verktyg som fungerar väldigt bra är Pingomatic. Det låter dig snabbt pinga en massa webbtjänster för att meddela dem om ditt nya innehåll. Genom att använda denna gratistjänst får man sitt färska, högkvalitativa innehåll indexerat på betydligt kortare tid än det skulle tagit annars."}
+        afterCardButton={"Ready for takeoff"}
+        afterCardLink={"/kontakt"}
+        
+        mainTitle2={"Är gästbloggar bra för SEO?"}
+        mainDesc2={`
+          <p class="text-sm text-left pb-5">
+          Det blev ett ramaskri 2014 när Matt Cutts sa att ”om du använder gästbloggning som ett sätt att få länkar … sluta”. Många som inte förstod exakt vad det uttalandet innebar började omedelbart predika mot gästbloggning.
+          </p>
+          <p class="text-sm text-left pb-5">
+          Det finns ingen teknisk skillnad mellan gästbloggar och andra typer av innehåll. Gästbloggning innebär helt enkelt att en gäst har bidragit med ett innehåll som sedan länkar till din sajt.
+          </p>
+          <p class="text-sm text-left pb-5">
+         Det Matt Cutts menade med sitt uttalande är att inte länka dåligt innehåll på bloggar från en utomstående författare. Men även när författaren var webbplatsens ägare hade Googles Panda-uppdatering redan börjat straffa innehåll av låg kvalitet.
+          </p>
+        `}
+
+
+        cardTitle2={"BRA LÄNKAR GÖR DIG MER TROVÄRDIG"}
+        cardDesc2={`
+          <p class="text-sm text-white pb-5">Digital marknadsföring är beroende av högkvalitativt innehåll med en stark länkprofil. Sökmotorerna indexerar miljarder webbsidor från olika domännamn, IP-adresser, platser osv, som alla ägs av olika personer. Att sammanlänka med dessa skapar relevans för dig och en stark länkprofil.</p>    
+        `}
+
+        endDesc={`
+          <p class="text-sm pb-5">
+           När det kommer till externa länkar är det varken författaren eller innehållet som är problemet, utan sammanhanget. Så länge artikeln kommer från en pålitlig källa, och innehållet i artikeln är relevant och är av hög klass, fungerar länken absolut som en bra rekommendation som stärker din hemsidas auktoritet.
+          </p>
+          <p class="text-sm pb-5">
+          Vi anser att gästbloggning inte är dött. Och det kommer aldrig att dö, eftersom Google inte kan tyda exakt vad ett ”gästinlägg” är, särskilt inte när författarens länk finns direkt i texten och inte i författarprofilen.    
+          </p>
+          </div>
+        
+          <div class="heading-container pt-10 text-black">
+          <h1 class="text-lg uppercase text-center pb-5">VI REKOMMENDERAR FORTFARANDE GÄSTBLOGGNING</h1>
+          <p  class='text-sm pb-5'>
+          Iallafall så länge man följer dessa enkla råd:
+          </p>
+    
+          <p  class=' text-rose-300 text-base pb-2 '>
+          Kalla dem inte för ”gästinlägg”
+          </p>
+          <p  class='text-sm pb-5'>
+          Detta är viktigt. Google kan hitta texten ”detta är ett gästinlägg”. Skriv i stället som om du är en intern medarbetare. Det spelar ingen roll varifrån gästen kommer, så länge inlägget är användbart och relevant för din webbplats.
+          </p>
+    
+          <p  class=' text-rose-300 text-base pb-2 '>
+          Bygg relationer, inte länkar
+          </p>
+          <p  class='text-sm pb-5'>
+          Försök inte få ditt gästinlägg publicerat på en betrodd blogg bara för länkens skull. Om du tänker så, kommer du att gå miste om relationen.
+          </p>
+    
+          <p  class=' text-rose-300 text-base pb-2 '>
+          Bygg upp ditt varumärke
+          </p>
+          <p  class='text-sm pb-5'>
+          Använd ditt varumärkesnamn eller din URL som ankartext för naturlig länkuppbyggnad. Kontextbaserade länkar: Försök att så långt det är möjligt hålla dina länkar inom innehållet, inte i författarprofilen där de inte har lika stor tyngd. Använd gästbloggande med försiktighet och fokusera på slutanvändarna. Skapa högkvalitativt innehåll som får dem att tänka – som ger resonans med dem och löser deras problem. Engagera dem, svara på kommentarer och bygg upp din e-postlista.
+          </p>
+    
+        `}
+
+      />
       <HeadingCity
         title={"Behöver du hjälp med sökmotoroptimering i din stad?"}
         button1={"SEO i Linköping"}
@@ -572,7 +646,10 @@ export default function SEOSokopt() {
         buttonStyle5={"pl-1 pr-1"}
         buttonLink5={"/seo-sokmotoroptimering-norrkoping"}
       />
-      <HeadingSEO6 />
+      <HeadingSEO6 
+      
+      
+      />
       <HeadingCity
         title={"Behöver du hjälp med sökmotoroptimering i din stad?"}
         button1={"SEO i Norrtälje"}
