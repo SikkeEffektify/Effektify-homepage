86446c3383145263a461c07a0be07f0e46a54193
Mattias Sikmar <mattiassikmar@Mattiass-MBP.localdomain>
2023-07-07 15:52:50 +0200

    Searchengine optimization progress

--- src/pages/SEOSearchEngine.jsx
+++ src/pages/SEOSearchEngine.jsx
@@ -525,81 +525,91 @@ export default function SEOSearchEngine() {
         buttonLink5={"/seo-sokmotoroptimering-helsingborg"}
       />
       <HeadingSEO4 
-        mainTitle1={"Bra content ger bra resultat"}
+        mainTitle1={"Good content produces good results"}
         mainDesc1={`
           <p class="text-sm text-left pb-5">
-          Content, eller innehåll som det kallas på svenska, är något Google finner allt viktigare. Google strävar efter att visa upp hemsidor som de anser ger deras användare bästa möjliga svar. För dig som har en hemsida innebär detta att innehållet på din hemsida är viktigt för huruvida du syns på Google eller inte när någon googlar efter det du erbjuder.
+          Content is something Google finds increasingly important. Google strives to display websites that they believe provide their users with the best possible answers. For those of you who have a website, this means that the content of your website is important for whether or not you appear on Google when someone googles what you offer.
           </p>
   
-          <h1 class="text-lg pt-5 uppercase text-center pb-5">I Sverige görs det varje dag mer än 50 miljoner sökningar på Internet. Syns din hemsida i dessa sökningar idag?</h1> 
+          <h1 class="text-lg pt-5 uppercase text-center pb-5">
+          In Sweden, more than 50 million searches are made on the Internet every day. Does your website appear in these searches today?
+          </h1> 
           <p class="text-sm text-left pb-5">
-          Att fylla din hemsida med lättlästa och användarvänliga texter som innehåller sökord som du vill ranka på, hjälper Google och andra sökmotorer att anse sidan vara mer relevant.
+          Filling your website with easy-to-read and user-friendly texts that contain keywords, will help Google and other search engines consider the page more relevant.
           </p>
       
-          <h1 class="text-lg pt-5 uppercase text-center pb-5">RÄTT CONTENT FÖR RÄTT MÅLGRUPP = BRA SÖKMOTOROPTIMERING</h1>
+          <h1 class="text-lg pt-5 uppercase text-center pb-5">
+          RIGHT CONTENT FOR RIGHT TARGET GROUP = GOOD SEARCH ENGINE OPTIMIZATION
+          </h1>
           <p class="text-sm text-left pb-5">
-          Då Googles algoritmer läser igenom informationen hos en webbsida tittar den på textinnehållet för att ta reda på vad den handlar om, för att sedan matcha den till sökningarna som görs på ämnet i fråga. Skulle texten på sidan matcha sökningen som görs, anses sidan vara relevant. Ju fler kriterier som uppfylls på din hemsida, desto mer relevant är den och därmed rankar den högre.
+          When Google's algorithms read through the information of a web page, it looks at the textual content to find out what it is about, and then matches it to the searches made on the subject in question. Should the text on the page match the search being made, the page is considered relevant. The more criteria your website fulfills, the more relevant it is and therefore ranks higher.
           </p>
           <p class="text-sm text-left pb-5">
-          Vad som avgör hur bra innehållet på din hemsida står sig gentemot konkurrenters sidor beror på en rad element som alla väger olika tungt i Googles ögon. All text anses inte vara lika betydelsefull när det kommer till att öka din relevans, vilket gör det viktigt att veta vilka element som betyder mest.
+          What determines how well the content on your website compares to competitors' pages depends on a number of elements that all weigh differently in Google's eyes. Not all text is considered equally important when it comes to increasing your relevance, which makes it important to know which elements matter the most.
           </p>
   
           `}
 
-          cardTitle1={"NATURLIGT & ANVÄNDARVÄNLIGT INNEHÅLL HJÄLPER SEO"}
+          cardTitle1={"NATURAL & USER-FRIENDLY CONTENT HELPS SEO"}
           cardDesc1={`
-            <p class="text-sm text-white pb-5">Element som huvudrubrik (H1), underrubriker (H2, H3 osv.), listor och fetstilta ord påverkar alla hur hög relevans din sida har och genom att använda sig av sökord på dessa viktiga positioner, stärker du din webbsidas relevans betydligt och förbättrar resultatet.</p>
-            <p class="text-sm text-white pb-5">Används sökord som du vill ranka på endast ett fåtal gånger på hemsidan anses det inte troligt att den handlar om det som sökningen avser. Ju mer text som finns, desto fler gånger behöver söktermen finnas med för att texten ska vara relevant. Om du däremot använder söktermer onaturligt mycket ser Google det som”keyword spamming” vilket kan göra att sidans ranking istället försämras.</p>
-            <p class="text-sm text-white pb-5">Genom att analysera och använda sig av innehåll som är naturligt och användarvänligt håller du både besökare och sökmotorer nöjda, vilket oftast lönar sig i det långa loppet när man ser till sökresultaten.</p>
+            <p class="text-sm text-white pb-5">
+            Elements such as main heading (H1), sub-headings (H2, H3, etc.), lists and bold words all affect how relevant your page is and by using keywords in these important positions, you significantly strengthen your web page's relevance and improve results.
+            </p>
+            <p class="text-sm text-white pb-5">
+            If a keyword that you want to rank for is used only a few times on the website, it is not considered likely that it is about what the search refers to. The more text there is, the more times the search term needs to be included for the text to be relevant. If, on the other hand, you use search terms unnaturally, Google sees it as "keyword spamming", which can cause the page's ranking to deteriorate instead.
+            </p>
+            <p class="text-sm text-white pb-5">
+            By analyzing and using content that is natural and user-friendly, you keep both visitors and search engines happy, which usually pays off in the long run when looking at the search results.
+            </p>
           `}
 
 
-          mainTitle2={"Interna länkar gör dig mer relevant"}
+          mainTitle2={"Internal links make you more relevant"}
           mainDesc2={`
             <p class='text-sm text-left pb-5'>
-            En intern länk är när en sidas innehåll även består av länkar, alltså man hänvisar till en annan sida på samma hemsida. Som exempel länkar vi här till vår sida för Google Ads härifrån, vilket är ett verktyg som fungerar som en intern länk. Då många sidor pekar med länkar till samma sida blir denna sida mer relevant vilket gör att den rankar bättre på Google.
+            An internal link is when a page's content also consists of links, meaning you refer to another page on the same website. As an example, we link here to our page for Google Ads from here, which is a tool that acts as an internal link. Since many pages point with links to the same page, this page becomes more relevant, which makes it rank better on Google.
             </p>
             <p class='text-sm text-left pb-5'>
-            Genom att använda sig av relevanta sökord i länktexten för den interna länken ökar också relevansen för målsidan som länken pekar till. Detta gör att du med hjälp av viktiga sökord i länktexten kan tala om för Google att målsidan den pekar till är relevant för de sökningar som görs på sökordet.
+            By using relevant keywords in the link text for the internal link, the relevance of the target page that the link points to also increases. This allows you to use important keywords in the link text to tell Google that the landing page it points to is relevant to the searches made on the keyword.
             </p>
             <p class='text-sm text-left pb-5'>
-            Tillsammans med meta-data, content & rubriker på målsidan hjälper varje intern länk till att öka relevansen för målsidan ytterligare, och får denna att ranka högre på sökmotorerna.
+            Together with metadata, content & headers on the website, each internal link helps to further increase the relevance of the landing page, and makes it rank higher on the search engines.
             </p>
           `}
 
-          underImageDesc={"Marknadsföring av dina tjänster och produkter med en seo, alltså optimering eller sökmotoroptimering som är utförd av professionella och erfarna seo-tekniker som har rätt verktyg och know-how skapar resultat i sökmotorerarna. Vi vet att sökmotorerna gillar länkar på en sajt, för få länkar är mindre bra, vi vet att ord i texter om tjänster och produkter inom verksamheten är viktiga, vi vet att seo vid search kräver innehåll av kvalitet och vi vet att driva trafik till en sajt genom att optimera den kan ge den ett bättre läge än andra webbplatser vid search på nätet. Optimering är den självklara partnern till en sajt."}
+          underImageDesc={"Marketing your services and products with an seo, i.e. optimization or search engine optimization that is carried out by professional and experienced seo technicians who have the right tools and know-how creates results in the search engines. We know that search engines like links on a site, because few links are less good, we know that words in texts about services and products within the business are important, we know that seo in search requires quality content and we know to drive traffic to a site by optimizing it can give it a better position than other websites in online searches. Optimization is the obvious partner of a site."}
           underImageButton={"Ready for takeoff"}
-          underImageLink={"/kontakt"}
+          underImageLink={"/en/contact-us"}
 
-          mainTitle3={"Off-page SEO stärker dina positioner"}
+          mainTitle3={"Off-page SEO strengthens your positions"}
           mainDesc3={`
             <p class='text-sm text-left pb-5'>
-            En annan viktig del av SEO är off-page. Off-page är det kontinuerliga arbete som ska utföras efter att arbetet med on-page är klart på hemsidan, man kan säga att det är det underhåll som krävs för att få din hemsida att behålla sin placering på Google.
+            Another important part of SEO is off-page. Off-page is the continuous work that must be carried out after the work with on-page is finished on the website, you can say that it is the maintenance required to get your website to maintain its position on Google. Off-page SEO strengthens your positions.
             </p>
       
             <p class='text-sm text-left pb-5'>
-            När Google gör en bedömning av huruvida din hemsida är relevant vad gäller specifika sökord ser sökmotorn över innehållet på hemsidan. Dock är det inte bara innehållet Google ser över, utan sökmotorn ser även över vilka hemsidor som länkar till din hemsida.
+            When Google makes an assessment of whether your website is relevant in terms of specific keywords, the search engine looks over the content of the website. However, it is not only the content that Google looks over, but the search engine also looks over which websites link to your website.
             </p>
             </div>
       
             <div class='pt-20 text-center text-black bg-white'>
-            <h1 class="text-xl uppercase pb-5">BRA REKOMMENDATIONER GER BRA POSITIONER</h1>
+            <h1 class="text-xl uppercase pb-5">GOOD RECOMMENDATIONS GIVE GOOD POSITIONS</h1>
       
             <p class='text-sm text-left pb-5'>
-            På samma sätt som vi människor baserar många av våra köpbeslut på rekommendationer så baserar Google sina placeringar av hemsidor på ”rekommendationer” från andra hemsidor. Dessa rekommendationer är viktiga för att få upp din hemsida så högt upp som möjligt på Google. Google anser ”inlänkar” från andra hemsidor vara ”rekommendationer” till din hemsida.
+            In the same way that we humans base many of our purchase decisions on recommendations, Google bases its rankings of websites on "recommendations" from other websites. These recommendations are important to get your website as high as possible on Google. Google considers "links" from other websites to be "recommendations" to your website.
             </p>
             <p class='text-sm text-left pb-5'>
-            När Google ska bestämma huruvida din hemsida är relevant att visa upp på första sidan tar sökmotorn hänsyn till om det finns andra hemsidor som länkar till din, och ”lyssnar” till detta. Vad detta betyder är att inlänkar är en rankningsfaktor och därmed påverkar möjligheten att synas på Googles första sida. Man kan säga att det är en grundläggande del av SEO att ha en effektiv länkstrategi.
+            When Google has to decide whether your website is relevant to display on the first page, the search engine takes into account whether there are other websites that link to yours, and "listens" to this. What this means is that backlinks are a ranking factor and thus affect the ability to appear on the first page of Google. It can be said that having an effective link strategy is a fundamental part of SEO.
             </p>
           `}
 
-          cardTitle2={"BRA REKOMMENDATIONER SÄLJER DIN PRODUKT LÄTTARE"}
+          cardTitle2={"GOOD RECOMMENDATIONS SELL YOUR PRODUCT EASIER"}
           cardDesc2={`
             <p class="text-sm text-white pb-5">
-            Ett exempel som går att applicera på detta är om du planerar att byta tak på ditt hus och letar efter en byggfirma som kan utföra detta. Du har två vänner, en som arbetar som snickare, och en som arbetar som revisor. De båda rekommenderar två olika byggfirmor, vems rekommendation kommer du lyssna på? Med störst sannolikhet är det snickaren eftersom denne har mer kunskap inom ämnet.
+            An example that can be applied to this is if you plan to change the roof of your house and are looking for a construction company that can carry this out. You have two friends, one who works as a carpenter, and one who works as an accountant. They both recommend two different construction companies, whose recommendation will you listen to? It is most likely the carpenter because he has more knowledge in the subject.
             </p>
             <p class="text-sm text-white pb-5">
-            Google fungerar på samma sätt, sökmotorn lyssnar på relevanta ”rekommendationer” där det finns rätt sökord som är kopplade till din verksamhet. Därför är det viktigt att se till att inlänkningarna till din hemsida är relevanta. Om du driver ett byggföretag och får länkar från en blogg som handlar om bygg och snickeri så anser Google den länken väga mer än en som kommer från en matblogg till exempel. Sökmotoroptimering handlar om att skapa eller förbättra sökresultaten för webbplatser och det görs med texter efter gedigen analys.
+            Google works the same way, the search engine listens to relevant "recommendations" where there are the right keywords that are linked to your business. Therefore, it is important to make sure that the links to your website are relevant. If you run a construction company and get links from a blog that deals with construction and carpentry, Google considers that link to have more weight than one that comes from a food blog, for example. Search engine optimization is about creating or improving search results for websites and it is done with texts after thorough analysis.
             </p>
           `}
 
@@ -607,7 +617,7 @@ export default function SEOSearchEngine() {
 
       />
       <HeadingCity
-        title={"Behöver du hjälp med sökmotoroptimering i din stad?"}
+        title={"Do you need help with search engine optimization in your city?"}
         button1={"SEO i Huddinge"}
         buttonStyle1={"pl-12 pr-12"}
         buttonLink1={"/seo-sokmotoroptimering-huddinge"}
