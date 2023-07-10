17d9da46506595eea35a748975498bd51e986606
Mattias Sikmar <mattiassikmar@Mattiass-MBP.localdomain>
2023-07-10 14:44:55 +0200

    Made linkedinAds EN non static

--- src/pages/LinkedInAdsEN.jsx
+++ src/pages/LinkedInAdsEN.jsx
@@ -8,7 +8,134 @@ export default function LinkedInAdsEN() {
   return (
     <>
     
-    <LinkedInComponent />
+     
+    <LinkedInComponent 
+      mainTitle1={"LinkedIn Ads"}
+      mainDesc1={" There are over 675 million users on LinkedIn, about 70 million of whom are in a decision-making position. This makes LinkedIn a great option for reaching millions of people in a specific industry. On top of that, LinkedIn is 277% more effective than Facebook at generating leads. Linkedin Ads is primarily targeted at B2B companies and many use it because of the platform’s audience segmentation, which is superior in terms of reaching the right person compared to similar platforms."}
+      underCard1Title={"Sociala media is the future"}
+      underCard1Desc={`
+      <p class="text-sm text-left pb-5">
+      Advertising on social media is becoming increasingly important. Marketing through social media ads can be optimised in many ways and is one of the most effective ways to reach your target audience. At Effektify, we can help you by actively working on your marketing on LinkedIn.
+      </p>
+      <p class="text-sm text-left pb-5">
+      SEO plays a major role here as well. Just like optimizing a website, keywords play an important role in optimizing your LinkedIn profile or company profile. Just like optimizing a website, keywords play an important role in optimizing your LinkedIn profile or company profile. It can then be perceived as spam, unlike if the text is written personally and with commitment.
+      </p>
+      <p class="text-sm text-left pb-5">
+      When advertising on LinkdIn, you can choose to pay per click (CPC) or per 1000 impressions (CPM). This gives you maximum control over your budget. If no one clicks on the ad, you pay nothing, just like with Google ads.
+      </p>
+      <p class="text-sm text-left pb-10">
+      There are many ways to target your LinkedIn ads. Target your message to people with a specific skill or industry. You can choose to target your ads to a specific age, gender, geographic area, selected companies in a particular industry or size of company. Your ads can also be targeted to people with a specific role, job title or skill set.
+      </p>
+    `}
+
+      mainTitle2={" Different types of LinkedIn ads"}
+      mainDesc2={`
+      <i class="fa-regular fa-square-check text-green-500 fa-2xl pb-5"></i>
+      <h1 class="text-center text-3xl pb-5 ">
+      Sponsored content
+      </h1>
+      <p class="text-sm text-left pb-5">
+      By advertising with sponsored content, LinkedIn ads show up in the flow of your targeted audience. LinkedIn marks these posts as sponsored to make it clear that they are different from regular content. Sponsored content is visible whether you use LinkedIn on mobile or desktop.
+      </p>
+      <p class="text-sm text-left pb-10">
+      The types of ads available as sponsored content are single image ads, video ads, gallery ads and event ads.
+      </p>
+  
+  
+      <i class="fa-regular fa-square-check text-green-500 fa-2xl pb-5"></i>
+      <h1 class="text-center text-3xl pb-5 ">
+      Sponsored messages
+      </h1>
+      <p class="text-sm text-left pb-5">
+      Sponsored messages allow advertisers to advertise directly to specific LinkedIn users in their inbox. It’s your chance to create a personalised and relevant message directly to your chosen audience. Sponsored messages have no limit on the number of characters.
+      </p>
+      <p class="text-sm text-left pb-10">
+      This is a great way to drive conversions tailored to the target audience in question. However, the landing page needs to be optimised, as well as mobile-friendly. Be clear about the benefits to the member of engaging with your messages.
+      </p>
+  
+  
+      <i class="fa-regular fa-square-check text-green-500 fa-2xl pb-5"></i>
+      <h1 class="text-center text-3xl pb-5 ">
+      Text ads
+      </h1>
+      <p class="text-sm text-left pb-5">
+      Text ads consist of a headline, a short text and an image. It’s possible to use click tracking, which means you can track and measure ad performance.
+      </p>
+      <p class="text-sm text-left pb-10">
+      These LinkedIn ads are also known as Text Ads and are placed on LinkedIn pages that are adapted for this type of advertising. The ads appear at the top right of the feed. However, only on desktop. This type of ad is a great option for those who want to build professional leads.
+      </p>
+  
+  
+  
+      <i class="fa-regular fa-square-check text-green-500 fa-2xl pb-5"></i>
+      <h1 class="text-center text-3xl pb-5 ">
+      Dynamic Ads
+      </h1>
+      <p class="text-sm text-left pb-5">
+      Dynamic Ads appear on the right side of LinkedIn and speak directly to the target audience by being personal.
+      </p>
+      <p class="text-sm text-left pb-10">
+      A dynamic ad pops up in a user’s feed and is based on that user’s personal information, such as their profile picture, job title and employer. This type of advertising speaks directly to the person and encourages them to follow your company, for example.
+      </p>
+      
+      `}
+
+
+      mainTitle3={"How do you go about creating LinkedIn ads?"}
+      mainDesc3={`
+      
+      <p class="text-sm text-left pb-5">
+      The first step you need to take to create LinkedIn Ads is to create a Campaign Manager account, which therefore requires your company to have a LinkedIn page. If you don’t have a LinkedIn page, it’s easy to arrange. Be careful to fill in as much as possible when creating your page, as details can help you rank higher.
+      </p>
+
+      <p class="text-sm text-left pb-5">
+      Then you need to set an ad target. On LinkedIn, you can choose from several different ones. Whether your goal with advertising is to generate more visitors to your website or create more leads, there are appropriate goals to choose.
+      </p>
+      <p class="text-sm text-left pb-5">
+      Now it’s time to choose your target audience. You can choose specializations such as industry, location, skills and education. By making these targetings, you can ensure that your ad is shown to the right people. Here you can also choose to exclude a particular type of audience if it is irrelevant to you.
+      </p>
+      <p class="text-sm text-left pb-5">
+      Once you’ve created your ads, it’s time to choose an ad format. You can choose between text ads, dynamic ads or video ads. The budget and schedule for the advertising campaign are then set. Campaign Manager gives you a suggested budget based on other ad campaigns similar to yours.
+      </p>
+      `}
+
+
+      underCard2Title={"Analyse your LinkedIn-ads"}
+      underCard2Desc={`
+      
+      <p class="text-sm text-left pb-5">
+      It is very important to analyse the results of your LinkedIn ads to see if your investment has paid off. By analysing the data from your advertising, you can easily get information on whether it is relevant to continue running the same ads or whether it is no longer profitable for your business to spend money on these particular ads. It may turn out that some LinkedIn ads work better than others and that you therefore want to focus on these only.
+      </p>
+      <p class="text-sm text-left pb-5">
+      Campaign Manager also has a feature where you can track key metrics to get an understanding of how your ads are performing. This feature includes statistics on factors such as number of clicks, impressions, cost per click, click-through rate, cost per impression, conversions, cost per conversion, potential customers and cost per lead.
+      </p>
+      <p class="text-sm text-left pb-10">
+      Based on your analytics, you can optimize your ads and continue to test to get the best possible output from your advertising.
+      </p>
+
+      `}
+
+      endTitle={"The importance of links"}
+      endDesc={`
+      <p class="text-sm text-left pb-5">
+      In digital marketing, backlinks are important. The same applies when you want to rank your LinkedIn profile higher. Getting as many backlinks as possible to your LinkedIn profile will make a big difference. The more links you have to your page, the more people will see your profile. If more people get involved, your profile will rank higher. This is a great way to raise your page’s authority and drive traffic to your profile.
+      </p>
+  
+      <p class="text-sm text-right pb-5">
+      If you’d like more information about advertising on LinkedIn, contact us at Effektify today so we can help you get started. With us, you’ll be able to reach new audiences and build your profile to look professional and generate more leads.
+      </p>
+  
+  
+      <h1 class="text-center text-xl pb-20 ">
+      Do You want to advertise on TikTok instead?
+      <br />
+      <a class="text-rose-300 underline" href="-/en/tiktok-ads">Click Here</a>
+      </h1>
+      `}
+
+    
+    />
+  
 
     <img src={gif} alt="" />
     <ContactForm 
