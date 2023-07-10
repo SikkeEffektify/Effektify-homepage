d528026f63171fa99afeeaca3fe56c11923a3749
Mattias Sikmar <mattiassikmar@Mattiass-MBP.localdomain>
2023-07-10 11:39:21 +0200

    Searchengine optimization Finished

--- src/pages/SEOSearchEngine.jsx
+++ src/pages/SEOSearchEngine.jsx
@@ -1365,75 +1365,76 @@ export default function SEOSearchEngine() {
 
 
         <h1 class="text-center text-xl pt-5 pb-5 ">
-        ”TOPPDOMÄNER FÖRBÄTTRAR RANKNINGEN”
+        "TOP DOMAINS IMPROVE THE RANKING"
         </h1>
         <p class="text-sm  pb-5">
-        En toppdomän har den högsta positionen i det hierarkiska domännamnssystemet. De flesta sökanvändare känner igen .com som ett webbplatstillägg, men kan bli förvirrade när de hittar .biz eller .guru. Detta beror delvis på att 98 % av sökresultaten när du skriver in en sökordsfras i Googles sökning är .com-domännamn.
+        A top domain has the highest position in the hierarchical domain name system. Most searchers recognize .com as a website extension, but may be confused when they find .biz or .guru. This is partly because 98% of the search results when you type a keyword phrase into Google search are .com domain names.
         </p>
         <p class="text-sm  pb-5">
-        Det enda undantaget är domäner som kan riktas till geografiska målgrupper. Om jag bor i Kanada men har ett .com-domännamn antar Google att min målgrupp är global och visar mina webbsidor i sina sökresultat för den globala målgruppen. Å andra sidan kommer en kanadensisk webbplats inom samma bransch med ett .ca-tillägg att visas främst för sökanvändare från det landet.
+        The only exception is domains that can be targeted to geographic audiences. If I live in Canada but have a .com domain name, Google assumes that my audience is global and shows my web pages in its search results for the global audience. On the other hand, a Canadian website in the same industry with a .ca extension will appear primarily to searchers from that country.
         </p>
       `}
 
-      cardTitle2={"MINNESVÄRDT OCH PROFESSIONELLT DOMÄNNAMN"}
+      cardTitle2={"MEMORABLE AND PROFESSIONAL DOMAIN NAME"}
       cardDesc2={`
         <p class="text-sm text-white pb-5">
-        År 2007, före uppdateringarna av Googles algoritmer, införde ICANN nya generiska toppdomännamn – till exempel .guru, .club, .company och .email. Men efter mer än 10 år är .com fortfarande överlägset alla dessa namn när det gäller global användning. Den viktigaste faktorn för om toppdomäner har en effekt på SEO-innehållsmarknadsföring, är om domänen innehåller nyckelord eller inte.
+        In 2007, before the updates to Google's algorithms, ICANN introduced new generic TLDs – such as .guru, .club, .company and .email. But after more than 10 years, .com is still superior to all of these names in terms of global usage. The most important factor in whether TLDs have an effect on SEO content marketing is whether or not the domain contains keywords.
         </p>
         <p class="text-sm text-white pb-5">
-        Dessa uppgifter ska inte skrämma dig, utan snarare vägleda dig. Nu vet du att när du väljer toppdomäner är det viktiga det långsiktiga varumärket och fördelarna när du registrerar ett minnesvärt och professionellt domännamn. Om du ännu inte har registrerat något domännamn rekommenderar vi dig att välja toppdomännamn (.com och .net). Äger du dock redan en webbplats ska du fokusera på att erbjuda ett enormt värde, skapa innehåll av hög kvalitet och använda sociala medier för att få kontakt med influencers som kan hjälpa dig att marknadsföra ditt innehåll. Vi har sett domännamn med konstiga tillägg göra bra ifrån sig i sökningar.
+        This information should not scare you, but rather guide you. Now you know that when choosing top-level domains, what matters is the long-term brand and the benefits of registering a memorable and professional domain name. If you have not yet registered a domain name, we recommend you choose top-level domain names (.com and .net). However, if you already own a website, you should focus on offering tremendous value, creating high-quality content, and using social media to connect with influencers who can help you promote your content. We've seen domain names with weird extensions do well in searches.
         </p>
       `}
       />
       <Heading
       bg={"bg-white"}
       textColor={"text-black"}
-      title={"ATT INKLUDERA ETT SÖKORD I SIN ANKARTEXT HAR INTE LÄNGRE NÅGON BETYDELSE FÖR SEO"}
+      title={"INCLUDING A KEYWORD IN ITS ANCHOR TEXT IS NO LONGER MEANINGFUL FOR SEO"}
       descCustomHtml={`
       <p class="text-sm text-left pb-5">
-      Ankartext är den klickbara texten i en hyperlänk. När man klickar på frasen tar den användaren till målsidan som är länkad. Många branschexperter tror att ankartext kommer att förbli en rankingfaktor för alltid, oavsett vilka förändringar som kommer med nästa Google-uppdatering.</p>
-    <p class="text-sm text-left pb-5">
-      Målet med varje uppdatering eller ändring av algoritmen är att hjälpa sökare att hitta rätt webbplatser som innehåller den information de behöver på ett användarvänligt sätt. Överoptimering kan leda till att du straffas av Google. Innan Google Penguin kom, kunde du använda sökordsrik ankartext i dina blogginlägg, både för intern länkning och extern länkning (dvs. länkning till andra webbplatser). Sökmotorerna har nu upptäckt detta trick och straffar dig.</p>
+      Anchor text is the clickable text in a hyperlink. When clicking on the phrase, it takes the user to the landing page that is linked. Many industry experts believe that anchor text will remain a ranking factor forever, no matter what changes come with the next Google update.</p>
       <p class="text-sm text-left pb-5">
-      Det finns dock en liten förändring nu. Kontextuella länkar 5-10 gånger mer rankingkraft vanliga blogglänkar som till exempel visas i dina gästinlägg, men överdriven användning av sökordsrik ankartext kan eventuellt att påverka rankningen av din webbplats negativt. Korslänkning med rik ankartext är lika riskabelt och bör därför också undvikas. Men att säga att ankartext som innehåller sökord inte längre kommer att vara relevant i sökmotorrankningen är inte korrekt, och med andra ord en myt. 
+      The goal of each update or change to the algorithm is to help searchers find the right websites that contain the information they need in a user-friendly way. Over-optimization can get you penalized by Google. Before Google Penguin came along, you could use keyword-rich anchor text in your blog posts, both for internal linking and external linking (ie linking to other websites). The search engines have now discovered this trick and are penalizing you.</p>
+      <p class="text-sm text-left pb-5">
+      However, there is a slight change now. Contextual links 5-10 times more ranking power than regular blog links that appear in your guest posts, for example, but excessive use of keyword-rich anchor text can potentially negatively affect the ranking of your site. Cross-linking with rich anchor text is equally risky and should therefore also be avoided. However, saying that anchor text containing keywords will no longer be relevant in search engine rankings is not correct, and in other words, a myth.
       </p>
       <p class="text-sm text-left pb-5">
-      Ändra ditt tillvägagångssätt. Variera ankartexten och se till att dina primära och sekundära sökord, dina meta-sökordsfraser samt ditt varumärke, din URL och generiska sökord används som ankartexter.
+      Change your approach. Vary the anchor text and make sure your primary and secondary keywords, your meta keyword phrases, and your brand, URL, and generic keywords are used as anchor texts.
       </p>
       <p class="text-sm text-left pb-5">
-      Vi gillar att bygga naturliga länkar, för det är vad Google vill ha. Du kan inte vara smartare än de som tillbringar sina arbetsdagar med att få algoritmen att fungera bättre. Fokusera istället på högkvalitativt innehåll och undvik straff från Google och andra sökmotorer. Efter Penguin-uppdateringen blev de flesta webbplatser med optimerad ankartext straffade. Å andra sidan upplevde en webbplats med diversifierad ankartext ett uppsving och endast ett fåtal av dem påverkades negativt.
+      We like to build natural links, because that's what Google wants. You can't be smarter than those who spend their working days making the algorithm work better. Instead, focus on high-quality content and avoid penalties from Google and other search engines. After the Penguin update, most sites with optimized anchor text were penalized. On the other hand, a website with diversified anchor text experienced a boost and only a few of them were negatively affected.
       </p>
       <p class="text-sm text-left pb-5">
-      Det finns flera typer av ankartext som du kan använda, men varumärkesbyggande är det enklaste sättet att sticka ut positivt i Googles ögon.
+      There are several types of anchor text you can use, but branding is the easiest way to stand out positively in Google's eyes.
       </p>
 
-      <h1 class="text-lg text-left uppercase pb-5">Du kan alltså följa den här ankartextstrategin:</h1>
-      <p class="text-base text-left pb-5">1. Naturlig ankartext</p>
-      <p class="text-sm text-left pb-5">Det kan vara en ankartext med nyckelord, varumärke eller URL. Den ska flyta naturligt med resten av innehållet. Dessa är oftast redaktionella länkar och kommer definitivt att påverka dina placeringar.</p>
+      <h1 class="text-lg text-left uppercase pb-5">So you can follow this anchor text strategy:</h1>
+      <p class="text-base text-left pb-5">1. Natural anchor text</p>
+      <p class="text-sm text-left pb-5">It can be an anchor text with keywords, brand or URL. It should flow naturally with the rest of the content. These are mostly editorial links and will definitely affect your rankings.</p>
 
-      <p class="text-base text-left pb-5">2. Ankartext för varumärket eller varumärkets URL</p>
-      <p class="text-sm text-left pb-5">Om du kan skapa mer varumärkes- eller URL-ankringstext bör du klara dig bra. Sträva efter att ha varumärkesankringstext i ungefär 90 % av dina länkar.</p>
-      <p class="text-sm text-left pb-5">Det finns tusentals nyckelord som du skulle kunna använda i sin länkprofil för att bygga länkar, utan att späda ut sin webbplatsstrategi. Istället är de flesta ankartexter som pekar på ens egna målsidor oftast antingen varumärkesnamn, varumärkes-URL:er eller rena URL:er . Du kan naturligtvis inte kontrollera vilken ankartext en besökare använder för att länka till din webbplats. Men om du har möjlighet till det, antingen genom gästbloggande eller intervjuer, bör du undvika sökordsrik ankartext. Detta är en del av digital marknadsföring. Eller ännu bättre, använd ditt varumärke + nyckelord. Du kan också använda varumärkets webbadresser som ankartext.</p>
-      
-      <p class="text-base text-left pb-5">3. Generisk ankartext</p>
-      <p class="text-sm text-left pb-5">Använd den inte i för stor utsträckning, eftersom den kan visa sig vara överoptimerad. Minst 5 % av dina ankarlänkar bör vara generiska, särskilt när du länkar till eller från en irrelevant sida. Förstå din länkprofil. Om man till exempel vill göra en hänvisning till en sportwebbplats från en blogg om digital marknadsföring är det bäst att använda generiska nyckelord.</p>
+     
+      <p class="text-base text-left pb-5">2. Anchor text for the brand or brand URL</p>
+      <p class="text-sm text-left pb-5">If you can create more branding or URL anchor text, you should be fine. Aim to have branded anchor text in about 90% of your links.</p>
+      <p class="text-sm text-left pb-5">There are thousands of keywords that you could use in your link profile to build links, without diluting your website strategy. Instead, most anchor texts pointing to one's own landing pages are usually either brand names, branded URLs, or pure URLs . Of course, you cannot control what anchor text a visitor uses to link to your site. But if you have the opportunity, either through guest blogging or interviews, you should avoid keyword-rich anchor text. This is part of digital marketing. Or better yet, use your brand + keywords. You can also use brand URLs as anchor text.</p>
 
-      <h1 class="text-lg uppercase pb-5">Du kan alltså följa den här ankartextstrategin:</h1>
+      <p class="text-base text-left pb-5">3. Generic anchor text</p>
+      <p class="text-sm text-left pb-5">Do not use it too much, as it may turn out to be over-optimized. At least 5% of your anchor links should be generic, especially when linking to or from an irrelevant page. Understand your link profile. For example, if you want to link to a sports website from a digital marketing blog, it's best to use generic keywords.</p>
+
+      <h1 class="text-lg uppercase pb-5">You can follow this anchor text strategy:</h1>
       <p class="text-base text-left pb-5">
-      Denna myt är farlig att lyssna på. När det kommer till digital marknadsföring och sökmotoroptimering har du inte råd att ignorera högkvalitativ sökordsanalys för din webbplats.
+      This myth is dangerous to listen to. When it comes to digital marketing and search engine optimization, you cannot afford to ignore high-quality keyword analysis for your website.
       </p>
       <p class="text-base text-left pb-5">
-      Vad du bör undvika är överoptimering, överdriven användning av söksordsrika ankartexter och uppdateringar av Exact Match Domain. Fokusera på ROI, istället för på topplaceringar för det sökordet. Trots allt, oavsett om du rankas på plats 1 eller 5, är det viktiga vad du får tillbaka.
+      What you should avoid is over-optimization, excessive use of keyword-rich anchor texts and Exact Match Domain updates. Focus on ROI, instead of top rankings for that keyword. After all, whether you rank 1 or 5, what matters is what you get back.
       </p>
       <p class="text-base text-left pb-5">
-      Sökordsanalys är i hög grad fortfarande relevant. Men för att vara säker bör du fokusera på användarens avsikt bakom sökningen, inte bara på själva nyckelorden, vilket även gäller meta-sökordsfraserna.
+      Keyword analysis is still very much relevant. But to be safe, you should focus on the user's intent behind the search, not just the keywords themselves, which also applies to the meta keyword phrases.
       </p>
       `}
       />
 
       <HeadingSEO9 
-      mainTitle={"För dig som behöver lokal sökmotoroptimering"}
-      listButton={"Välj din stad i listan nedan"}
+      mainTitle={"For those who need local search engine optimization"}
+      listButton={"Select your city from the list below"}
       city1={"Umeå"}
       city1Link={"/seo-sokmotoroptimering-umea/"}
       city2={"Upplands väsby"}
