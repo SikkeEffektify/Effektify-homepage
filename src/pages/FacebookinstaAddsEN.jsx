e410f287da47a412a30aa18e6db8fc9a50fc5850
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-12 15:32:48 +0200

    instaEN  nonstatic progress

--- src/pages/FacebookinstaAddsEN.jsx
+++ src/pages/FacebookinstaAddsEN.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import gif from '../assets/PONG_Light_version2.gif'
 import ContactForm from '../components/ContactForm'
+import { Heading } from '../components/Heading'
 
 export default function FacebookinstaAddsEN() {
   return (
    <>
    
+   <Heading 
+   bg={"bg-white"}
+   textColor={"text-black"}
+   title={"Facebook ads / Instagram ads"}
+   desc={" Ads Manager är verktyget för dig som vill skapa annonser på Facebook, Instagram eller Messenger. Verktyget är ett allt i ett-verktyg som används för att skapa annonser, hantera annonser (så som när och var de ska köras), samt bevaka annonskampanjerna. Ads Manager är inte bara ett effektivt verktyg för annonsering, det är också skapat för att kunna användas av annonsörer med olika kunskapsnivåer. Detta verktyg låter dig skapa effektfulla annonser på både Facebook och Instagram, och du kan alltid anpassa och förbättra innehåll. Med Ads Manager kan du enkelt skapa relevanta annonser och tillhandahålla viktig information till dina potentiella kunder."}
+   />
+
    <section>
    <div class='pt-20 heading-container bg-white text-black'>
-    <h1 class="text-left text-4xl pb-5 ">
-    Facebook ads
-    <br />
-    Instagram ads
-    </h1>
-    <p class="text-sm text-left pb-10">
-    Ads Manager är verktyget för dig som vill skapa annonser på Facebook, Instagram eller Messenger. Verktyget är ett allt i ett-verktyg som används för att skapa annonser, hantera annonser (så som när och var de ska köras), samt bevaka annonskampanjerna. Ads Manager är inte bara ett effektivt verktyg för annonsering, det är också skapat för att kunna användas av annonsörer med olika kunskapsnivåer. Detta verktyg låter dig skapa effektfulla annonser på både Facebook och Instagram, och du kan alltid anpassa och förbättra innehåll. Med Ads Manager kan du enkelt skapa relevanta annonser och tillhandahålla viktig information till dina potentiella kunder.
-    </p>
-
     <section class="cards-fb">
-       <div class="pt-5 cards-container2 pb-10">
+       <div class="pt-5 cards-container2 pb-5">
      <div class="custom-back max-w-sm pb-10 border border-black shado mx-auto pt-10">
         <h1 class="pt-40 cards2-title text-center mb-2 font-semibold tracking-tight uppercase text-rose-300"><span class="text-white"></span></h1>
         <h2 class="pb-20 text-center cards2-under-title text-white"><span class="pl-4"></span></h2>
@@ -26,64 +25,63 @@ export default function FacebookinstaAddsEN() {
     </div>
     </section>
 
-    <h1 class="text-center text-4xl pb-5 ">
-    Vad är Ads Manager?
-    </h1>
-    <p class="text-sm text-left pb-10">
-    Facebook består av olika typer av content, plattformar och appar, vilket gör det till dagens mest komplexa sociala media. Facebook Ads Manager är en del av Facebooks nätverk.
-    </p>
-    <p>
-    </p>
-
     </div>
-
    </section>
+   <Heading 
+   bg={"bg-white pb-5"}
+   textColor={"text-black"}
+   title={"Vad är Ads Manager?"}
+   desc={" Facebook består av olika typer av content, plattformar och appar, vilket gör det till dagens mest komplexa sociala media. Facebook Ads Manager är en del av Facebooks nätverk."}
+   
+   />
 
    <div class="whitedivider5 fb-custom-color  pb-20">
       </div>
 
+   <Heading 
+   bg={"fb-custom-color"}
+   textColor={"text-white"}
+   title={"Vad är skillnaden mellan inlägg, sponsrade inlägg och annonser?"}
+   descCustomHtml={`
+   
+   <p class="text-sm text-white text-left pb-10">
+   Allt som är annonser på Facebook eller Instagram märks som “Sponsrad”, och står precis under användarens namn. Men hur vet man när man ska använda Facebook Ads Manager, och vad innebär egentligen sponsrade inlägg och annonser?
+   </p>
 
+
+   <h1 class="text-left text-white text-xl ">
+   Inlägg
+   </h1>
+
+   <p class="text-sm text-white text-left pb-10">
+   Vanliga, traditionella inlägg som inte har pushats genom betald marknadsföring. Dessa kallas även ibland för organiskt innehåll eller organiskt inlägg för att klargöra att det inte är sponsrat.
+   </p>
+
+
+
+   <h1 class="text-left text-white text-xl ">
+   Sponsrade Inlägg
+   </h1>
+
+   <p class="text-sm text-white text-left pb-10">
+   Ett sponsrat inlägg publiceras som ett vanligt inlägg men till skillnad från ett vanligt inlägg så finns det en annonsbudget skapad för inlägget för att kunna ge det en extra push.
+   </p>
+
+
+   <h1 class="text-left text-white text-xl ">
+   Annonser
+   </h1>
+
+   <p class="text-sm text-white  text-left pb-10">
+   Annonser utgår inte från flödet på Facebook eller Instagram till skillnad från vanliga inlägg eller sponsrade inlägg. Istället skapas specifikt innehåll för annonsen i fråga i Ads Manager, vilket innebär att annonser utgår helt från annonsbudgeten och får ingen organisk spridning.
+   </p>
+   <hr class="my-12 h-0.5 border-t-0 bg-white opacity-100 dark:opacity-50" />
+   `}
+   />
 
     <section>
-    <div class='pt-20 heading-container fb-custom-color text-white'>
-    <h1 class="text-left text-2xl pb-5 ">
-    Vad är skillnaden mellan inlägg, sponsrade inlägg och annonser?
-    </h1>
-
-    <p class="text-sm text-left pb-10">
-    Allt som är annonser på Facebook eller Instagram märks som “Sponsrad”, och står precis under användarens namn. Men hur vet man när man ska använda Facebook Ads Manager, och vad innebär egentligen sponsrade inlägg och annonser?
-    </p>
-
-
-    <h1 class="text-left text-xl ">
-    Inlägg
-    </h1>
-
-    <p class="text-sm text-left pb-10">
-    Vanliga, traditionella inlägg som inte har pushats genom betald marknadsföring. Dessa kallas även ibland för organiskt innehåll eller organiskt inlägg för att klargöra att det inte är sponsrat.
-    </p>
-
-
-
-    <h1 class="text-left text-xl ">
-    Sponsrade Inlägg
-    </h1>
-
-    <p class="text-sm text-left pb-10">
-    Ett sponsrat inlägg publiceras som ett vanligt inlägg men till skillnad från ett vanligt inlägg så finns det en annonsbudget skapad för inlägget för att kunna ge det en extra push.
-    </p>
-
-
-    <h1 class="text-left text-xl ">
-    Annonser
-    </h1>
-
-    <p class="text-sm text-left pb-10">
-    Annonser utgår inte från flödet på Facebook eller Instagram till skillnad från vanliga inlägg eller sponsrade inlägg. Istället skapas specifikt innehåll för annonsen i fråga i Ads Manager, vilket innebär att annonser utgår helt från annonsbudgeten och får ingen organisk spridning.
-    </p>
-
-    <hr class="my-12 h-0.5 border-t-0 bg-white opacity-100 dark:opacity-50" />
-
+    <div class='pt-5 heading-container fb-custom-color text-white'>
+    
 
     <section class="cards-fb2">
        <div class="pt-5 cards-container2 pb-10">
@@ -94,43 +92,46 @@ export default function FacebookinstaAddsEN() {
     </div>
     </section>
 
-
-    <h1 class="text-left text-2xl ">
-    Skapa annonskampanjer
-    <br />
-    med Ads Manager
-    </h1>
-
-    <p class="text-sm text-left pb-5">
-    Med Ads Manager har du möjligheten att välja bland olika kampanjparametrar så som målgrupp och målsättning, du kan även hantera de kreativa delarna av arbetet i verktyget, samt sköta saker som att skriva annonstexter, ladda upp annonsinnehåll och se över hur dina annonser ser ut på olika enheter så som mobil, data och surfplatta.   
-    </p>
-    <p class="text-sm text-left pb-5">
-    Med Ads Manager kan du också redigera dina kampanjer enkelt, du kan redigera budgeten, målgruppen och innehållet. Du har även möjligheten att pausa, kopiera och starta om kampanjer vid behov. I Ads Manager finner du även ett rapporteringsverktyg som du kan använda för att se över huruvida dina annonser är lämpliga för det avsedda syftet. Med rapporteringsverktyget kan du se trender över tid och på så vis identifiera områden du behöver anpassa och förbättra. Innehåll, bild, budget eller målgrupp är exempel på detta. Detta görs för att sedan kunna förbättra resultatet av kampanjen.   
-    </p>
-
     </div>
     </section>
     
+    <Heading 
+      bg={"fb-custom-color"}
+      textColor={"text-white"}
+      title={"Skapa annonskampanjer med Ads Manager"}
+      descCustomHtml={`
+      <p class="text-sm text-white text-left pb-5">
+      Med Ads Manager har du möjligheten att välja bland olika kampanjparametrar så som målgrupp och målsättning, du kan även hantera de kreativa delarna av arbetet i verktyget, samt sköta saker som att skriva annonstexter, ladda upp annonsinnehåll och se över hur dina annonser ser ut på olika enheter så som mobil, data och surfplatta.   
+      </p>
+      <p class="text-sm text-white text-left pb-5">
+      Med Ads Manager kan du också redigera dina kampanjer enkelt, du kan redigera budgeten, målgruppen och innehållet. Du har även möjligheten att pausa, kopiera och starta om kampanjer vid behov. I Ads Manager finner du även ett rapporteringsverktyg som du kan använda för att se över huruvida dina annonser är lämpliga för det avsedda syftet. Med rapporteringsverktyget kan du se trender över tid och på så vis identifiera områden du behöver anpassa och förbättra. Innehåll, bild, budget eller målgrupp är exempel på detta. Detta görs för att sedan kunna förbättra resultatet av kampanjen.   
+      </p>
+      `}
+    />
+
+
     <div class="blacktopdivider2 fb-custom-color pb-20">
     </div>
 
-    <section>
-   <div class='pt-20 heading-container text-white'>
-    <h1 class="text-left text-4xl pb-5 ">
-    När bör Ads
-    <br />
-    Manager användas?
-    </h1>
-    <p class="text-sm text-left pb-5">
-    Ads Manager är lämpligt att använda du vill kunna duplicera dina annonser, Ad Set och dina annons-kampanjer snabbt. Verktyget är även passande för saker som att redigera annonsinställningar, se över alla viktiga mått och analysera resultat. Ytterligare saker som Facebook Ads Manager är bra att använda till är göra ändringar baserade på de analyser man redan gjort, det är även bra när man vill anpassa diagrammen och tabellerna till de mått som är viktigast för en själv. Använd Ads Manager för att skapa relevanta annonser och tillhandahålla den information du vill förmedla till den aktuella målgruppen.   
-    </p>
+    <Heading 
+     textColor={"text-white"}
+     title={"När bör Ads  Manager användas?"}
+     descCustomHtml={`
+     <p class="text-sm text-white text-left pb-5">
+     Ads Manager är lämpligt att använda du vill kunna duplicera dina annonser, Ad Set och dina annons-kampanjer snabbt. Verktyget är även passande för saker som att redigera annonsinställningar, se över alla viktiga mått och analysera resultat. Ytterligare saker som Facebook Ads Manager är bra att använda till är göra ändringar baserade på de analyser man redan gjort, det är även bra när man vill anpassa diagrammen och tabellerna till de mått som är viktigast för en själv. Använd Ads Manager för att skapa relevanta annonser och tillhandahålla den information du vill förmedla till den aktuella målgruppen.   
+     </p>
+ 
+     <h1 class="text-left text-white text-2xl pb-5 ">
+     Att sätta en budget för annonskampanjen
+     </h1>
+     <p class="text-sm text-white text-left pb-5">
+     När du ska skapa budgeten för din annonskampanj kan du välja antingen budgeten “daglig budget” eller budgeten “livstid”. Daglig budget är passande om du vill att din kampanj ska vara fortlöpande och om du vill ha möjligheten att justera budgeten då och då. Denna möjlighet är bra om du driver ett företag som säljer säsongsrelaterade produkter eller tjänster och vill öka budgeten under högsäsong, och minska alternativt pausa när det inte är säsong. Livstid, däremot, är passande för de företag som vill att kampanjen ska vara aktiv under en begränsad tidsperiod, exempelvis samtidigt som företaget har en kampanj i butik eller om man vill ge ett evenemang en push.
+     </p>
+     `}
+    />
 
-    <h1 class="text-left text-2xl pb-5 ">
-    Att sätta en budget för annonskampanjen
-    </h1>
-    <p class="text-sm text-left pb-5">
-    När du ska skapa budgeten för din annonskampanj kan du välja antingen budgeten “daglig budget” eller budgeten “livstid”. Daglig budget är passande om du vill att din kampanj ska vara fortlöpande och om du vill ha möjligheten att justera budgeten då och då. Denna möjlighet är bra om du driver ett företag som säljer säsongsrelaterade produkter eller tjänster och vill öka budgeten under högsäsong, och minska alternativt pausa när det inte är säsong. Livstid, däremot, är passande för de företag som vill att kampanjen ska vara aktiv under en begränsad tidsperiod, exempelvis samtidigt som företaget har en kampanj i butik eller om man vill ge ett evenemang en push.
-    </p>
+    <section>
+   <div class='heading-container text-white'>
 
     <section class="cards-fb2">
        <div class="pt-5 cards-container2 pb-10">
@@ -144,15 +145,23 @@ export default function FacebookinstaAddsEN() {
     </section>
 
     <img src={gif} alt="" />
-    <ContactForm />
+    <ContactForm 
+    title={"Vad kul att du har hittat till oss!"}
+    desc={"Har du frågor eller är redo att starta din digitala resa redan nu, är du välkommen att fylla i några rader eller maila oss på "}
+    company={"Företag"}
+    name={"Namn"}
+    phone={"Telefon"}
+    email={"E-post"}
+    text={"Vad kan vi hjälpa dig med?"}
+    buttonText={"Skicka"}
+  />
 
-
-    <section>
-   <div class='pt-20 heading-container bg-white text-black'>
-    <h1 class="text-left text-3xl pb-5 ">
-    Hur stor bör budgeten för en annonskampanj vara?   
-    </h1>
-    <p class="text-sm text-left pb-5">
+   <Heading 
+   bg={"bg-white"}
+   textColor={"text-black"}
+   title={"Hur stor bör budgeten för en annonskampanj vara?"}
+   descCustomHtml={`
+   <p class="text-sm text-left pb-5">
     Vad gäller storleken på budgeten så ger en större budget större synlighet, bortsett från utformningen av annonskampanjen som också är en påverkande faktor. Ju bättre annonserna är utformade, desto bättre räckvidd får du för varje krona du betalar.   
     </p>
     <p class="text-sm text-left pb-10">
@@ -210,7 +219,7 @@ export default function FacebookinstaAddsEN() {
     </section>
 
     <section>
-    <div class='pt-20 heading-container bg-neutral-100 text-black'>
+    <div class='pt-5  text-black'>
     <h1 class="text-left text-3xl pb-5 ">
     Steg 2:
     <br />
@@ -238,19 +247,21 @@ export default function FacebookinstaAddsEN() {
     <p class="text-sm text-left pb-5">
     Det första steget som ska göras när man vill skapa en annonskampanj är att välja ett kampanjmål. När du väljer mål för din kampanj hjälper Facebook dig att både skapa en lämplig annons utifrån målet och att justera budgeten så att den är lagom, och så att du på så vis når ditt önskade resultat.
     </p>
-    <p class="text-sm text-left pb-20">
+    <p class="text-sm text-left pb-0">
     Målen är uppdelade i tre olika kategorier, medvetenhet, övervägande och konvertering.  
     </p>
 
-    <h1 class="text-center text-xl pb-20 ">
+    <h1 class="text-center text-xl pt-10 pb-10 ">
     Vill du istället annonsera på LinkedIn?
     <br />
     <a class="text-rose-300 underline" href="/linkedin-ads">Klicka Här</a>
     </h1>
 
 
-    </div>
-    </section>
+   `}
+   
+   />
+
    </>
   )
 }
