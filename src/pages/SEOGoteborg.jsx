ad4699d01618a7777e223fff8df3e992ba8a5300
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-14 14:54:07 +0200

    seogoteborg

--- src/pages/SEOGoteborg.jsx
+++ src/pages/SEOGoteborg.jsx
@@ -1,31 +1,23 @@
 import React from 'react'
-import bg from '../assets/SEO-goteborg.webp'
+import background from '../assets/SEO-goteborg.webp'
 import bg2 from '../assets/seo-google-my-business.webp'
 import stats2 from '../assets/Sökmotoroptimering-seo-1.webp'
 import ContactForm from '../components/ContactForm'
+import SEOBiggerComponent from '../components/SEOBiggercomponent'
 
 
 
 export default function SEOGoteborg() {
   return (
     <>
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/60">
-        <h1 class='text-5xl pl-4 text-left uppercase text-white pt-10 pb-10'>Seo Göteborg<span class="text-rose-300">.</span></h1>
-        <p class='text-left text-lg text-white'>
-        Behöver du hjälp med SEO i Göteborg? SEO är komplicerat, därför finns vi! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Göteborg kan vi hjälpa dig synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO. Hör av dig till oss för hjälp med att öka din trafik online och förbättrad synlighet på sökmotorer.
-        </p>   
-    </div>
-   </div>
-    </section>
 
-    <section>
-    <div class='pt-20 heading-container text-center text-white '>
-    <h1 class="text-2xl uppercase pb-10">
-    Göteborg – sökmotoroptimering
-    </h1>
+    <SEOBiggerComponent 
+    bg={background}
+    mainTitle1={"Seo Göteborg"}
+    mainDesc1={"   Behöver du hjälp med SEO i Göteborg? SEO är komplicerat, därför finns vi! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Göteborg kan vi hjälpa dig synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO. Hör av dig till oss för hjälp med att öka din trafik online och förbättrad synlighet på sökmotorer."}
+    
+    mainTitle2={"Göteborg – sökmotoroptimering"}
+    mainDesc2={`
     <p class="text-left pb-10 text-sm">
     Det är ingen nyhet att synlighet skapar möjligheter för företag – både genom traditionell marknadsföring och digital marknadsföring. Att öka din synlighet online ökar också dina chanser att få fler besökare till din hemsida, och därmed fler kunder som sedan genererar större försäljning. Det finns många sätt att använda sig av digital marknadsföring på, och Effektify är inte enbart en SEO-byrå, vi hjälper våra kunder med allt inom digital marknadsföring, både i Göteborg och resterande Sverige.
     </p>
@@ -35,32 +27,18 @@ export default function SEOGoteborg() {
     – vi 
     hjälper dig att lyckas på Google.
     </p>
-    <section class="cards-seo">
-       <div class="pt-5 cards-container2 pb-10">
-     <div class="custom-back max-w-sm pb-10 border border-black shado  mx-auto  pt-10">
-        <h1 class="pt-40 cards2-title text-center mb-2 font-semibold tracking-tight uppercase text-rose-300"><span class="text-white"></span></h1>
-        <h2 class="pb-20 text-center cards2-under-title text-white"><span class="pl-4"></span></h2>
-    </div>
-    </div>
+    `}
 
-    <div class='pt-10 heading-container text-center text-white '>
-    <h1 class="text-2xl uppercase pb-10">
-    Hur fungerar
-    <br />
-    SEO i Göteborg?
-    </h1>
+    mainTitle3={"Hur fungerar SEO i Göteborg?"}
+    mainDesc3={`
     <p class="text-left pb-10 text-sm">
     <a class="text-rose-300" href="/seo-sokmotoroptimering/">SEO </a>
     står för det engelska uttrycket Search Engine Optimization, vilket översatt till svenska är sökmotoroptimering. SEO består av många olika delar och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på tjänster eller produkter som du erbjuder. Enkelt förklarat innebär SEO att göra din webbplats synlig på Google och andra sökmotorer. Syftet med SEO är att synas så högt upp på Google (och andra sökmotorer) som möjligt på sökord som är relevanta för dig och ditt företag. Google analyserar faktorer så som huruvida din webbplats är relevant och hur den är uppbyggd, hur många, samt vilka som länkar till din webbplats och det innehåll du har på din webbplats för att sedan avgöra vilken placering du hamnar på.
     </p>
+    `}
 
-
-    <h1 class="text-3xl  uppercase pb-10">
-    Sökmotor
-    <br /> 
-    optimering
-    </h1>
-
+    mainTitle4={"Sökmotor Optimering"}
+    mainDesc4={`
     <p class="text-left pb-5 text-base">
     SEO består huvudsakligen av tre delar, analys av sökord, on-page och off-page. En analys av sökord är viktigt för att kunna finna de specifika sökfraser som är relevanta för dig och som gynnar dig och din verksamhet. Vid denna typ av analys kollar man på konkurrenter, antal sökningar på sökordet i fråga, sökintentionen m.m. 
     </p>
@@ -71,110 +49,74 @@ export default function SEOGoteborg() {
     <p class="text-center pb-20 text-2xl">
     SEO består huvudsakligen av tre delar; en analys av sökord, on-page och off-page optimering.
     </p>
+    `}
 
-    <div class=' heading-container text-center text-white pb-20'>
+    mainTitle5={"Google utvecklas ständigt – därför måste också SEO göra det"}
+    mainDesc5={`
     <h1 class="text-3xl uppercase pb-10">
     <a href="/seo-sokmotoroptimering/">Vill du veta mer om SEO?</a>
     </h1>
     <a class="vidButton" href='/seo-sokmotoroptimering/'>Klicka Här</a>
-    </div>
+    <p class="text-left pb-10 pt-10 text-sm">
+    Det är ett faktum att företag som satsar på SEO ser en positiv skillnad på resultaten efter att denna strategi har implementerats och efter att man börjat optimera hemsidan i fråga. Arbetet med sökmotoroptimering utvecklas och ändras ständigt eftersom Google gör det, sökmotorn uppdateras ofta och därför är det av stor vikt att hänga med. Detta innebär att även företag som arbetar med sökmotoroptimering bör se över sin strategi regelbundet för att fortsätta vara aktuella. Vi på Effektify anser det vara av stor betydelse att hålla oss uppdaterade och att ständigt utvecklas inom sökmotoroptimering för att kunna erbjuda våra kunder bästa möjliga service.
+    </p>
 
-    </div>
-    </section>
-
-    </div>
-    </section>
-    <div class="whitedivider3  pb-20">
-      
-      </div>
-
-      <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-      <h1 class="text-2xl text-right uppercase pb-10">
-      Google utvecklas ständigt
-      <br />
-      – därför måste också SEO göra det
-      </h1>
-      <p class="text-left pb-10 text-sm">
-      Det är ett faktum att företag som satsar på SEO ser en positiv skillnad på resultaten efter att denna strategi har implementerats och efter att man börjat optimera hemsidan i fråga. Arbetet med sökmotoroptimering utvecklas och ändras ständigt eftersom Google gör det, sökmotorn uppdateras ofta och därför är det av stor vikt att hänga med. Detta innebär att även företag som arbetar med sökmotoroptimering bör se över sin strategi regelbundet för att fortsätta vara aktuella. Vi på Effektify anser det vara av stor betydelse att hålla oss uppdaterade och att ständigt utvecklas inom sökmotoroptimering för att kunna erbjuda våra kunder bästa möjliga service.
-      </p>
-
-      <h1 class="text-2xl text-left uppercase pb-10">
-      Hur arbetar vi på Effektify med SEO?
-      </h1>
-      <p class="text-left pb-5 text-sm">
-      Vi har lång erfarenhet och stor kompetens inom SEO och har därför utvecklat en SEO-strategi som vi är säkra på kommer hjälpa dig att nå dina uppsatta mål. Vårt SEO-arbete är en omfattande process och bygger på väl genomtänkta steg.
-      </p>
-      <p class="text-left pb-5 text-sm">
-      Det första steget inför SEO-arbetet är att analysera hemsidan ifråga för att se över vad för typ av optimering som krävs. Därefter görs en sökordsanalys för att se vilka sökord du har möjlighet att ranka på samt vilka du redan syns på. Detta inkluderar att se över sökresultatet för sökorden för att avgöra huruvida sökintentionen är rätt, se över antal sökningar per månad för att avgöra huruvida det är ett sökord som kommer generera en ökning i din försäljning, samt en konkurrensanalys för att avgöra om du har möjlighet att ranka högre än dina konkurrenter. Därefter bokar vi ett uppstartsmöte med dig där vi diskuterar fram vilka sökord som är mest relevanta för dig och din hemsida. Vi bestämmer även vad som bör åtgärdas på din hemsida för att vi ska kunna utföra vårt arbete med off-page senare, ofta är det innehåll som behöver förbättras, vilket är något vi också kan hjälpa till med.
-      </p>
-      <p class="text-left pb-5 text-sm">
-      Efter att detta är gjort börjar själva arbetet med SEO utifrån vad vi ihop har bestämt arbetar vi med on-page och off-page. Arbetet kan inkludera allt från skriva texter till din hemsida och hjälpa dig med meta-titlar och meta-rubriker, till att skicka länkar till din hemsida för att ge den en bättre plats på Googles sökresultat.
-      </p>
-      <p class="text-left pb-5 text-sm">
-      Vi värderar ett långsiktigt samarbete högt och därför är vi helt transparenta. Varje månad får du en rapport från oss för att kunna se vilken plats dina sökord har på Googles organiska sökresultat.
-      </p>
-      <p class="text-left pb-10 text-sm">
-      Resultatet av detta SEO-arbete är att dina utvalda sökord syns bättre i det organiska sökresultatet och därmed bidrar till ökad trafik till din hemsida, det vill säga fler besökare, vilket i sin tur innebär ökad försäljning.
-      </p>  
+    <h1 class="text-2xl text-left uppercase pb-10">
+    Hur arbetar vi på Effektify med SEO?
+    </h1>
+    <p class="text-left pb-5 text-sm">
+    Vi har lång erfarenhet och stor kompetens inom SEO och har därför utvecklat en SEO-strategi som vi är säkra på kommer hjälpa dig att nå dina uppsatta mål. Vårt SEO-arbete är en omfattande process och bygger på väl genomtänkta steg.
+    </p>
+    <p class="text-left pb-5 text-sm">
+    Det första steget inför SEO-arbetet är att analysera hemsidan ifråga för att se över vad för typ av optimering som krävs. Därefter görs en sökordsanalys för att se vilka sökord du har möjlighet att ranka på samt vilka du redan syns på. Detta inkluderar att se över sökresultatet för sökorden för att avgöra huruvida sökintentionen är rätt, se över antal sökningar per månad för att avgöra huruvida det är ett sökord som kommer generera en ökning i din försäljning, samt en konkurrensanalys för att avgöra om du har möjlighet att ranka högre än dina konkurrenter. Därefter bokar vi ett uppstartsmöte med dig där vi diskuterar fram vilka sökord som är mest relevanta för dig och din hemsida. Vi bestämmer även vad som bör åtgärdas på din hemsida för att vi ska kunna utföra vårt arbete med off-page senare, ofta är det innehåll som behöver förbättras, vilket är något vi också kan hjälpa till med.
+    </p>
+    <p class="text-left pb-5 text-sm">
+    Efter att detta är gjort börjar själva arbetet med SEO utifrån vad vi ihop har bestämt arbetar vi med on-page och off-page. Arbetet kan inkludera allt från skriva texter till din hemsida och hjälpa dig med meta-titlar och meta-rubriker, till att skicka länkar till din hemsida för att ge den en bättre plats på Googles sökresultat.
+    </p>
+    <p class="text-left pb-5 text-sm">
+    Vi värderar ett långsiktigt samarbete högt och därför är vi helt transparenta. Varje månad får du en rapport från oss för att kunna se vilken plats dina sökord har på Googles organiska sökresultat.
+    </p>
+    <p class="text-left pb-10 text-sm">
+    Resultatet av detta SEO-arbete är att dina utvalda sökord syns bättre i det organiska sökresultatet och därmed bidrar till ökad trafik till din hemsida, det vill säga fler besökare, vilket i sin tur innebär ökad försäljning.
+    </p>  
 
 
-      <h1 class="text-2xl uppercase pb-10">
-      Lokal SEO – Göteborg
-      </h1>
-      <p class="text-left pb-5 text-sm">
-      Lokal SEO, eller lokal sökmotoroptimering som det också kallas, skiljer sig något från SEO på nationell nivå. Den största skillnaden är konkurrensen, den är betydligt större på nationell nivå. För många branscher är det relevant att enbart synas lokalt online då deras tjänster eller produkter är avsedda att säljas lokalt. Om du exempelvis behöver hjälp av en rörmokare så kommer du troligtvis söka på “rörmokare” plus din stad eftersom det är vad som är relevant för dig.
-      </p>
-      <p class="text-left pb-20 text-sm">
-      En viktig del av lokal SEO är att tydliggöra var du befinner dig och i vilka orter du verkar på din hemsida, både för dina kunders skull, men också för Google så att sökmotorn anser din hemsida vara relevant. För att lyckas med lokal SEO behövs det även tas hänsyn till vissa tekniska åtgärder bakom hemsidan. Detta gör det lättare för Google att begripa vilka geografiska områden sökmotorn ska förknippa dig och ditt företag med.
-      </p>
+    <h1 class="text-2xl uppercase pb-10">
+    Lokal SEO – Göteborg
+    </h1>
+    <p class="text-left pb-5 text-sm">
+    Lokal SEO, eller lokal sökmotoroptimering som det också kallas, skiljer sig något från SEO på nationell nivå. Den största skillnaden är konkurrensen, den är betydligt större på nationell nivå. För många branscher är det relevant att enbart synas lokalt online då deras tjänster eller produkter är avsedda att säljas lokalt. Om du exempelvis behöver hjälp av en rörmokare så kommer du troligtvis söka på “rörmokare” plus din stad eftersom det är vad som är relevant för dig.
+    </p>
+    <p class="text-left pb-20 text-sm">
+    En viktig del av lokal SEO är att tydliggöra var du befinner dig och i vilka orter du verkar på din hemsida, både för dina kunders skull, men också för Google så att sökmotorn anser din hemsida vara relevant. För att lyckas med lokal SEO behövs det även tas hänsyn till vissa tekniska åtgärder bakom hemsidan. Detta gör det lättare för Google att begripa vilka geografiska områden sökmotorn ska förknippa dig och ditt företag med.
+    </p>
 
+    `}
 
-      <section class="cards-seo2">
-       <div class="pt-5 cards-container2 pb-10">
-     <div class="custom-back max-w-sm pb-10 border border-black shado mx-auto pt-10">
-        <h1 class="pt-40 cards2-title text-center mb-2 font-semibold tracking-tight uppercase text-rose-300"><span class="text-white"></span></h1>
-        <h2 class="pb-20 text-center cards2-under-title text-white"><span class="pl-4"></span></h2>
-    </div>
-    </div>
-    </section>
+    mainTitle8={"Google My  Business (GMB)"}
+    mainDesc8={`
+    <p class='text-left pb-5 text-base text-white'>
+    Om du driver ett lokalt företag som verkar inom ett avgränsat geografiskt område är det av stor vikt för dig att finnas på Google My Business. Det är viktigt då Google My Business är det resultat som Google visar om du söker efter lokala företag. Google visar det som en lista av de olika relevanta lokala företagen ovanför de organiska sökresultaten.
+    </p>   
+    `}
 
-    <hr class="my-12 h-0.5 border-t-0 bg-black opacity-100 dark:opacity-50" />
-    
-    </div>
-    </section>
+    mainTitle9={" Om Göteborg"}
+    mainDesc9={`
+    <p class='text-left pb-5 text-base text-white'>
+    Göteborg är Sveriges näst största tätort, efter vår huvudstad Stockholm. Det bor 580 000 invånare i Göteborg där 27 % av invånarna är utrikesfödda. Detta är märkbart om man ser till stadens näringsliv, många stora företag i Göteborg ägs av internationella aktörer samt så finns det många lokala företag som är världsledande inom sin bransch. Göteborg fick sina stadsprivilegier år 1621 efter att Gustaf II Adolf tog initiativet att anlägga staden. Göteborg byggdes av tyskar och holländare och utvecklades under 1700-talet till en sjöfarts- och handelsstad. Under 1800- och 1900-talet utvecklades Göteborg till en industristad med hjälp av teknik och kompetens från England och Skottland. Även ekonomin expanderade med hjälp av arbetskraftsinvandring från länder som Italien, Grekland, Forna Jugoslavien och Finland. Göteborg ligger på Västkusten och är en populär stad att besöka året om på grund av allt staden har att erbjuda. På sommaren är Liseberg, norra Europas största nöjespark, populär att åka till. Utöver det så är Göteborg fullt av mysiga caféer och restauranger, museer och fin natur. Har du ett företag i Göteborg och vill öka din trafik online? Vi på Effektify hjälper dig gärna! Vi hjälper företag i hela Sverige att förbättra sina resultat och uppnå sina mål genom digital marknadsföring, våra tjänster inkluderar allt från SEO och Google Ads till att bygga hemsidor.
+    </p>`}
+    />
 
-
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg2} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/80">
-        <h1 class='text-5xl pl-4 text-left uppercase text-white pt-40 pb-10'>
-        Google My
-        <br />
-        Business (GMB)
-        </h1>
-        <p class='text-left pb-5 text-base text-white'>
-        Om du driver ett lokalt företag som verkar inom ett avgränsat geografiskt område är det av stor vikt för dig att finnas på Google My Business. Det är viktigt då Google My Business är det resultat som Google visar om du söker efter lokala företag. Google visar det som en lista av de olika relevanta lokala företagen ovanför de organiska sökresultaten.
-        </p>   
-    </div>
-   </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-white'>
-      <h1 class="text-2xl uppercase pb-10">
-      Om Göteborg
-      </h1>
-      <p class='text-left pb-5 text-base text-white'>
-      Göteborg är Sveriges näst största tätort, efter vår huvudstad Stockholm. Det bor 580 000 invånare i Göteborg där 27 % av invånarna är utrikesfödda. Detta är märkbart om man ser till stadens näringsliv, många stora företag i Göteborg ägs av internationella aktörer samt så finns det många lokala företag som är världsledande inom sin bransch. Göteborg fick sina stadsprivilegier år 1621 efter att Gustaf II Adolf tog initiativet att anlägga staden. Göteborg byggdes av tyskar och holländare och utvecklades under 1700-talet till en sjöfarts- och handelsstad. Under 1800- och 1900-talet utvecklades Göteborg till en industristad med hjälp av teknik och kompetens från England och Skottland. Även ekonomin expanderade med hjälp av arbetskraftsinvandring från länder som Italien, Grekland, Forna Jugoslavien och Finland. Göteborg ligger på Västkusten och är en populär stad att besöka året om på grund av allt staden har att erbjuda. På sommaren är Liseberg, norra Europas största nöjespark, populär att åka till. Utöver det så är Göteborg fullt av mysiga caféer och restauranger, museer och fin natur. Har du ett företag i Göteborg och vill öka din trafik online? Vi på Effektify hjälper dig gärna! Vi hjälper företag i hela Sverige att förbättra sina resultat och uppnå sina mål genom digital marknadsföring, våra tjänster inkluderar allt från SEO och Google Ads till att bygga hemsidor.
-      </p>
-    </div>
-    </section>
-
-    <HeadingCity3/>
-
-    <ContactForm />
+<ContactForm 
+    title={"Vad kul att du har hittat till oss!"}
+    desc={"Har du frågor eller är redo att starta din digitala resa redan nu, är du välkommen att fylla i några rader eller maila oss på "}
+    company={"Företag"}
+    name={"Namn"}
+    phone={"Telefon"}
+    email={"E-post"}
+    text={"Vad kan vi hjälpa dig med?"}
+    buttonText={"Skicka"}
+  />
 
     </>
 
