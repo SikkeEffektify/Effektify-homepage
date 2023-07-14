9bc61a8648a229119a3979334aa53b7fcf9f4cdd
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-14 13:37:52 +0200

    SEO stockholm

--- src/pages/SEOStockholm.jsx
+++ src/pages/SEOStockholm.jsx
@@ -1,61 +1,44 @@
 import React from 'react'
-import bg from '../assets/SEO-stockholm.webp'
+import background from '../assets/SEO-stockholm.webp'
 import bg2 from '../assets/seo-google-my-business.webp'
 import stats2 from '../assets/Sökmotoroptimering-seo-1.webp'
 import ContactForm from '../components/ContactForm'
+import SEOBiggerComponent from '../components/SEOBiggercomponent'
+
 
 
 
 export default function SEOStockholm() {
   return (
     <>
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/60">
-        <h1 class='text-5xl pl-4 text-left uppercase text-white pt-10 pb-10'>Seo Stockholm<span class="text-rose-300">.</span></h1>
-        <p class='text-left text-lg text-white'>
-        Behöver du hjälp med SEO i Stockholm? Låt oss ta den rollen! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Stockholm kan vi hjälpa dig synas för dina potentiella kunder som söker efter de produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO och erbjuder våra tjänster till alla som vill ranka högt på Googles organiska sökresultat eller behöver hjälp med annonsering genom Google Ads eller sociala medier. 
-        </p>   
-    </div>
-   </div>
-    </section>
 
-    <section>
-    <div class='pt-20 heading-container text-center text-white '>
-    <h1 class="text-2xl uppercase pb-10">
-    Stockholm – sökmotoroptimering
-    </h1>
+    <SEOBiggerComponent
+    bg={background}
+    mainTitle1={"Seo Stockholm"}
+    mainDesc1={" Behöver du hjälp med SEO i Stockholm? Låt oss ta den rollen! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Stockholm kan vi hjälpa dig synas för dina potentiella kunder som söker efter de produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO och erbjuder våra tjänster till alla som vill ranka högt på Googles organiska sökresultat eller behöver hjälp med annonsering genom Google Ads eller sociala medier. "}
+    
+    mainTitle2={"Stockholm – sökmotoroptimering"}
+    mainDesc2={`
     <p class="text-left pb-5 text-sm">
     Det är ingen nyhet att synlighet skapar möjligheter för företag – både genom traditionell marknadsföring och digital marknadsföring. Att öka din synlighet online ökar också dina chanser att få ökad trafik från fler besökare till din hemsida, och därmed få fler kunder som leder till ökad försäljning. Det finns många sätt att använda sig av digital marknadsföring men det måste göras på rätt sätt för att det ska effekt.
     </p>
     <p class="text-left pb-5 text-sm">
     Effektify är inte enbart en SEO-byrå. Vi hjälper våra kunder med allt inom digital marknadsföring, både i Stockholm och i resten av Sverige, och har bred kunskap när det kommer till sökmotoroptimering/SEO och annonsering med Google Ads. Här ser vi sökmotoroptimeringen som ett sätt att ge alla samma förutsättningar, att lyckas uppnå drömmen av att driva en framgångsrik verksamhet.
     </p>
-    <section class="cards-seo">
-       <div class="pt-5 cards-container2 pb-10">
-     <div class="custom-back max-w-sm pb-10 border border-black shado  mx-auto  pt-10">
-        <h1 class="pt-40 cards2-title text-center mb-2 font-semibold tracking-tight uppercase text-rose-300"><span class="text-white"></span></h1>
-        <h2 class="pb-20 text-center cards2-under-title text-white"><span class="pl-4"></span></h2>
-    </div>
-    </div>
+    `}
 
-    <div class='pt-10 heading-container text-center text-white '>
-    <h1 class="text-2xl uppercase pb-10">
-    Hur fungerar SEO i Stockholm?
-    </h1>
+    mainTitle3={"Hur fungerar SEO i Stockholm?"}
+    mainDesc3={`
     <p class="text-left pb-10 text-sm">
     <a class="text-rose-300" href="/seo-sokmotoroptimering/">SEO </a>
     kommer från det engelska uttrycket Search Engine Optimization, vilket direkt översatt till svenska betyder sökmotoroptimering. SEO handlar i grund och botten om att optimera din hemsida för Googles ranking och består av oerhört många olika delar och är en väldigt viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sina sökningar på just de tjänster eller produkter som du erbjuder.
     </p>
 
 
-    <h1 class="text-3xl  uppercase pb-10">
-    Sökmotor
-    <br /> 
-    optimering
-    </h1>
+    `}
 
+    mainTitle4={"Sökmotor optimering"}
+    mainDesc4={`
     <p class="text-left pb-5 text-base">
     Genom att med SEO genomföra en grundlig optimering av din webbplats gör du den synlig på Google och andra sökmotorer som används av besökare som letar efter sidor som din.
     </p>
@@ -66,22 +49,12 @@ export default function SEOStockholm() {
     <p class="text-center pb-20 text-2xl">
     SEO består huvudsakligen av tre delar; en analys av sökord, on-page och off-page optimering.
     </p>
+    `}
 
-    </div>
-    </section>
 
-    </div>
-    </section>
-    <div class="whitedivider3  pb-20">
-      
-      </div>
-
-      <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-      <h1 class="text-2xl uppercase pb-10">
-      SEO analys
-      </h1>
-      <p class="text-left pb-10 text-sm">
+    mainTitle5={"SEO Analys"}
+    mainDesc5={`
+    <p class="text-left pb-10 text-sm">
       En analys av sökord är viktigt för att kunna finna de specifika sökfraser som är relevanta för dig och din verksamhet. Vid denna typ av analys tittar man närmare på vilka konkurrenter ditt företag har, på antalet sökningar på sökordet i fråga, intentionen som ligger bakom själva sökningen och mycket mer. Det är viktigt att kontrollera att konkurrensen inte är alltför hög men att det samtidigt är många sökningar på sökfrasen för att det ska vara gynnsamt för det SEO-arbete som väntar.
       </p>
 
@@ -115,76 +88,44 @@ export default function SEOStockholm() {
       Detta är den främsta anledningen till att SEO-byråer ständigt arbetar för att hjälpa företag att få fler kunder och för att kunna ha en chans att stå upp mot konkurrenter med högre trafik och bättre placering av sökresultatet. En bra SEO-byrå hjälper dig att med en analys av hemsidan komma fram till sökfraser som skulle gynna dig på bästa sätt och som skulle driva trafiken i din riktning. På Effektify arbetar vi dagligen med att hjälpa företag i Stockholm med sökmotoroptimering/SEO och kan med våra experter på varje område se vad som behöver förbättras för att du ska ranka bättre bland sökresultatet.
       </p>
 
-      <section class="cards-seo2">
-       <div class="pt-5 cards-container2 pb-10">
-     <div class="custom-back max-w-sm pb-10 border border-black shado mx-auto pt-10">
-        <h1 class="pt-40 cards2-title text-center mb-2 font-semibold tracking-tight uppercase text-rose-300"><span class="text-white"></span></h1>
-        <h2 class="pb-20 text-center cards2-under-title text-white"><span class="pl-4"></span></h2>
-    </div>
-    </div>
-    </section>
+    `}
 
-    <hr
-  class="my-12 h-0.5 border-t-0 bg-black opacity-100 dark:opacity-50" />
-    <h1 class="text-2xl text-right uppercase pt-5 pb-5">
-    Google utvecklas ständigt
-    <br />
-    – därför måste också SEO göra det
-    </h1>
+    mainTitle6={"Google utvecklas ständig – därför måste också SEO göra det"}
+    mainDesc6={`
     <p class="text-left pb-5 text-sm">
     Det är ett faktum att människor som satsar på sökmotoroptimering ser en positiv skillnad på resultaten efter att denna strategi har implementerats och efter att man börjat optimera hemsidan i fråga. Arbetet med sökmotoroptimering utvecklas och ändras ständigt eftersom Google gör det. Sökmotorn uppdateras ofta och därför är det av stor vikt att hänga med i dess utveckling. Detta innebär att även företag som arbetar med sökmotoroptimering bör se över sin strategi regelbundet för att fortsätta vara aktuella och för att kunna prestera på den nivå som just SEO-byråer bör.
     </p>
     <p class="text-left pb-10 text-sm">
     Vi på Effektify anser det vara av stor betydelse att hålla oss uppdaterade och att ständigt utvecklas inom sökmotoroptimering för att kunna erbjuda våra kunder bästa möjliga service. Våra experter utbildas ständigt inom varje del av SEO och SEM för att alltid kunna erbjuda våra kunder det som krävs för att lyckas hålla sig relevanta i sökmotorernas ögon.
     </p>
-    </div>
-    </section>
+    `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-white '>
-      <h1 class="text-2xl uppercase pb-10">
-      Hur fungerar
-      <br />
-      SEO i Stockholm?
-      </h1>
-      <p class="text-left pb-5 text-sm">
-      Lokal SEO, eller lokal sökmotoroptimering som det också kallas, skiljer sig något från SEO på nationell nivå. Den största skillnaden är konkurrensen, vilken är betydligt större på nationell nivå. För många branscher är det relevant att enbart synas lokalt online då deras tjänster eller produkter är avsedda att säljas lokalt. Om du exempelvis behöver hjälp av en rörmokare så kommer du troligtvis söka på ”rörmokare” tillsammans med staden i fråga eftersom det är just vad som är relevant för dig. Du är troligtvis inte på jakt efter en rörmokare i Luleå då du befinner dig i Stockholm.  
-      </p>
-      <p class="text-left pb-5 text-sm">
-      En viktig del av lokal SEO är att tydliggöra var du befinner dig och i vilka orter du verkar. Detta behöver tydligt redogöras för på din hemsida, både för dina kunders skull, men också för att sökmotorn Google ska anse din hemsida vara relevant för den som söker efter det du erbjuder. För att lyckas med lokal SEO behövs det även tas hänsyn till vissa tekniska åtgärder som ligger till grund för hemsidan. Detta gör det betydligt lättare för Google att begripa vilka geografiska områden sökmotorn ska förknippa dig och ditt företag med.
-      </p> 
-      <p class="text-left pb-10 text-sm">
-      Vanligtvis kräver det betydligt mindre arbete för företag som vill synas lokalt då konkurrensen begränsas i jämförelse med företag som är verksamma över hela landet. Detta har naturligtvis även att göra med vilken stad du vill synas lokalt på, då konkurrensen naturligtvis är betydligt högre i städer som Stockholm, Göteborg eller Malmö, till skillnad från mindre städer.
-      </p>
+    mainTitle7={"Hur fungerar SEO i Stockholm?"}
+    mainDesc7={`
+    <p class="text-left pb-5 text-sm">
+    Lokal SEO, eller lokal sökmotoroptimering som det också kallas, skiljer sig något från SEO på nationell nivå. Den största skillnaden är konkurrensen, vilken är betydligt större på nationell nivå. För många branscher är det relevant att enbart synas lokalt online då deras tjänster eller produkter är avsedda att säljas lokalt. Om du exempelvis behöver hjälp av en rörmokare så kommer du troligtvis söka på ”rörmokare” tillsammans med staden i fråga eftersom det är just vad som är relevant för dig. Du är troligtvis inte på jakt efter en rörmokare i Luleå då du befinner dig i Stockholm.  
+    </p>
+    <p class="text-left pb-5 text-sm">
+    En viktig del av lokal SEO är att tydliggöra var du befinner dig och i vilka orter du verkar. Detta behöver tydligt redogöras för på din hemsida, både för dina kunders skull, men också för att sökmotorn Google ska anse din hemsida vara relevant för den som söker efter det du erbjuder. För att lyckas med lokal SEO behövs det även tas hänsyn till vissa tekniska åtgärder som ligger till grund för hemsidan. Detta gör det betydligt lättare för Google att begripa vilka geografiska områden sökmotorn ska förknippa dig och ditt företag med.
+    </p> 
+    <p class="text-left pb-10 text-sm">
+    Vanligtvis kräver det betydligt mindre arbete för företag som vill synas lokalt då konkurrensen begränsas i jämförelse med företag som är verksamma över hela landet. Detta har naturligtvis även att göra med vilken stad du vill synas lokalt på, då konkurrensen naturligtvis är betydligt högre i städer som Stockholm, Göteborg eller Malmö, till skillnad från mindre städer.
+    </p>
+    `}
 
-    </div>
-    </section>
+    mainTitle8={"Google My Business (GMB)"}
+    mainDesc8={`
+    <p class='text-left pb-5 text-base text-white'>
+    Om du driver ett lokalt företag som verkar inom ett avgränsat geografiskt område är det av stor vikt för dig att finnas på Google My Business. Det är viktigt då Google My Business är det resultat som Google visar om du söker efter lokala företag. Google visar upp det som en lista av de olika relevanta lokala företagen ovanför det organiska sökresultatet. Detta är en enkel lösning som inte kräver mycket arbete och passar lokala företag väl.e Ads är det möjligt att specificera den målgrupp du riktar dig mot. Du har även möjlighet att rikta in annonserna geografiskt så att du syns på specifika sökord i specifika orter.
+    </p>   
+    <p class='text-left pb-5 text-base text-white'>
+    Skulle information om ditt företag förändras, såsom öppettider eller adressändring, kan du enkelt uppdatera den med Google My Business så att dina potentiella kunder alltid har rätt information om din verksamhet.
+    </p>
+    `}
 
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg2} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/80">
-        <h1 class='text-5xl pl-4 text-left uppercase text-white pt-40 pb-10'>
-        Google My
-        <br />
-        Business (GMB)
-        </h1>
-        <p class='text-left pb-5 text-base text-white'>
-        Om du driver ett lokalt företag som verkar inom ett avgränsat geografiskt område är det av stor vikt för dig att finnas på Google My Business. Det är viktigt då Google My Business är det resultat som Google visar om du söker efter lokala företag. Google visar upp det som en lista av de olika relevanta lokala företagen ovanför det organiska sökresultatet. Detta är en enkel lösning som inte kräver mycket arbete och passar lokala företag väl.e Ads är det möjligt att specificera den målgrupp du riktar dig mot. Du har även möjlighet att rikta in annonserna geografiskt så att du syns på specifika sökord i specifika orter.
-        </p>   
-        <p class='text-left pb-5 text-base text-white'>
-        Skulle information om ditt företag förändras, såsom öppettider eller adressändring, kan du enkelt uppdatera den med Google My Business så att dina potentiella kunder alltid har rätt information om din verksamhet.
-        </p>
-    </div>
-   </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-white'>
-      <h1 class="text-2xl uppercase pb-10">
-      Om Stockholm
-      </h1>
-      <p class='text-left pb-5 text-base text-white'>
+    mainTitle9={"Om Stockholm"}
+    mainDesc9={`
+    <p class='text-left pb-5 text-base text-white'>
       Som Sveriges huvudstad, anses Stockholm vara en de snabbast växande städerna i Europa då allt fler personer bestämmer sig för att flytta hit. Även om staden grundades redan på 1200-talet dröjde det så länge som till år 1436 för att den skulle bli till Sveriges huvudstad.
       </p>
       <p class='text-left pb-5 text-base text-white'>
@@ -206,13 +147,20 @@ export default function SEOStockholm() {
       <p class='text-left pb-10 text-base text-white'>
       Har du ett företag i Stockholm och vill att nya kunder ska hitta din hemsida? Som SEO-byrå är Effektify redo att hjälpa dig! Vi hjälper företag i hela Sverige att förbättra sina resultat och uppnå sina mål genom digital marknadsföring då våra tjänster inkluderar allt från SEO och SEM till att bygga hemsidor. Kontakta oss redan idag för hjälp med SEO i Stockholm!
       </p>
-    </div>
-    </section>
-
-    <HeadingCity2/>
-
-    <ContactForm />
-
+    `}
+    />
+   
+   <ContactForm 
+    title={"Vad kul att du har hittat till oss!"}
+    desc={"Har du frågor eller är redo att starta din digitala resa redan nu, är du välkommen att fylla i några rader eller maila oss på "}
+    company={"Företag"}
+    name={"Namn"}
+    phone={"Telefon"}
+    email={"E-post"}
+    text={"Vad kan vi hjälpa dig med?"}
+    buttonText={"Skicka"}
+  />
+  
     </>
 
   )
