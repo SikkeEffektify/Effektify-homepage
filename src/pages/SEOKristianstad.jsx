bbaa31706f9175c2eb87b3407a3f61eb685941ff
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-17 14:47:47 +0200

    seokristianstad

--- src/pages/SEOKristianstad.jsx
+++ src/pages/SEOKristianstad.jsx
@@ -4,50 +4,38 @@ import stats from '../assets/Effektify-hjalper-foretag-med-lokal-SEO.webp'
 import stats2 from '../assets/Effektify-hjalper-foretag-med-lokal-sokmotoroptimering.webp'
 import stats3 from '../assets/Effektify-hjalper-dig-med-SEO-lokalt.webp'
 import ContactForm from '../components/ContactForm'
+import SEOComponent from '../components/SEOComponent'
 
 export default function SEOKristianstad() {
   return (
     <>
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/60">
-        <h1 class='text-5xl pl-4 text-left uppercase text-white pt-10 pb-10'>Seo Kristianstad<span class="text-rose-300">.</span></h1>
-        <p class='text-left text-base text-white'>
-        Behöver du hjälp med SEO i Kristianstad? SEO kan vara krångligt, kontakta därför vår SEO-byrå, Effektify! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Kristianstad kan vi hjälpa dig och din verksamhet synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO. Vi är det givna valet för dig som söker efter en byrå som kan hjälpa dig med en SEO-analys och optimeringen av din hemsida så att den syns bättre på de olika sökmotorerna. 
-        </p>   
-    </div>
-   </div>
-    </section>
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats} alt="" />
+    <SEOComponent 
+    bg={bg}
+    mainTitle1={"Seo Kristianstad"}
+    mainDesc1={`
+    <p class='text-left text-base text-white'>
+    Behöver du hjälp med SEO i Kristianstad? SEO kan vara krångligt, kontakta därför vår SEO-byrå, Effektify! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Kristianstad kan vi hjälpa dig och din verksamhet synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO. Vi är det givna valet för dig som söker efter en byrå som kan hjälpa dig med en SEO-analys och optimeringen av din hemsida så att den syns bättre på de olika sökmotorerna. 
+    </p> 
+    
+    
+    `}
+
+    underImage1={`
     <p class="text-left pt-5 text-base pb-10">
     Det är ingen nyhet att synlighet skapar möjligheter för företag – både genom traditionell marknadsföring och digital marknadsföring. Att öka digital synlighet på olika sökmotorer ökar också chansen att besökarna till hemsidan ökar, och därmed fler kunder som sedan genererar större försäljning. Det finns många sätt att arbeta med digital marknadsföring på, och Effektify är inte enbart en SEO-byrå, vi är en byrå som arbetar med allt inom digital marknadsföring, både i Kristianstad och resterande Sverige. Vi hjälper våra kunder öka digital trafik och antal besökare till sin hemsida genom optimering och genom att förbättra hemsidan, och på så sätt även påverka deras resultat samt bidra till att deras ranking är bra på olika sökmotorer. Kunden är alltid i fokus hos oss!
     </p>
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-gray-100'>
-    <h1 class="text-2xl uppercase pb-10">
-    Hur fungerar SEO i Kristianstad?
-    </h1>
+    `}
+    mainTitle2={""}
+    mainDesc2={`
     <p class="text-left pb-10 text-sm">
     SEO står för det engelska uttrycket Search Engine Optimization, vilket översatt till svenska är sökmotoroptimering. SEO/sökmotoroptimering består av många olika delar och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på tjänster eller produkter som du erbjuder. Enkelt förklarat innebär SEO att optimera din webbplats så att den syns på de olika sökmotorerna. 
     <a href="https://effektify.com/seo-sokmotoroptimering/" class="text-rose-300" title="Sökmotoroptimering"> Syftet med SEO är att synas så högt upp på Google (och andra sökmotorer) som möjligt på sökord som är relevanta för dig och ditt företag</a>
     . Google analyserar faktorer så som huruvida din webbplats är relevant och hur den är uppbyggd, hur många, samt vilka som länkar till din webbplats och det innehåll du har på din webbplats för att sedan avgöra vilken placering du hamnar på. 
     </p>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats2} alt="" />
-  
+    `}
 
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats3} alt="" />
+    underImage2={`
     <h1 class="text-xl uppercase pb-10">
     Sökmotoroptimering
     </h1>
@@ -61,23 +49,10 @@ export default function SEOKristianstad() {
     <p class="text-left pb-10 text-sm">
     SEO består huvudsakligen av tre delar, analys av sökord, on-page och off-page. En analys av sökord är viktigt för att kunna finna de specifika sökfraser som är relevanta för dig och som gynnar dig och din verksamhet. Vid denna typ av analys kollar man på konkurrenter, antal sökningar på sökordet i fråga, sökintentionen m.m. Det är viktigt att konkurrensen inte är för hög men att det fortfarande är många sökningar på sökfrasen för att det ska vara gynnsamt. On-page-optimering är också en viktig del av SEO, det är det arbete som utförs på din hemsida. On-page innebär optimering av hemsidans innehåll, bland annat texter, rubriker, meta-titlar och meta-beskrivningar på hemsidan. Tanken är att optimera dessa utifrån de utvalda sökorden. Off-page är det arbete inom SEO som görs utanför hemsidan, så som att skicka länkar till hemsidor. Det är av stor vikt att man använder sig av relevanta länkar för detta. Dessa tre delar utgör en framgångsrik SEO-strategi. 
     </p>
-    </div>
-    </section>
+    `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-white bg-rose-300 pb-20'>
-    <h1 class="text-lg uppercase pb-10">
-    <a href="/seo-sokmotoroptimering/">Vill du veta mer om SEO?</a>
-    </h1>
-    <a class="vidButton" href='/seo-sokmotoroptimering/'>Klicka Här</a>
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <h1 class="text-2xl uppercase pb-10">
-    Google utvecklas ständigt – därför måste också SEO göra det
-    </h1>
+    mainTitle3={"Google utvecklas ständigt – därför måste också SEO göra det"}
+    mainDesc3={`
     <p class="text-left pb-5 text-sm">
     Det är ett faktum att företag som satsar på SEO ser en positiv skillnad på resultaten efter att denna strategi har implementerats och efter att man börjat optimera hemsidan i fråga. Arbetet med sökmotoroptimering utvecklas och ändras ständigt eftersom Google gör det, sökmotorn uppdateras ofta och därför är det av stor vikt att hänga med. 
     </p>
@@ -106,16 +81,11 @@ export default function SEOKristianstad() {
     <p class="text-left pb-10 text-sm">
     Innehållet på din hemsida är en viktig del av SEO då det ska finnas en relevans i det för att kunna öka den digitala trafiken till din hemsida, men samtidigt måste man också arbeta med att implementera relevanta sökord i texten för att få hemsidan att ranka högre. Det är mycket att tänka på, därför hjälper vi dig gärna med detta och alla andra delar av sökmotoroptimering i Kristianstad, så att du får så bra resultat som möjligt.
     </p>
-
-    </div>
-
-    </section>
-
-
-    <ContactForm/>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
+    `}
+    seoLink={"/seo-sokmotoroptimering/"}
+    seoLinkText={"Vill du veta mer om SEO?"}
+    seoLinkButton={"Klicka här"}
+    underButtonDesc={`
     <h1 class="text-2xl uppercase pb-10">
     Google My Business (GMB)
     </h1>
@@ -125,10 +95,10 @@ export default function SEOKristianstad() {
     <p class="text-left pb-10 text-sm">
     En ytterligare fördel, förutom att synas i Googles organiska sökresultat, är att om ditt företag är registrerat på Google My Business då kan det även synas i Google Maps när någon söker efter den typ av företag som du driver. Exempelvis om någon söker på ”frisör” + en specifik plats så visas lokala företag som är registrerade i tjänsten upp på kartan. Detta gör att du får mer trafik till din webbplats, vilket i sin tur leder till högre försäljning.
     </p>
-    
-    <h1 class="text-2xl uppercase pb-10">
-    Om Kristianstad
-    </h1>
+    `}
+
+    endTitle={"Om Kristiandstad"}
+    endDesc={`
     <p class="text-left pb-5 text-sm">
     Kristianstad är en tätort i Skåne, med drygt 
     <a href="https://www.kristianstad.se/sv/kommun-och-politik/kommunfakta/befolkning/" title="Läs mer om Kungsbacka här" class="text-rose-500" rel="nofollow noopener" target="_blank"> 86 000 invånare i kommunen och ca 40 000 som bor i staden. </a>
@@ -140,11 +110,9 @@ export default function SEOKristianstad() {
     <p class="text-left pb-10 text-sm">
     Har du ett företag i Kristianstad och vill att nya kunder ska hitta din hemsida? Vår SEO-byrå, Effektify hjälper dig gärna med SEO i Kristianstad! Vi hjälper företag i hela Sverige att synas på sökresultaten, förbättra sin ranking och uppnå sina mål genom digital marknadsföring, våra tjänster inkluderar allt från SEO och Google Ads till att bygga hemsidor. Kontakta oss för en SEO-analys och annan hjälp med digitala lösningar till ditt företag i Kristianstad (och resterande Sverige). Sökmotoroptimering/SEO kan vara krångligt, låt därför oss utföra ditt arbete med SEO åt dig!
     </p>
-
-
-
-    </div>
-    </section>
+    `}
+    
+    />
     </>
 
   )
