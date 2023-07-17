da2f3719fb1a6ca45adada0fee21405e717d097f
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-17 16:24:37 +0200

    seoluleå

--- src/pages/SEOLulea.jsx
+++ src/pages/SEOLulea.jsx
@@ -4,49 +4,34 @@ import stats from '../assets/Effektify-hjalper-foretag-med-lokal-SEO.webp'
 import stats2 from '../assets/Effektify-hjalper-foretag-med-lokal-sokmotoroptimering.webp'
 import stats3 from '../assets/Effektify-hjalper-dig-med-SEO-lokalt.webp'
 import ContactForm from '../components/ContactForm'
+import SEOComponent from '../components/SEOComponent'
 
 export default function SEOLulea() {
   return (
     <>
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/60">
-        <h1 class='text-5xl pl-4 text-left uppercase text-white pt-5 pb-10'>Seo Luleå<span class="text-rose-300">.</span></h1>
-        <p class='text-left text-lg text-white'>
-        Behöver du hjälp med SEO i Luleå? SEO kan vara krångligt, kontakta därför vår SEO-byrå, Effektify! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Luleå kan vi hjälpa dig och din verksamhet synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO på olika hemsidor. Vår SEO-byrå är det givna valet för dig som söker efter en byrå som kan hjälpa dig med optimering av din hemsida så att den får en bättre ranking och syns högt upp på de olika sökmotorerna. 
-        </p>   
-    </div>
-   </div>
-    </section>
+    <SEOComponent 
+    bg={bg}
+    mainTitle1={"Seo Luleå"}
+    mainDesc1={`
+    <p class='text-left text-lg text-white'>
+    Behöver du hjälp med SEO i Luleå? SEO kan vara krångligt, kontakta därför vår SEO-byrå, Effektify! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Luleå kan vi hjälpa dig och din verksamhet synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO på olika hemsidor. Vår SEO-byrå är det givna valet för dig som söker efter en byrå som kan hjälpa dig med optimering av din hemsida så att den får en bättre ranking och syns högt upp på de olika sökmotorerna. 
+    </p> 
+    `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats} alt="" />
+    underImage1={`
     <p class="text-left pt-5 text-base pb-10">
     Det är ingen nyhet att synlighet skapar möjligheter för företag – både genom traditionell marknadsföring och digital marknadsföring. Att öka digital synlighet på olika sökmotorer ökar också chanser att få fler besökare till sin hemsida, och därmed fler kunder som sedan genererar större försäljning. Det finns många sätt att arbeta med digital marknadsföring på, och Effektify är inte enbart en SEO-byrå, vi är en byrå som arbetar med allt inom digital marknadsföring, både i Luleå och resterande Sverige. Vi hjälper våra kunder öka digital trafik och antal besökare till sin webbplats genom att förbättra och optimera den, och på så sätt får våra kunder mer relevant trafik till sin hemsida samt så rankar de bra på olika sökmotorer. Kunden är alltid i fokus hos oss!
     </p>
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-gray-100'>
-    <h1 class="text-2xl uppercase pb-10">
-    Hur fungerar SEO i Luleå?
-    </h1>
+    `}
+    mainTitle2={"Hur fungerar SEO i Luleå?"}
+    mainDesc2={`
     <p class="text-left pb-5 text-sm">
     SEO står för det engelska uttrycket Search Engine Optimization, vilket översatt till svenska är sökmotoroptimering. SEO/sökmotoroptimering består av många olika delar och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på tjänster eller produkter som du erbjuder. 
     <a href="https://effektify.com/seo-sokmotoroptimering/" class="text-rose-300" title="SEO"> Syftet med SEO är att synas så högt upp på Google (och andra sökmotorer) som möjligt på sökord som är relevanta för dig och ditt företag </a>
     . Syftet med SEO är att synas så högt upp på Google (och andra sökmotorer) som möjligt på sökord som är relevanta för dig och ditt företag. Google analyserar faktorer så som huruvida din webbplats är relevant och hur den är uppbyggd, hur många, samt vilka som länkar till din webbplats och det innehåll du har på din webbplats för att sedan avgöra vilken ranking du hamnar på. 
     </p>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats2} alt="" />
-
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats3} alt="" />
+    `}
+    underImage2={`
     <h1 class="text-xl uppercase pb-10">
     Sökmotoroptimering
     </h1>
@@ -64,23 +49,10 @@ export default function SEOLulea() {
     <a href="https://effektify.com/on-page-seo/" class="text-rose-300" title="on-page seo"> on-page </a>
     och off-page. En analys av sökord är viktigt för att kunna finna de specifika sökfraser som är relevanta för dig och som gynnar dig och din verksamhet. Vid denna typ av analys kollar man på konkurrenter, antal sökningar på sökordet i fråga, huruvida sökintentionen är bra, om sökordet når din målgrupp m.m. Det är viktigt att konkurrensen inte är för hög men att det fortfarande är många sökningar på sökfrasen för att det ska vara gynnsamt att fokusera på sökordet i fråga. On-page-optimering är också en viktig del av sökmotoroptimering, det är det arbete som utförs på din hemsida. On-page innebär optimering av hemsidans innehåll, bland annat texter, rubriker, meta-titlar och meta-beskrivningar på hemsidan. Tanken är att optimera detta innehåll utifrån de utvalda sökorden. Off-page är det arbete inom sökmotoroptimering som görs utanför hemsidan, så som att skicka rätt och relevanta länkar till din hemsida. Dessa tre delar utgör en framgångsrik SEO-strategi. 
     </p>
-    </div>
-    </section>
+    `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-white bg-rose-300 pb-20'>
-    <h1 class="text-lg uppercase pb-10">
-    <a href="/seo-sokmotoroptimering/">Vill du veta mer om SEO?</a>
-    </h1>
-    <a class="vidButton" href='/seo-sokmotoroptimering/'>Klicka Här</a>
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <h1 class="text-2xl uppercase pb-10">
-    Google utvecklas ständigt – därför måste också SEO göra det
-    </h1>
+    mainTitle3={"Google utvecklas ständigt – därför måste också SEO göra det"}
+    mainDesc3={`
     <p class="text-left pb-10 text-sm">
     Det är ett faktum att företag som satsar på sökmotoroptimering ser en positiv skillnad på resultaten efter att denna strategi har implementerats och efter att man börjat optimera hemsidan i fråga. Arbetet med sökmotoroptimering utvecklas och ändras ständigt eftersom Google gör det, sökmotorn uppdateras ofta och därför är det av stor vikt att hänga med. Detta innebär att även företag som arbetar med sökmotoroptimering bör se över sin strategi regelbundet för att fortsätta vara aktuella. Vi på Effektify anser det vara av stor betydelse att hålla oss uppdaterade och att ständigt utvecklas inom sökmotoroptimering för att kunna erbjuda våra kunder bästa möjliga service och rådgivning. 
     </p>
@@ -94,16 +66,12 @@ export default function SEOLulea() {
     <p class="text-left pb-10 text-sm">
     En viktig del av lokal SEO är att tydliggöra var du befinner dig och i vilka orter du verkar på din hemsida, både för dina kunders skull, men också för Google så att sökmotorn anser din hemsida vara relevant när den gör en analys av den. För att lyckas med lokal SEO i Luleå behövs det även tas hänsyn till vissa tekniska åtgärder bakom webbplatsen. Detta gör det lättare för Google att begripa vilka geografiska områden sökmotorn ska förknippa dig och ditt företag med, och därmed får du också bättre synlighet.
     </p>
+    `}
 
-    </div>
-
-    </section>
-
-
-    <ContactForm/>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
+    seoLink={"/seo-sokmotoroptimering/"}
+    seoLinkText={"Vill du veta mer om SEO?"}
+    seoLinkButton={"Klicka här"}
+    underButtonDesc={`
     <h1 class="text-2xl uppercase pb-10">
     Google My Business (GMB)
     </h1>
@@ -113,10 +81,10 @@ export default function SEOLulea() {
     <p class="text-left pb-10 text-sm">
     En ytterligare fördel, förutom att synas i Googles organiska sökresultat, är att om ditt företag är registrerat på Google My Business då kan det även synas i Google Maps när någon söker efter den typ av företag som du driver. Exempelvis om någon söker på ”frisör” + en specifik plats så visas lokala företag som är registrerade i tjänsten upp på kartan. Detta gör att du får mer trafik till din webbplats, vilket i sin tur leder till högre försäljning.
     </p>
-    
-    <h1 class="text-2xl uppercase pb-10">
-    Om Luleå
-    </h1>
+    `}
+
+    endTitle={"Om Luleå"}
+    endDesc={`
     <p class="text-left pb-5 text-sm">
     Luleå är en tätort i Norrbotten, och i slutet av år 2021 hade staden
     <a href="https://www.lulea.se/kommun--politik/fakta-och-statistik/befolkningsstatistik.html" class="text-rose-300" title="Läs mer om Luleå här" rel="nofollow noopener" target="_blank"> 78 549 invånare </a>
@@ -131,11 +99,19 @@ export default function SEOLulea() {
     <p class="text-left pb-5 text-sm">
     Har du ett företag i Luleå och vill att nya kunder ska hitta din hemsida? Vår SEO-byrå, Effektify erbjuder hjälp med sökmotoroptimering i Luleå! Vi hjälper företag i hela Sverige att förbättra sina resultat, uppnå sina mål och få en ökad försäljning genom digital marknadsföring. Våra tjänster inkluderar allt från SEO och Google Ads till att bygga hemsidor. Kontakta oss för en SEO-analys och annan hjälp med digitala lösningar till ditt företag i Luleå (och resterande Sverige). Sökmotoroptimering/SEO kan vara krångligt, låt därför oss utföra ditt arbete med SEO åt dig!
     </p>
+    `}
+    />
 
-
-
-    </div>
-    </section>
+<ContactForm 
+    title={"Vad kul att du har hittat till oss!"}
+    desc={"Har du frågor eller är redo att starta din digitala resa redan nu, är du välkommen att fylla i några rader eller maila oss på "}
+    company={"Företag"}
+    name={"Namn"}
+    phone={"Telefon"}
+    email={"E-post"}
+    text={"Vad kan vi hjälpa dig med?"}
+    buttonText={"Skicka"}
+  />
     </>
 
   )
