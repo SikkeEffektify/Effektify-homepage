f05a720f735a9604e44cc1e524095dc124a0691a
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-17 09:25:59 +0200

    seofalk

--- src/pages/SEOFalkenberg.jsx
+++ src/pages/SEOFalkenberg.jsx
@@ -1,126 +1,107 @@
 import React from 'react'
-import bg from '../assets/seo-falkenberg.webp'
+import backg from '../assets/seo-falkenberg.webp'
 import stats from '../assets/Effektify-hjalper-foretag-med-lokal-SEO.webp'
 import stats2 from '../assets/Effektify-hjalper-foretag-med-lokal-sokmotoroptimering.webp'
 import stats3 from '../assets/Effektify-hjalper-dig-med-SEO-lokalt.webp'
 import ContactForm from '../components/ContactForm'
+import SEOComponent from '../components/SEOComponent'
 
 export default function SEOFalkenberg() {
   return (
+
+
     <>
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/60">
-        <h1 class='text-5xl pl-4 text-left uppercase text-white pb-10'>Seo Falkenberg<span class="text-rose-300">.</span></h1>
-        <p class='text-left text-lg text-white'>
+
+    <SEOComponent 
+      bg={backg}
+      mainTitle1={"Seo Falkenberg"}
+      mainDesc1={`
+      <p class='text-left text-lg text-white'>
         Behöver du hjälp med SEO i Falkenberg? Låt vår byrå ta den rollen! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Falkenberg kan vi hjälpa dig synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder på din hemsida. På denna byrå finns lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO för många olika typer av hemsidor. Ta kontakt med oss på Effektify för bättre resultat.
-        </p>   
-    </div>
-   </div>
-    </section>
+      </p>   
+      `}
+      underImage1={`<p class="text-left pt-5 text-base pb-10">
+      Det är ingen nyhet att synlighet skapar möjligheter för företag – både genom traditionell marknadsföring och digital marknadsföring. Att öka din synlighet online ökar också dina chanser att få fler besökare till din hemsida, och därmed fler kunder som sedan genererar större försäljning. Det finns många sätt att använda sig av digital marknadsföring på. Effektify är inte enbart en SEO-byrå, vi hjälper våra kunder med allt inom digital marknadsföring, både i Falkenberg, men också i hela Sverige. 
+      </p>`}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats} alt="" />
-    <p class="text-left pt-5 text-base pb-5">
-    Det är ingen nyhet att synlighet skapar möjligheter för företag – både genom traditionell marknadsföring och digital marknadsföring. Att öka din synlighet online ökar också dina chanser att få fler besökare till din hemsida, och därmed fler kunder som sedan genererar större försäljning. Det finns många sätt att använda sig av digital marknadsföring på. Effektify är inte enbart en SEO-byrå, vi hjälper våra kunder med allt inom digital marknadsföring, både i Falkenberg, men också i hela Sverige. 
-    </p>
-   
- 
-    </div>
-    </section>
+      mainTitle2={"Hur fungerar SEO i Falkenberg?"}
+      mainDesc2={`
+      <p class="text-left pb-5 text-sm">
+      SEO står för det engelska uttrycket Search Engine Optimization, vilket översatt till svenska är sökmotoroptimering. 
+      <a href="https://effektify.com/seo-sokmotoroptimering/" class="text-rose-300 underline" title="Sökmotoroptimering"> SEO </a>
+      består av många olika delar och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på nätet efter tjänster eller produkter som du erbjuder. Enkelt förklarat innebär SEO att göra hemsidor synliga på Google och andra sökmotorer. Syftet med SEO är att få hemsidor att synas så högt upp på Google (och andra sökmotorer) som möjligt på sökord som är relevanta för den specifika hemsidan i fråga. Google analyserar faktorer så som huruvida din webbplats är relevant och hur den är uppbyggd, hur många, samt vilka som länkar till din webbplats och det innehåll du har på din webbplats för att sedan avgöra vilken placering du hamnar på. 
+      </p>
+      `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-gray-100'>
-    <h1 class="text-2xl uppercase pb-10">
-    Hur fungerar SEO i Falkenberg?
-    </h1>
-    <p class="text-left pb-5 text-sm">
-    SEO står för det engelska uttrycket Search Engine Optimization, vilket översatt till svenska är sökmotoroptimering. 
-    <a href="https://effektify.com/seo-sokmotoroptimering/" class="text-rose-300 underline" title="Sökmotoroptimering"> SEO </a>
-    består av många olika delar och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på nätet efter tjänster eller produkter som du erbjuder. Enkelt förklarat innebär SEO att göra hemsidor synliga på Google och andra sökmotorer. Syftet med SEO är att få hemsidor att synas så högt upp på Google (och andra sökmotorer) som möjligt på sökord som är relevanta för den specifika hemsidan i fråga. Google analyserar faktorer så som huruvida din webbplats är relevant och hur den är uppbyggd, hur många, samt vilka som länkar till din webbplats och det innehåll du har på din webbplats för att sedan avgöra vilken placering du hamnar på. 
-    </p>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats2} alt="" />
-    </div>
-    </section>
+      underImage2={`
+      <h1 class="text-xl uppercase pb-10">
+      Sökmotoroptimering
+      </h1>
+      <p class="text-left pb-5 text-sm">
+      Syftet med SEO är att synas så högt upp på Google (och andra sökmotorer) som möjligt på sökord som är relevanta för dig och ditt företag. 
+      </p>
+      <p class="text-left pb-5 text-sm">
+      Google analyserar faktorer så som huruvida din webbplats är relevant och hur den är uppbyggd, hur många, samt vilka som länkar till din webbplats och det innehåll du har på din webbplats för att sedan avgöra vilken placering du hamnar på.
+      </p>
+      <p class="text-left pb-5 text-sm">
+      SEO består huvudsakligen av tre delar, analys av sökord, on-page-optimering och off-page-optimering. En analys av sökord är viktigt för att kunna finna de specifika sökfraser som är relevanta för dig och som gynnar dig och din verksamhet. Vid denna typ av analys kollar man på konkurrenter, antal sökningar på sökordet i fråga, sökintentionen m.m. Det är viktigt att konkurrensen inte är för hög men att det fortfarande är många sökningar på sökfrasen för att det ska vara gynnsamt. On-page-optimering är också en viktig del av SEO, det är det arbete som utförs på din hemsida. On-page innebär optimering av hemsidans innehåll, bland annat texter, rubriker, meta-titlar och meta-beskrivningar på hemsidan. Tanken är att optimera dessa utifrån de utvalda sökorden. Off-page är det arbete inom SEO som görs utanför hemsidan, så som att skicka länkar till din hemsida. Dessa tre delar utgör en framgångsrik SEO-strategi.
+      </p>
+      `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats3} alt="" />
-    <h1 class="text-xl uppercase pb-10">
-    Sökmotoroptimering
-    </h1>
-    <p class="text-left pb-5 text-sm">
-    Syftet med SEO är att synas så högt upp på Google (och andra sökmotorer) som möjligt på sökord som är relevanta för dig och ditt företag. 
-    </p>
-    <p class="text-left pb-5 text-sm">
-    Google analyserar faktorer så som huruvida din webbplats är relevant och hur den är uppbyggd, hur många, samt vilka som länkar till din webbplats och det innehåll du har på din webbplats för att sedan avgöra vilken placering du hamnar på.
-    </p>
-    <p class="text-left pb-5 text-sm">
-    SEO består huvudsakligen av tre delar, analys av sökord, on-page-optimering och off-page-optimering. En analys av sökord är viktigt för att kunna finna de specifika sökfraser som är relevanta för dig och som gynnar dig och din verksamhet. Vid denna typ av analys kollar man på konkurrenter, antal sökningar på sökordet i fråga, sökintentionen m.m. Det är viktigt att konkurrensen inte är för hög men att det fortfarande är många sökningar på sökfrasen för att det ska vara gynnsamt. On-page-optimering är också en viktig del av SEO, det är det arbete som utförs på din hemsida. On-page innebär optimering av hemsidans innehåll, bland annat texter, rubriker, meta-titlar och meta-beskrivningar på hemsidan. Tanken är att optimera dessa utifrån de utvalda sökorden. Off-page är det arbete inom SEO som görs utanför hemsidan, så som att skicka länkar till din hemsida. Dessa tre delar utgör en framgångsrik SEO-strategi.
-    </p>
-    </div>
-    </section>
+      mainTitle3={"Google utvecklas ständigt – därför måste också SEO göra det"}
+      mainDesc3={`
+      <p class="text-left pb-5 text-sm">
+      Lokal SEO, eller lokal sökmotoroptimering som det också kallas, skiljer sig något från SEO på nationell nivå. Den största skillnaden är konkurrensen, den är betydligt större på nationell nivå. För många branscher är det relevant att enbart synas lokalt på online då deras tjänster eller produkter är avsedda att säljas lokalt. Om du exempelvis behöver hjälp av en rörmokare så kommer du troligtvis söka på ”rörmokare” plus din stad eftersom det är vad som är relevant för dig.
+      </p>
+      <p class="text-left pb-10 text-sm">
+      En viktig del av lokal SEO är att tydliggöra var du befinner dig och i vilka orter du verkar på din hemsida, både för dina kunders skull, men också för Google så att sökmotorn anser din hemsida vara relevant. För att lyckas med lokal SEO behövs det även tas hänsyn till vissa tekniska åtgärder bakom hemsidan. Detta gör det lättare för Google att begripa vilka geografiska områden sökmotorn ska förknippa dig och ditt företag med.
+      </p>
+      `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-white bg-rose-300 pb-20'>
-    <h1 class="text-lg uppercase pb-10">
-    <a href="/seo-sokmotoroptimering/">Vill du veta mer om SEO?</a>
-    </h1>
-    <a class="vidButton" href='/seo-sokmotoroptimering/'>Klicka Här</a>
-    </div>
-    </section>
+      seoLink={"/seo-sokmotoroptimering/"}
+      seoLinkText={"Vill du veta mer om SEO?"}
+      seoLinkButton={"Klicka här"}
+      underButtonDesc={`
+      <h1 class="text-2xl uppercase pb-10">
+      Google My Business (GMB)
+      </h1>
+      <p class="text-left pb-5 text-sm">
+      Om du driver ett lokalt företag som verkar inom ett avgränsat geografiskt område är det viktigt för dig att finnas på Google My Business. Det är viktigt då Google My Business är det resultat som Google visar om du söker efter lokala företag. Google visar det som en lista av de olika relevanta lokala företagen ovanför de organiska sökresultaten. Tjänsten är kostnadsfri, och dessutom fungerar den även som ett skyltfönster för ditt företag där du kan avgöra hur du vill presentera ditt företag. Du kan välja att lägga till telefonnummer, adress och öppettider exempelvis.
+      </p>
+      <p class="text-left pb-10 text-sm">
+      En ytterligare fördel, förutom att synas i Googles organiska sökresultat, är att om ditt företag är registrerat på Google My Business då kan det även synas i Google Maps när någon söker efter den typ av företag som du driver. Exempelvis om någon söker på ”frisör” + en specifik plats så visas lokala företag som är registrerade i tjänsten upp på kartan. Detta gör att du får mer trafik till din webbplats, vilket i sin tur leder till högre försäljning.
+      </p>
+      `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <h1 class="text-2xl uppercase pb-10">
-    Google utvecklas ständigt – därför måste också SEO göra det
-    </h1>
-    <p class="text-left pb-5 text-sm">
-    Lokal SEO, eller lokal sökmotoroptimering som det också kallas, skiljer sig något från SEO på nationell nivå. Den största skillnaden är konkurrensen, den är betydligt större på nationell nivå. För många branscher är det relevant att enbart synas lokalt på online då deras tjänster eller produkter är avsedda att säljas lokalt. Om du exempelvis behöver hjälp av en rörmokare så kommer du troligtvis söka på ”rörmokare” plus din stad eftersom det är vad som är relevant för dig.
-    </p>
-    <p class="text-left pb-10 text-sm">
-    En viktig del av lokal SEO är att tydliggöra var du befinner dig och i vilka orter du verkar på din hemsida, både för dina kunders skull, men också för Google så att sökmotorn anser din hemsida vara relevant. För att lyckas med lokal SEO behövs det även tas hänsyn till vissa tekniska åtgärder bakom hemsidan. Detta gör det lättare för Google att begripa vilka geografiska områden sökmotorn ska förknippa dig och ditt företag med.
-    </p>
+      endTitle={"Om Falkenberg"}
+      endDesc={`
+      <p class="text-left pb-5 text-sm">
+      Falkenberg är en tätort i Halland, som ligger på Västkusten. I Falkenberg finns det 
+      <a href="https://kommun.falkenberg.se/kommun/kommunenisiffror.4.56c01845162d219cb417c9f.html" class="text-rose-300 underline" title="Läs mer om Falkenberg här" rel="nofollow noopener" target="_blank"> 46 245 invånare </a>
+      . Man tror att Falkenberg ska ha grundats år 1200, då det är från den tiden man har funnit de första skriftliga beläggen. Staden tillhörde Danmark tidigare, precis som resterande Halland. Falkenberg centrum ligger väster om Ätran, där även stora delar av stadens butiker, restauranger och caféer, samt gymnasieskola ligger. Öster om Ätran ligger bland annat Skrea strand och Klitterbadet.
+      </p>
+      <p class="text-left pb-5 text-sm">
+      Falkenberg, som är en gammal hamnstad, har även ett rikt näringsliv där mycket ingår i livsmedels- och verkstadsindustrin. Exempel på detta är Carlsberg Sverige, Arla mejeri och Falkenbergs Laxrökeri. Även fast Falkenberg är en mindre stad så finns det mycket göra så som att besöka mysiga caféer, restauranger och museer. I Falkenberg finns även en av Sveriges finaste stränder, Skrea strand.
+      </p>
+      <p class="text-left pb-5 text-sm">
+      Har du ett företag i Falkenberg och behöver hjälp med att öka trafiken till din hemsida? Vår byrå Effektify hjälper dig gärna! Vi hjälper företag i hela Sverige att förbättra sina resultat och uppnå sina mål genom digital marknadsföring, vi arbetar med allt från SEO och Google Ads till att bygga hemsidor. Kontakta vår byrå för professionell hjälp.
+      </p>
+      `}
+      
 
-    <h1 class="text-2xl uppercase pb-10">
-    Google My Business (GMB)
-    </h1>
-    <p class="text-left pb-5 text-sm">
-    Om du driver ett lokalt företag som verkar inom ett avgränsat geografiskt område är det viktigt för dig att finnas på Google My Business. Det är viktigt då Google My Business är det resultat som Google visar om du söker efter lokala företag. Google visar det som en lista av de olika relevanta lokala företagen ovanför de organiska sökresultaten. Tjänsten är kostnadsfri, och dessutom fungerar den även som ett skyltfönster för ditt företag där du kan avgöra hur du vill presentera ditt företag. Du kan välja att lägga till telefonnummer, adress och öppettider exempelvis.
-    </p>
-    <p class="text-left pb-10 text-sm">
-    En ytterligare fördel, förutom att synas i Googles organiska sökresultat, är att om ditt företag är registrerat på Google My Business då kan det även synas i Google Maps när någon söker efter den typ av företag som du driver. Exempelvis om någon söker på ”frisör” + en specifik plats så visas lokala företag som är registrerade i tjänsten upp på kartan. Detta gör att du får mer trafik till din webbplats, vilket i sin tur leder till högre försäljning.
-    </p>
+      
+    />
 
-
-    </div>
-
-    </section>
-
-
-    <ContactForm/>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-
-    <h1 class="text-2xl uppercase pb-10">
-    Om Falkenberg
-    </h1>
-    <p class="text-left pb-5 text-sm">
-    Falkenberg är en tätort i Halland, som ligger på Västkusten. I Falkenberg finns det 
-    <a href="https://kommun.falkenberg.se/kommun/kommunenisiffror.4.56c01845162d219cb417c9f.html" class="text-rose-300 underline" title="Läs mer om Falkenberg här" rel="nofollow noopener" target="_blank"> 46 245 invånare </a>
-    . Man tror att Falkenberg ska ha grundats år 1200, då det är från den tiden man har funnit de första skriftliga beläggen. Staden tillhörde Danmark tidigare, precis som resterande Halland. Falkenberg centrum ligger väster om Ätran, där även stora delar av stadens butiker, restauranger och caféer, samt gymnasieskola ligger. Öster om Ätran ligger bland annat Skrea strand och Klitterbadet.
-    </p>
-    <p class="text-left pb-5 text-sm">
-    Falkenberg, som är en gammal hamnstad, har även ett rikt näringsliv där mycket ingår i livsmedels- och verkstadsindustrin. Exempel på detta är Carlsberg Sverige, Arla mejeri och Falkenbergs Laxrökeri. Även fast Falkenberg är en mindre stad så finns det mycket göra så som att besöka mysiga caféer, restauranger och museer. I Falkenberg finns även en av Sveriges finaste stränder, Skrea strand.
-    </p>
-    <p class="text-left pb-5 text-sm">
-    Har du ett företag i Falkenberg och behöver hjälp med att öka trafiken till din hemsida? Vår byrå Effektify hjälper dig gärna! Vi hjälper företag i hela Sverige att förbättra sina resultat och uppnå sina mål genom digital marknadsföring, vi arbetar med allt från SEO och Google Ads till att bygga hemsidor. Kontakta vår byrå för professionell hjälp.
-    </p>
-    </div>
-    </section>
+  <ContactForm 
+    title={"Vad kul att du har hittat till oss!"}
+    desc={"Har du frågor eller är redo att starta din digitala resa redan nu, är du välkommen att fylla i några rader eller maila oss på "}
+    company={"Företag"}
+    name={"Namn"}
+    phone={"Telefon"}
+    email={"E-post"}
+    text={"Vad kan vi hjälpa dig med?"}
+    buttonText={"Skicka"}
+  />
     </>
 
   )
