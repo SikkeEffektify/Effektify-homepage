9ae0f3a93e2364de3625532f610bd92ce16ff038
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-17 14:13:59 +0200

    seoeskil

--- src/pages/SEOEskilstuna.jsx
+++ src/pages/SEOEskilstuna.jsx
@@ -4,50 +4,36 @@ import stats from '../assets/Effektify-hjalper-foretag-med-lokal-SEO.webp'
 import stats2 from '../assets/Effektify-hjalper-foretag-med-lokal-sokmotoroptimering.webp'
 import stats3 from '../assets/Effektify-hjalper-dig-med-SEO-lokalt.webp'
 import ContactForm from '../components/ContactForm'
+import SEOComponent from '../components/SEOComponent'
 
 export default function SEOEskilstuna() {
   return (
     <>
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/60">
-        <h1 class='text-5xl pl-4 text-left uppercase text-white pb-10'>Seo Eskilstuna<span class="text-rose-300">.</span></h1>
-        <p class='text-left text-lg text-white'>
-        Behöver du hjälp med SEO i Eskilstuna? SEO kan vara krångligt, kontakta därför vår SEO-byrå, Effektify! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Eskilstuna kan vi hjälpa dig och din verksamhet synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO. Vi är det givna valet för dig som söker efter en byrå som kan hjälpa dig med optimering av din hemsida så att den syns bättre på de olika sökmotorerna. 
-        </p>   
-    </div>
-   </div>
-    </section>
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats} alt="" />
+    <SEOComponent
+    bg={bg}
+    mainTitle1={"Seo Eskilstuna"}
+    mainDesc1={`
+    <p class='text-left text-lg text-white'>
+    Behöver du hjälp med SEO i Eskilstuna? SEO kan vara krångligt, kontakta därför vår SEO-byrå, Effektify! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Eskilstuna kan vi hjälpa dig och din verksamhet synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO. Vi är det givna valet för dig som söker efter en byrå som kan hjälpa dig med optimering av din hemsida så att den syns bättre på de olika sökmotorerna. 
+    </p>
+    `}
+
+    underImage1={`
     <p class="text-left pt-5 text-base pb-5">
     Det är ingen nyhet att synlighet skapar möjligheter för företag – både genom traditionell marknadsföring och digital marknadsföring. Att öka digital synlighet på olika sökmotorer ökar också chanser att få fler besökare till sin hemsida, och därmed fler kunder som sedan genererar större försäljning. Det finns många sätt att arbeta med digital marknadsföring på, och Effektify är inte enbart en SEO-byrå, vi är en byrå som arbetar med allt inom digital marknadsföring, både i Eskilstuna och resterande Sverige. Vi hjälper våra kunder öka digital trafik och antal besökare till sin webbplats genom att förbättra och optimera den, och på så sätt även påverka deras resultat samt bidra till att de rankar bra på olika sökmotorer. Kunden är alltid i fokus hos oss!
     </p>
-   
- 
-    </div>
-    </section>
+    `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-gray-100'>
-    <h1 class="text-2xl uppercase pb-10">
-    Hur fungerar SEO i Eskilstuna?
-    </h1>
+    mainTitle2={"Hur fungerar SEO i Eskilstuna?"}
+    mainDesc2={`
     <p class="text-left pb-5 text-sm">
     SEO står för det engelska uttrycket Search Engine Optimization, vilket översatt till svenska är sökmotoroptimering. 
     <a href="https://effektify.com/seo-sokmotoroptimering/" class="text-rose-300 underline" title="Sökmotoroptimering"> SEO </a>
     består av många olika delar och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på tjänster eller produkter som du erbjuder. Enkelt förklarat innebär SEO att optimera din webbplats så att den syns på de olika sökmotorerna. Syftet med SEO är att synas så högt upp på Google (och andra sökmotorer) som möjligt på sökord som är relevanta för dig och ditt företag. Google analyserar faktorer så som huruvida din webbplats är relevant och hur den är uppbyggd, hur många, samt vilka som länkar till din webbplats och det innehåll du har på din webbplats för att sedan avgöra vilken placering du hamnar på.
     </p>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats2} alt="" />
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats3} alt="" />
+    `}
+    underImage2={`
     <h1 class="text-xl uppercase pb-10">
     Sökmotoroptimering
     </h1>
@@ -60,29 +46,17 @@ export default function SEOEskilstuna() {
     <p class="text-left pb-5 text-sm">
     SEO består huvudsakligen av tre delar, analys av sökord, on-page-optimering och off-page-optimering. En analys av sökord är viktigt för att kunna finna de specifika sökfraser som är relevanta för dig och som gynnar dig och din verksamhet. Vid denna typ av analys kollar man på konkurrenter, antal sökningar på sökordet i fråga, sökintentionen m.m. Det är viktigt att konkurrensen inte är för hög men att det fortfarande är många sökningar på sökfrasen för att det ska vara gynnsamt. On-page-optimering är också en viktig del av SEO, det är det arbete som utförs på din hemsida. On-page innebär optimering av hemsidans innehåll, bland annat texter, rubriker, meta-titlar och meta-beskrivningar på hemsidan. Tanken är att optimera dessa utifrån de utvalda sökorden. Off-page är det arbete inom SEO som görs utanför hemsidan, så som att skicka länkar till din hemsida. Dessa tre delar utgör en framgångsrik SEO-strategi.
     </p>
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-white bg-rose-300 pb-20'>
-    <h1 class="text-lg uppercase pb-10">
-    <a href="/seo-sokmotoroptimering/">Vill du veta mer om SEO?</a>
-    </h1>
-    <a class="vidButton" href='/seo-sokmotoroptimering/'>Klicka Här</a>
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <h1 class="text-2xl uppercase pb-10">
-    Google utvecklas ständigt – därför måste också SEO göra det
-    </h1>
+    `}
+    
+    mainTitle3={"Google utvecklas ständigt – därför måste också SEO göra det"}
+    mainDesc3={`
     <p class="text-left pb-5 text-sm">
     Lokal SEO, eller lokal sökmotoroptimering som det också kallas, skiljer sig något från SEO på nationell nivå. Den största skillnaden är konkurrensen, den är betydligt större på nationell nivå. För många branscher är det relevant att enbart synas lokalt på online då deras tjänster eller produkter är avsedda att säljas lokalt. Om du exempelvis behöver hjälp av en rörmokare så kommer du troligtvis söka på ”rörmokare” plus din stad eftersom det är vad som är relevant för dig.
     </p>
     <p class="text-left pb-10 text-sm">
     En viktig del av lokal SEO är att tydliggöra var du befinner dig och i vilka orter du verkar på din hemsida, både för dina kunders skull, men också för Google så att sökmotorn anser din hemsida vara relevant. För att lyckas med lokal SEO behövs det även tas hänsyn till vissa tekniska åtgärder bakom hemsidan. Detta gör det lättare för Google att begripa vilka geografiska områden sökmotorn ska förknippa dig och ditt företag med.
     </p>
+
 
     <h1 class="text-2xl uppercase pb-10">
     Lokal SEO – Eskilstuna
@@ -98,16 +72,12 @@ export default function SEOEskilstuna() {
     Enligt Google består en tredjedel av alla sökningar som görs på Google av sökningar som är relaterade till platser, och därav är det mycket viktigt för dig att arbeta med lokal SEO. För att lyckas med lokal SEO är det viktigt att blad annat vara konsekvent med företagsinformationen, att använda sig av online reviews och koppla på Google reviews, samt skapa och optimera landningssidor på sin hemsida för lokala sökord.
     </p>
 
-    </div>
+    `}
 
-    </section>
-
-
-    <ContactForm/>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-
+    seoLink={"/seo-sokmotoroptimering/"}
+    seoLinkText={"Vill du veta mer om SEO?"}
+    seoLinkButton={"Klicka här"}
+    underButtonDesc={`
     <h1 class="text-2xl uppercase pb-10">
     Google My Business (GMB)
     </h1>
@@ -117,6 +87,7 @@ export default function SEOEskilstuna() {
     <p class="text-left pb-10 text-sm">
     En ytterligare fördel, förutom att synas i Googles organiska sökresultat, är att om ditt företag är registrerat på Google My Business då kan det även synas i Google Maps när någon söker efter den typ av företag som du driver. Exempelvis om någon söker på ”frisör” + en specifik plats så visas lokala företag som är registrerade i tjänsten upp på kartan. Detta gör att du får mer trafik till din webbplats, vilket i sin tur leder till högre försäljning.
     </p>
+
 
 
     <h1 class="text-2xl uppercase pb-10">
@@ -137,10 +108,10 @@ export default function SEOEskilstuna() {
     <p class="text-left pb-10 text-sm">
     Resultatet av detta SEO-arbete är att dina utvalda sökord syns bättre i det organiska sökresultatet och därmed bidrar till att trafiken till din webbplats ökar, det vill säga fler besökare, vilket i sin tur innebär högre försäljning.
     </p>
+    `}
 
-    <h1 class="text-2xl uppercase pb-10">
-    Om Eskilstuna
-    </h1>
+    endTitle={"Om Eskilstuna"}
+    endDesc={`
     <p class="text-left pb-5 text-sm">
     Eskilstuna är en tätort i Södermanland, och staden hade ett invånarantal på 
     <a href="https://www.eskilstuna.se/kommun-och-politik/fakta-statistik-och-kartor/befolkning" class="text-rose-300 underline" title="Läs mer om Eskilstuna här" rel="nofollow noopener" target="_blank"> 106 972 </a>
@@ -152,8 +123,18 @@ export default function SEOEskilstuna() {
     <p class="text-left pb-5 text-sm">
     Har du ett företag i Eskilstuna och vill att nya kunder ska hitta din hemsida? Vår SEO-byrå, Effektify hjälper dig gärna med sökmotoroptimering i Eskilstuna! Vi hjälper företag i hela Sverige att förbättra sina resultat och uppnå sina mål genom digital marknadsföring, våra tjänster inkluderar allt från SEO och Google Ads till att bygga hemsidor. Kontakta oss för en SEO-analys och annan hjälp med digitala lösningar till ditt företag i Eskilstuna (och resterande Sverige). Sökmotoroptimering/SEO kan vara krångligt, låt därför oss utföra ditt arbete med SEO åt dig!
     </p>
-    </div>
-    </section>
+    `}
+    />
+   <ContactForm 
+    title={"Vad kul att du har hittat till oss!"}
+    desc={"Har du frågor eller är redo att starta din digitala resa redan nu, är du välkommen att fylla i några rader eller maila oss på "}
+    company={"Företag"}
+    name={"Namn"}
+    phone={"Telefon"}
+    email={"E-post"}
+    text={"Vad kan vi hjälpa dig med?"}
+    buttonText={"Skicka"}
+  />
     </>
 
   )
