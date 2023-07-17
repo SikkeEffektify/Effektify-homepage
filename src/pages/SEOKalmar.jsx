961f8c9c60fb44359dece299c03bd267c76fc2d9
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-17 13:38:34 +0200

    seokal

--- src/pages/SEOKalmar.jsx
+++ src/pages/SEOKalmar.jsx
@@ -4,50 +4,36 @@ import stats from '../assets/Effektify-hjalper-foretag-med-lokal-SEO.webp'
 import stats2 from '../assets/Effektify-hjalper-foretag-med-lokal-sokmotoroptimering.webp'
 import stats3 from '../assets/Effektify-hjalper-dig-med-SEO-lokalt.webp'
 import ContactForm from '../components/ContactForm'
+import SEOComponent from '../components/SEOComponent'
 
 export default function SEOKalmar() {
   return (
     <>
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/60">
-        <h1 class='text-5xl pl-4 text-left uppercase text-white pt-10 pb-10'>Seo Kalmar<span class="text-rose-300">.</span></h1>
-        <p class='text-left text-lg text-white'>
-        Behöver du hjälp med SEO i Kalmar? SEO är komplicerat, därför finns vår byrå! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Kalmar kan vi hjälpa dig synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO. Hör av dig till oss för hjälp med att öka din trafik online och med att ha bättre synlighet på Google och andra sökmotorer. 
-        </p>   
-    </div>
-   </div>
-    </section>
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats} alt="" />
+    <SEOComponent
+    bg={bg}
+    mainTitle1={"Seo Kalmar"}
+    mainDesc1={`
+    <p class='text-left text-lg text-white'>
+        Behöver du hjälp med SEO i Kalmar? SEO är komplicerat, därför finns vår byrå! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Kalmar kan vi hjälpa dig synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO. Hör av dig till oss för hjälp med att öka din trafik online och med att ha bättre synlighet på Google och andra sökmotorer. 
+    </p>   
+    `}
+    underImage1={`
     <p class="text-left pt-5 text-base pb-10">
     Det är ingen nyhet att synlighet skapar möjligheter för företag – både genom traditionell marknadsföring och digital marknadsföring. Resultatet av att öka din synlighet online är att dina chanser att få fler besökare till din webbplats också ökar, och därmed fler kunder som sedan genererar större försäljning. Det finns många sätt att använda sig av digital marknadsföring på, och Effektify är inte enbart en SEO-byrå, vi hjälper våra kunder med allt inom digital marknadsföring, både i Kalmar och resterande Sverige. Kontakta oss om du vill vara synlig på Google (och de andra sökmotorerna), få nya kunder, driva relevant trafik till din webbplats och få ett riktigt bra resultat.
     </p>
-    </div>
-    </section>
+    `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-gray-100'>
-    <h1 class="text-2xl uppercase pb-10">
-    Hur fungerar SEO i Kalmar?
-    </h1>
+    mainTitle2={"Hur fungerar SEO i Kalmar?"}
+    mainDesc2={`
     <p class="text-left pb-10 text-sm">
     SEO står för det engelska uttrycket Search Engine Optimization, vilket översatt till svenska är sökmotoroptimering. SEO/sökmotoroptimering består av många olika delar och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på tjänster eller produkter som du erbjuder. Enkelt förklarat innebär SEO att optimera din webbplats så att den syns på de olika sökmotorerna. 
     <a href="https://effektify.com/seo-sokmotoroptimering/" class="text-rose-300" title="Sökmotoroptimering"> Syftet med SEO är att synas så högt upp på Google (och andra sökmotorer) som möjligt på sökord som är relevanta för dig och ditt företag</a>
     . Google analyserar faktorer så som huruvida din webbplats är relevant och hur den är uppbyggd, hur många, samt vilka som länkar till din webbplats och det innehåll du har på din webbplats för att sedan avgöra vilken placering du hamnar på. 
     </p>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats2} alt="" />
-  
+    `}
 
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats3} alt="" />
+    underImage2={`
     <h1 class="text-xl uppercase pb-10">
     Sökmotoroptimering
     </h1>
@@ -61,20 +47,7 @@ export default function SEOKalmar() {
     <p class="text-left pb-10 text-sm">
     SEO består huvudsakligen av tre delar, analys av sökord, on-page och off-page. En analys av sökord är viktigt för att kunna finna de specifika sökfraser som är relevanta för dig och som gynnar dig och din verksamhet. Vid denna typ av analys kollar man på konkurrenter, antal sökningar på sökordet i fråga, sökintentionen m.m. Det är viktigt att konkurrensen inte är för hög men att det fortfarande är många sökningar på sökfrasen för att det ska vara gynnsamt. On-page-optimering är också en viktig del av SEO, det är det arbete som utförs på din hemsida. On-page innebär optimering av hemsidans innehåll, bland annat texter, rubriker, meta-titlar och meta-beskrivningar på hemsidan. Tanken är att optimera dessa utifrån de utvalda sökorden. Off-page är det arbete inom SEO som görs utanför hemsidan, så som att skicka länkar till hemsidor. Det är av stor vikt att man använder sig av relevanta länkar för detta. Dessa tre delar utgör en framgångsrik SEO-strategi. 
     </p>
-    </div>
-    </section>
 
-    <section>
-    <div class='pt-20 heading-container text-center text-white bg-rose-300 pb-20'>
-    <h1 class="text-lg uppercase pb-10">
-    <a href="/seo-sokmotoroptimering/">Vill du veta mer om SEO?</a>
-    </h1>
-    <a class="vidButton" href='/seo-sokmotoroptimering/'>Klicka Här</a>
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
     <h1 class="text-2xl uppercase pb-10">
     Google utvecklas ständigt – därför måste också SEO göra det
     </h1>
@@ -88,10 +61,10 @@ export default function SEOKalmar() {
     <p class="text-left pb-10 text-sm">
     Vi på Effektify anser det vara av stor betydelse att hålla oss uppdaterade och att ständigt utvecklas inom sökmotoroptimering för att kunna erbjuda våra kunder bästa möjliga service och SEO-arbete.
     </p>
+    `}
 
-    <h1 class="text-2xl uppercase pb-10">
-    Lokal SEO – Kalmar
-    </h1>
+    mainTitle3={"Lokal SEO – Kalmar"}
+    mainDesc3={`
     <p class="text-left pb-5 text-sm">
     Lokal SEO, eller lokal sökmotoroptimering som det också kallas, skiljer sig något från SEO på nationell nivå. Den största skillnaden är konkurrensen, den är betydligt större på nationell nivå. För många branscher är det relevant att enbart synas lokalt online då deras tjänster eller produkter är avsedda att säljas lokalt. Om du exempelvis behöver hjälp av en rörmokare så kommer du troligtvis söka på ”rörmokare” plus din stad eftersom det är vad som är relevant för dig.
     </p>
@@ -102,16 +75,11 @@ export default function SEOKalmar() {
     <p class="text-left pb-10 text-sm">
     Enligt Google består en tredjedel av alla sökningar som görs på Google av sökningar som är relaterade till platser, och därav är det mycket viktigt för dig att arbeta med lokal SEO. För att lyckas med lokal SEO är det viktigt att blad annat vara konsekvent med företagsinformationen, att använda sig av online reviews och koppla på Google reviews, samt skapa och optimera landningssidor på sin hemsida för lokala sökord.
     </p>
-
-    </div>
-
-    </section>
-
-
-    <ContactForm/>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
+    `}
+    seoLink={"/seo-sokmotoroptimering/"}
+    seoLinkText={"Vill du veta mer om SEO?"}
+    seoLinkButton={"Klicka här"}
+    underButtonDesc={`
     <h1 class="text-2xl uppercase pb-10">
     Google My Business (GMB)
     </h1>
@@ -121,15 +89,15 @@ export default function SEOKalmar() {
     <p class="text-left pb-10 text-sm">
     En ytterligare fördel, förutom att synas i Googles organiska sökresultat, är att om ditt företag är registrerat på Google My Business då kan det även synas i Google Maps när någon söker efter den typ av företag som du driver. Exempelvis om någon söker på ”frisör” + en specifik plats så visas lokala företag som är registrerade i tjänsten upp på kartan. Detta gör att du får mer trafik till din webbplats, vilket i sin tur leder till högre försäljning.
     </p>
-    
-    <h1 class="text-2xl uppercase pb-10">
-    Om Kalmar
-    </h1>
+    `}
+
+    endTitle={"Om Kalmar"}
+    endDesc={`
     <p class="text-left pb-5 text-sm">
     Kalmar är en tätort belägen på Östkusten i Småland. I slutet av år 2020 uppgick antalet invånare i staden till 
     <a href="https://kalmar.se/kommun-och-politik/kommunfakta/statistik/befolkning.html" class="text-rose-300" title="Läs mer om Kalmar här" rel="nofollow noopener" target="_blank"> 70 329 </a>
     , vilket gör Kalmar till den 33:e största kommunen i Sverige. Kalmar är en av Sveriges äldsta städer, och därav finns det även mycket historia i staden. Under medeltiden användes staden som ett gränsfäste mot Danmark, staden hade en betydande hamn, var ett handelscentrum, och var en av Sveriges större städer.
-   </p>
+    </p>
     <p class="text-left pb-5 text-sm">
     Kalmar stift grundades år 1602, och danskar intog därefter slottet år 1611 och brände ner hela staden. Staden byggdes därefter upp igen, men år 1647 brann den ner igen, vilket gjorde att de styrande flyttar staden till Kvarnholmen. Därefter byggs en ringmur runt stadskärnan.
     </p>
@@ -141,9 +109,19 @@ export default function SEOKalmar() {
     </p>
 
 
+    `}
+    />
 
-    </div>
-    </section>
+<ContactForm 
+    title={"Vad kul att du har hittat till oss!"}
+    desc={"Har du frågor eller är redo att starta din digitala resa redan nu, är du välkommen att fylla i några rader eller maila oss på "}
+    company={"Företag"}
+    name={"Namn"}
+    phone={"Telefon"}
+    email={"E-post"}
+    text={"Vad kan vi hjälpa dig med?"}
+    buttonText={"Skicka"}
+  />
     </>
 
   )
