7962e7bdc6fc63131dbb427c300442d805fe8d7b
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-17 15:27:22 +0200

    seokungs

--- src/pages/SEOKungsbacka.jsx
+++ src/pages/SEOKungsbacka.jsx
@@ -4,50 +4,34 @@ import stats from '../assets/Effektify-hjalper-foretag-med-lokal-SEO.webp'
 import stats2 from '../assets/Effektify-hjalper-foretag-med-lokal-sokmotoroptimering.webp'
 import stats3 from '../assets/Effektify-hjalper-dig-med-SEO-lokalt.webp'
 import ContactForm from '../components/ContactForm'
+import SEOComponent from '../components/SEOComponent'
 
 export default function SEOKungsbacka() {
   return (
     <>
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/60">
-        <h1 class='text-5xl pl-4 text-left uppercase text-white pt-10 pb-10'>Seo Kungsbacka<span class="text-rose-300">.</span></h1>
-        <p class='text-left text-base text-white'>
-        Behöver du hjälp med SEO i Kungsbacka? Låt vår digitala media-byrå ta den rollen! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Kungsbacka kan vi hjälpa dig synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO, och många andra digitala lösningar. Om du vill få mer trafik och fler besökare till din hemsida är vår byrå det givna valet, vi ser till att få dig att synas bland sökresultaten och höja din ranking genom analys och optimering av hemsida, förbättrat innehåll samt genom att skicka länkar till din hemsida. 
-        </p>   
-    </div>
-   </div>
-    </section>
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats} alt="" />
+    <SEOComponent
+    bg={bg}
+    mainTitle1={"Seo Kungsbacka"}
+    mainDesc1={`
+    <p class='text-left text-base text-white'>
+    Behöver du hjälp med SEO i Kungsbacka? Låt vår digitala media-byrå ta den rollen! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Kungsbacka kan vi hjälpa dig synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO, och många andra digitala lösningar. Om du vill få mer trafik och fler besökare till din hemsida är vår byrå det givna valet, vi ser till att få dig att synas bland sökresultaten och höja din ranking genom analys och optimering av hemsida, förbättrat innehåll samt genom att skicka länkar till din hemsida. 
+    </p>   
+    `}
+    underImage1={`
     <p class="text-left pt-5 text-base pb-10">
     Det är ingen nyhet att synlighet skapar möjligheter för företag – både genom traditionell marknadsföring och digital marknadsföring. Att öka din digitala synlighet ökar också dina chanser att få fler besökare till din hemsida, och därmed fler kunder som sedan genererar större försäljning. Det finns många sätt att använda sig av digital marknadsföring på, och Effektify är inte enbart en SEO-byrå, vi är en digital mediabyrå, vilket innebär att vi hjälper våra kunder med allt inom digital marknadsföring, både i Kungsbacka och resterande Sverige. 
     </p>
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-gray-100'>
-    <h1 class="text-2xl uppercase pb-10">
-    Hur fungerar SEO i Kungsbacka?
-    </h1>
+    `}
+    mainTitle2={" Hur fungerar SEO i Kungsbacka?"}
+    mainDesc2={`
     <p class="text-left pb-10 text-sm">
     SEO kommer från det engelska uttrycket 
     <em> Search Engine Optimization </em>
     , vilket direkt översatt betyder sökmotoroptimering. SEO handlar i grund och botten om att optimera din hemsida för att den dyka upp för sökningar som görs på Google. Detta består av oerhört många olika delar och är en väldigt viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sina sökningar på just de tjänster eller produkter som du erbjuder. Genom att med SEO genomföra en grundlig optimering av din webbplats gör du den synlig på sökmotorer som används av besökare som letar efter sidor som din. Syftet med SEO är att ranka så högt upp på sökmotorer som möjligt på sökord som är relevanta för just dig.
     </p>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats2} alt="" />
-  
-
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats3} alt="" />
+    `}
+    underImage2={`
     <h1 class="text-xl uppercase pb-10">
     Sökmotoroptimering
     </h1>
@@ -61,23 +45,9 @@ export default function SEOKungsbacka() {
     <p class="text-left pb-10 text-sm">
     SEO består huvudsakligen av tre delar, analys av sökord, on-page och off-page. En analys av sökord är viktigt för att kunna finna de specifika sökfraser som är relevanta för dig och som gynnar dig och din verksamhet. Vid denna typ av analys kollar man på konkurrenter, antal sökningar på sökordet i fråga, sökintentionen m.m. Det är viktigt att konkurrensen inte är för hög men att det fortfarande är många sökningar på sökfrasen för att det ska vara gynnsamt. On-page-optimering är också en viktig del av SEO, det är det arbete som utförs på din hemsida. On-page innebär optimering av hemsidans innehåll, bland annat texter, rubriker, meta-titlar och meta-beskrivningar på hemsidan. Tanken är att optimera dessa utifrån de utvalda sökorden. Off-page är det arbete inom SEO som görs utanför hemsidan, så som att skicka länkar till hemsidor. Det är av stor vikt att man använder sig av relevanta länkar för detta. Dessa tre delar utgör en framgångsrik SEO-strategi. 
     </p>
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-white bg-rose-300 pb-20'>
-    <h1 class="text-lg uppercase pb-10">
-    <a href="/seo-sokmotoroptimering/">Vill du veta mer om SEO?</a>
-    </h1>
-    <a class="vidButton" href='/seo-sokmotoroptimering/'>Klicka Här</a>
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <h1 class="text-2xl uppercase pb-10">
-    Google utvecklas ständigt – därför måste också SEO göra det
-    </h1>
+    `}
+    mainTitle3={"Google utvecklas ständigt – därför måste också SEO göra det"}
+    mainDesc3={`
     <p class="text-left pb-5 text-sm">
     Det är ett faktum att företag som satsar på SEO ser en positiv skillnad på resultaten efter att denna strategi har implementerats och efter att man börjat optimera hemsidan i fråga. Arbetet med sökmotoroptimering utvecklas och ändras ständigt eftersom Google gör det, sökmotorn uppdateras ofta och därför är det av stor vikt att hänga med. 
     </p>
@@ -99,16 +69,11 @@ export default function SEOKungsbacka() {
     <p class="text-left pb-5 text-sm">
     En viktig del av lokal SEO är att tydliggöra var du befinner dig och i vilka orter du verkar på din hemsida, både för dina kunders skull, men också för Google så att sökmotorn anser din hemsida vara relevant. För att lyckas med lokal SEO behövs det även tas hänsyn till vissa tekniska åtgärder bakom hemsidan. Detta gör det lättare för Google att begripa vilka geografiska områden sökmotorn ska förknippa dig och ditt företag med.
     </p>
-
-    </div>
-
-    </section>
-
-
-    <ContactForm/>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
+    `}
+    seoLink={"/seo-sokmotoroptimering/"}
+    seoLinkText={"Vill du veta mer om SEO?"}
+    seoLinkButton={"Klicka här"}
+    underButtonDesc={`
     <h1 class="text-2xl uppercase pb-10">
     Google My Business (GMB)
     </h1>
@@ -118,10 +83,10 @@ export default function SEOKungsbacka() {
     <p class="text-left pb-10 text-sm">
     En ytterligare fördel, förutom att synas i Googles organiska sökresultat, är att om ditt företag är registrerat på Google My Business då kan det även synas i Google Maps när någon söker efter den typ av företag som du driver. Exempelvis om någon söker på ”frisör” + en specifik plats så visas lokala företag som är registrerade i tjänsten upp på kartan. Detta gör att du får mer trafik till din webbplats, vilket i sin tur leder till högre försäljning.
     </p>
-    
-    <h1 class="text-2xl uppercase pb-10">
-    Om Kungsbacka
-    </h1>
+    `}
+
+    endTitle={"Om Kungsbacka"}
+    endDesc={`
     <p class="text-left pb-5 text-sm">
     Kungsbacka är en tätort i Halland, belägen 28 km ifrån Göteborg. Kungsbacka kommun hade ett 
     <a href="https://www.kungsbacka.se/Kommun-och-politik/Fakta-om-kommunen/Befolkning/" title="Läs mer om Kungsbacka här" class="text-rose-500" rel="nofollow noopener" target="_blank"> invånarantal på 84 395 </a>
@@ -137,10 +102,9 @@ export default function SEOKungsbacka() {
     Har du ett företag i Kungsbacka och vill få mer trafik till din hemsida så att nya kunder hittar till den? Vår byrå Effektify hjälper dig gärna! Vi hjälper företag i hela Sverige att förbättra sina resultat och uppnå sina mål genom digital marknadsföring, våra tjänster inkluderar allt från SEO och Google Ads till att bygga hemsidor. Låt oss göra en analys av din hemsida för att avgöra vad för typ av optimering du skulle behöva.
     </p>
 
-
-
-    </div>
-    </section>
+    `}
+    />
+  
     </>
 
   )
