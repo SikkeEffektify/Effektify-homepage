aaafde20742ce228a18b7b66132919abae2bcacd
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-18 13:24:09 +0200

    seosundsvall

--- src/pages/SEOSundsvall.jsx
+++ src/pages/SEOSundsvall.jsx
@@ -4,53 +4,37 @@ import stats from '../assets/Effektify-hjalper-foretag-med-lokal-SEO.webp'
 import stats2 from '../assets/Effektify-hjalper-foretag-med-lokal-sokmotoroptimering.webp'
 import stats3 from '../assets/Effektify-hjalper-dig-med-SEO-lokalt.webp'
 import ContactForm from '../components/ContactForm'
+import SEOComponent from '../components/SEOComponent'
 
 export default function SEOSundsvall() {
   return (
     <>
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/60">
-        <h1 class='text-5xl pl-4 text-left uppercase text-white pt-5 pb-10'>Seo Sundsvall<span class="text-rose-300">.</span></h1>
-        <p class='text-left text-lg text-white'>
-        Behöver du hjälp med SEO i Sundsvall? SEO kan vara krångligt, vår digitala media-byrå hjälper dig gärna! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Sundsvall kan vi hjälpa dig så att du syns bättre på de olika sökmotorerna när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder.
-        Vi har lång erfarenhet och bred kunskap inom SEO/sökmotoroptimering av hemsidor, och annan digital marknadsföring.
-        </p>   
-    </div>
-   </div>
-    </section>
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats} alt="" />
+    <SEOComponent
+    bg={bg}
+    mainTitle1={"Seo Sundsvall"}
+    mainDesc1={`
+    <p class='text-left text-lg text-white'>
+    Behöver du hjälp med SEO i Sundsvall? SEO kan vara krångligt, vår digitala media-byrå hjälper dig gärna! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Sundsvall kan vi hjälpa dig så att du syns bättre på de olika sökmotorerna när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder.
+    Vi har lång erfarenhet och bred kunskap inom SEO/sökmotoroptimering av hemsidor, och annan digital marknadsföring.
+    </p>   
+    `}
+    underImage1={`
     <p class="text-left pt-5 text-base pb-10">
     Det är ingen nyhet att synlighet skapar möjligheter för företag – både genom traditionell marknadsföring och digital marknadsföring. Att öka din synlighet online ökar också dina chanser att få fler besökare till din hemsida, och därmed fler kunder som sedan genererar större försäljning. Det finns många sätt att använda sig av digital marknadsföring på, och Effektify är inte enbart en SEO-byrå, vi hjälper våra kunder med allt inom digital marknadsföring, både i Sundsvall och resterande Sverige.
     </p>
- 
-    </div>
-    </section>
+    `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-gray-100'>
-    <h1 class="text-2xl uppercase pb-10">
-    Hur fungerar SEO i Sundsvall?
-    </h1>
+    mainTitle2={"Hur fungerar SEO i Sundsvall?"}
+    mainDesc2={`
     <p class="text-left pb-5 text-sm">
     SEO står för det engelska uttrycket Search Engine Optimization, vilket översatt till svenska är sökmotoroptimering. SEO består av många olika delar och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på tjänster eller produkter som du erbjuder. 
     </p>
     <p class="text-left pb-5 text-sm">
     Enkelt förklarat innebär SEO att göra din webbplats synlig på Google och andra sökmotorer.
     </p>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats2} alt="" />
-
-
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats3} alt="" />
+    `}
+    underImage2={`
     <h1 class="text-xl uppercase pb-10">
     Sökmotoroptimering
     </h1>
@@ -68,23 +52,10 @@ export default function SEOSundsvall() {
     <a href="https://effektify.com/on-page-seo/" class="text-rose-300 " title="on-page optimering"> on-page </a>
     och off-page. En analys av sökord är viktigt för att kunna finna de specifika sökfraser som är relevanta för dig och som gynnar dig och din verksamhet. Vid denna typ av analys kollar man på konkurrenter, antal sökningar på sökordet i fråga, sökintentionen m.m. Det är viktigt att konkurrensen inte är för hög men att det fortfarande är många sökningar på sökfrasen för att det ska vara gynnsamt. On-page-optimering är också en viktig del av sökmotoroptimering, det är det arbete som utförs på din hemsida. On-page innebär optimering av hemsidans innehåll, bland annat texter, rubriker, meta-titlar och meta-beskrivningar på hemsidan. Tanken är att optimera dessa utifrån de utvalda sökorden. Off-page är det arbete inom sökmotoroptimering som görs utanför hemsidan, så som att skicka länkar till din hemsida. Dessa tre delar utgör en framgångsrik SEO-strategi.
     </p>
-    </div>
-    </section>
+    `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-white bg-rose-300 pb-20'>
-    <h1 class="text-lg uppercase pb-10">
-    <a href="/seo-sokmotoroptimering/">Vill du veta mer om SEO?</a>
-    </h1>
-    <a class="vidButton" href='/seo-sokmotoroptimering/'>Klicka Här</a>
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <h1 class="text-2xl uppercase pb-10">
-    Google utvecklas ständigt – därför måste också SEO göra det
-    </h1>
+    mainTitle3={"Google utvecklas ständigt – därför måste också SEO göra det"}
+    mainDesc3={`
     <p class="text-left pb-5 text-sm">
     Det är ett faktum att företag som satsar på sökmotoroptimering ser en positiv skillnad på resultaten efter att denna strategi har implementerats och efter att man börjat optimera hemsidan i fråga. Arbetet med sökmotoroptimering utvecklas och ändras ständigt eftersom Google gör det, sökmotorn uppdateras ofta och därför är det av stor vikt att hänga med.
     </p>
@@ -110,26 +81,22 @@ export default function SEOSundsvall() {
     Enligt Google består en tredjedel av alla sökningar som görs på Google av sökningar som är relaterade till platser, och därav är det mycket viktigt för dig att arbeta med lokal SEO. För att lyckas med lokal SEO är det viktigt att blad annat vara konsekvent med företagsinformationen, att använda sig av online reviews och koppla på Google reviews, samt skapa och optimera landningssidor på sin hemsida för lokala sökord.
     </p>
 
-    </div>
+    `}
 
-    </section>
-
-
-    <ContactForm/>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-
+    seoLink={"/seo-sokmotoroptimering/"}
+    seoLinkText={"Vill du veta mer om SEO?"}
+    seoLinkButton={"Klicka här"}
+    underButtonDesc={`
     <h1 class="text-2xl uppercase pb-10">
     Google My Business (GMB)
     </h1>
     <p class="text-left pb-10 text-sm">
     Om du driver ett lokalt företag som verkar inom ett avgränsat geografiskt område är det av stor vikt för dig att finnas på Google My Business. Det är viktigt då Google My Business är det resultat som Google visar om du söker efter lokala företag. Google visar det som en lista av de olika relevanta lokala företagen ovanför de organiska sökresultaten. Tjänsten är kostnadsfri, och är lämpligt att använda som ett skyltfönster till ditt företag. Du kan, på ett professionellt sätt, presentera ditt företag med hjälp av detta verktyg, du kan lägga till dina öppettider, adress och telefonnummer. Med denna tjänst kommer du inte enbart synas i Googles sökresultat, du kommer även synas i Google Maps. Om någon exempelvis gör en sökning på ”frisör” + ort, så syns företag som är registrerade i GMB på Google Maps. Dessa typer av kunder är ofta nära ett köpbeslut, och därför är det viktigt att du finns med bland alternativen.
     </p>
-    
-    <h1 class="text-2xl uppercase pb-10">
-    Om Sundsvall
-    </h1>
+    `}
+
+    endTitle={"Om Sundsvall"}
+    endDesc={`
     <p class="text-left pb-5 text-sm">
     Sundsvall är en tätort i Västernorrlands län, och i slutet av år 2019 hade staden
     <a href="https://sundsvall.se/kommun-och-politik/fakta-om-sundsvalls-kommun/befolkning/kommunfakta-omradesfakta-kartor/" class="text-rose-300" title="här kan du läsa mer information om Sundsvall" rel="nofollow noopener" target="_blank"> 99 449 invånare </a>
@@ -140,10 +107,19 @@ export default function SEOSundsvall() {
     </p>
     <p class="text-left pb-10 text-sm">
     Har du ett företag i Sundsvall och vill få mer relevant trafik till webbplatsen så att nya kunder hittar till den? Vi är en digital byrå med mycket erfarenhet av SEO och vi hjälper dig gärna! Vi hjälper företag i hela Sverige att förbättra sina resultat och uppnå sina mål genom digital marknadsföring, våra tjänster inkluderar allt från SEO/sökmotoroptimering och Google Ads till att bygga hemsidor. Kontakta oss för hjälp med sökmotoroptimering i Sundsvall och låt oss hjälpa dig med en analys av din hemsida för att avgöra vad för typ av optimering du skulle behöva för att synas på sökresultaten.
-    </p>
-
-    </div>
-    </section>
+    </p> 
+    `}
+    />
+   <ContactForm 
+    title={"Vad kul att du har hittat till oss!"}
+    desc={"Har du frågor eller är redo att starta din digitala resa redan nu, är du välkommen att fylla i några rader eller maila oss på "}
+    company={"Företag"}
+    name={"Namn"}
+    phone={"Telefon"}
+    email={"E-post"}
+    text={"Vad kan vi hjälpa dig med?"}
+    buttonText={"Skicka"}
+  />
     </>
 
   )
