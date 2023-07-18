ab5806538ce29c94abadbf5f028f12152573ef59
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-18 14:55:48 +0200

    seouppsala

--- src/pages/SEOUppsala.jsx
+++ src/pages/SEOUppsala.jsx
@@ -4,26 +4,22 @@ import stats from '../assets/Effektify-hjalper-foretag-med-lokal-SEO.webp'
 import stats2 from '../assets/Effektify-hjalper-foretag-med-lokal-sokmotoroptimering.webp'
 import stats3 from '../assets/Effektify-hjalper-dig-med-SEO-lokalt.webp'
 import ContactForm from '../components/ContactForm'
+import SEOComponent from '../components/SEOComponent'
 
 
 export default function SEOUppsala() {
   return (
     <>
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/60">
-        <h1 class='text-6xl pl-4 text-left uppercase text-white pb-10'>Seo Uppsala<span class="text-rose-300">.</span></h1>
-        <p class='text-left text-lg text-white'>
-        Genom att öka din synlighet online ökar dina chanser att få mer trafik till din hemsida och att få fler kunder som leder till ökad försäljning. Det finns många sätt att använda sig av digital marknadsföring men det måste göras på rätt sätt för att det ska effekt.
-        </p>   
-    </div>
-   </div>
-    </section>
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats} alt="" />
+    <SEOComponent
+    bg={bg} 
+    mainTitle1={"Seo Uppsala"}
+    mainDesc1={`
+    <p class='text-left text-lg text-white'>
+    Genom att öka din synlighet online ökar dina chanser att få mer trafik till din hemsida och att få fler kunder som leder till ökad försäljning. Det finns många sätt att använda sig av digital marknadsföring men det måste göras på rätt sätt för att det ska effekt.
+    </p>   
+    `}
+    underImage1={`
     <p class='text-left pb-10 text-sm'>
     Effektify är inte enbart en SEO-byrå utan vårt mål är att hjälpa våra kunder med allt inom digital marknadsföring, oavsett om du befinner dig i Uppsala eller någon annanstans i Sverige. Vi har bred kunskap när det kommer till sökmotoroptimering/SEO, 
     <a href="/google-ads/" class="text-rose-300 " title="Google Ads"> Google Ads </a>
@@ -39,15 +35,10 @@ export default function SEOUppsala() {
     <a href="https://effektify.com/seo-sokmotoroptimering/" class="text-rose-300" title="Sökmotoroptimering"> SEO </a>
     i Uppsala? På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Uppsala kan vi hjälpa din sida att synas för dina potentiella kunder som söker efter de produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO och erbjuder våra tjänster till alla som vill ranka högt på Googles organiska sökresultat eller som behöver hjälp med annonsering genom Google Ads eller på sociala medier. Vi säger inte bara att kan SEO, vi levererar.
     </p>
-    </div>
-
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-gray-100'>
-    <h1 class="text-2xl uppercase pb-10">
-    Hur fungerar SEO i Uppsala?
-    </h1>
+    `}
+    
+    mainTitle2={"Hur fungerar SEO i Uppsala?"}
+    mainDesc2={`
     <p class="text-left pb-5 text-sm">
     SEO, från det engelska uttrycket 
     <em> Search Engine Optimization </em>
@@ -61,12 +52,9 @@ export default function SEOUppsala() {
     <p class="text-left pb-5 text-sm">
     SEO görs ofta i tre steg; en sökordsanalys, on-page optimering och off-page optimering. Dessa delar utgör tillsammans en framgångsrik SEO-strategi och är betydande för din ranking på sökmotorerna.
     </p>
-    </div>
-    </section>
+    `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats3} alt="" />
+    underImage2={`
     <h1 class="text-xl uppercase pb-10">
     Att göra hemsidor synliga med hjälp av SEO
     </h1>
@@ -79,23 +67,11 @@ export default function SEOUppsala() {
     <p class="text-left pb-5 text-sm">
     Vi på Effektify anser det vara av stor betydelse att hålla oss uppdaterade och att ständigt utvecklas inom sökmotoroptimering och håller därför arbetet löpande för att kunna erbjuda våra kunder bästa möjliga service och för att kunna erbjuda rätt strategi som lämpar sig för just din digitala marknadsföring. Våra experter utbildas ständigt inom varje del av SEO och SEM för att alltid kunna erbjuda våra kunder det som krävs för att lyckas hålla sig relevanta i sökmotorernas ögon.
     </p>
-    </div>
-    </section>
+    `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-white bg-rose-300 pb-20'>
-    <h1 class="text-lg uppercase pb-10">
-    <a href="/seo-sokmotoroptimering/">Vill du veta mer om SEO?</a>
-    </h1>
-    <a class="vidButton" href='/seo-sokmotoroptimering/'>Klicka Här</a>
-    </div>
-    </section>
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <h1 class="text-2xl uppercase pb-10">
-    Lokal SEO – Uppsala
-    </h1>
+    mainTitle3={"Lokal SEO – Uppsala"}
+    mainDesc3={`
     <p class="text-left pb-5 text-sm">
     Lokal SEO, eller lokal sökmotoroptimering som det också kallas, skiljer sig något från SEO på nationell nivå. Den stora skillnaden är konkurrensen som är betydligt större på nationell nivå. För många branscher är det relevant att enbart synas lokalt online då deras tjänster eller produkter är avsedda att säljas lokalt. Om du exempelvis söker efter en bagare så kommer du troligtvis söka på ”bagare” tillsammans med staden i fråga eftersom det är just vad som är relevant för dig. Du är troligtvis inte på jakt efter en bagare i Göteborg när du bor i exempelvis Stockholm.
     </p>
@@ -106,8 +82,11 @@ export default function SEOUppsala() {
     Vanligtvis kräver det betydligt mindre arbete för företag som vill synas lokalt då konkurrensen begränsas i jämförelse med företag som är verksamma över hela landet. Detta har naturligtvis även att göra med vilken stad du vill synas lokalt på, då konkurrensen naturligtvis är betydligt högre i städer som Stockholm, Göteborg eller Malmö, till skillnad från mindre städer.
     </p>
 
-
-
+    `}
+    seoLink={"/seo-sokmotoroptimering/"}
+    seoLinkText={"Vill du veta mer om SEO?"}
+    seoLinkButton={"Klicka här"}
+    underButtonDesc={`
     <h1 class="text-2xl uppercase pb-10">
     Google My Business (GMB)
     </h1>
@@ -120,19 +99,10 @@ export default function SEOUppsala() {
     <p class="text-left pb-20 text-sm">
     Enligt Google består en tredjedel av alla sökningar som görs på Google av sökningar som är relaterade till platser, och därav är det mycket viktigt för dig att arbeta med lokal SEO. För att lyckas med lokal SEO är det viktigt att blad annat vara konsekvent med företagsinformationen, att använda sig av online reviews och koppla på Google reviews, samt skapa och optimera landningssidor på sin hemsida för lokala sökord.
     </p>
-    </div>
+    `}
 
-    </section>
-
-
-    <ContactForm/>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-
-    <h1 class="text-2xl uppercase pb-10">
-    Om Uppsala
-    </h1>
+    endTitle={"Om Uppsala"}
+    endDesc={`
     <p class="text-left pb-5 text-sm">
     Uppsala är en charmig stad som erbjuder mängder av saker att se. Oavsett om du lockas av konst, natur eller historia, hittar du i Uppsala ett stort urval av platser att besöka och lära dig mer om. Här hittar du Uppsala domkyrka, Skandinaviens största och högsta kyrka som började byggas år 1270 och har lockat till sig miljontals människor sedan dess.
     </p>
@@ -153,11 +123,22 @@ export default function SEOUppsala() {
     <p class="text-left pb-5 text-sm">
     Har du ett företag i Uppsala och vill att nya kunder ska hitta din hemsida? Som SEO-byrå skapar Effektify rätt förutsättningar för dig! Vi hjälper företag i hela Sverige att förbättra sina resultat och uppnå sina mål genom digital marknadsföring då våra tjänster inkluderar allt från analys av SEO och SEM till att bygga hemsidor. Kontakta oss redan idag för hjälp med SEO Uppsala!
     </p>
-    </div>
-    </section>
+    `}
+
+    />
+ 
+ <ContactForm 
+    title={"Vad kul att du har hittat till oss!"}
+    desc={"Har du frågor eller är redo att starta din digitala resa redan nu, är du välkommen att fylla i några rader eller maila oss på "}
+    company={"Företag"}
+    name={"Namn"}
+    phone={"Telefon"}
+    email={"E-post"}
+    text={"Vad kan vi hjälpa dig med?"}
+    buttonText={"Skicka"}
+  />
 
 
-    <HeadingCity3 />
     </>
 
 
