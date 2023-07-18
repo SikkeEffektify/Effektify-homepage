a906eaaae61ab1090285f71cf959e7a9cdb0f9df
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-18 15:32:52 +0200

    seovarberg

--- src/pages/SEOVarberg.jsx
+++ src/pages/SEOVarberg.jsx
@@ -4,50 +4,34 @@ import stats from '../assets/Effektify-hjalper-foretag-med-lokal-SEO.webp'
 import stats2 from '../assets/Effektify-hjalper-foretag-med-lokal-sokmotoroptimering.webp'
 import stats3 from '../assets/Effektify-hjalper-dig-med-SEO-lokalt.webp'
 import ContactForm from '../components/ContactForm'
+import SEOComponent from '../components/SEOComponent'
 
 export default function SEOVarberg() {
   return (
     <>
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/60">
-        <h1 class='text-6xl pl-4 text-left uppercase text-white pb-10'>Seo Varberg<span class="text-rose-300">.</span></h1>
-        <p class='text-left text-lg text-white'>
-        Behöver du hjälp med SEO i Varberg? Kontakta oss på Effektify! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Varberg kan vi hjälpa dig synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO och vi är det givna valet för dig som söker efter en byrå som kan hjälpa dig med optimering av din hemsida så att den syns bättre på de olika sökmotorerna. 
-        </p>   
-    </div>
-   </div>
-    </section>
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats} alt="" />
+    <SEOComponent 
+    bg={bg}
+    mainTitle1={"Seo Varberg"}
+    mainDesc1={`
+    <p class='text-left text-lg text-white'>
+    Behöver du hjälp med SEO i Varberg? Kontakta oss på Effektify! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Varberg kan vi hjälpa dig synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO och vi är det givna valet för dig som söker efter en byrå som kan hjälpa dig med optimering av din hemsida så att den syns bättre på de olika sökmotorerna. 
+    </p>   
+    `}
+    underImage1={`
     <p class="text-left pt-5 text-base pb-5">
     Det är ingen nyhet att synlighet skapar möjligheter för företag – både genom traditionell marknadsföring och digital marknadsföring. Att öka din digitala synlighet ökar också dina chanser att få fler besökare till din hemsida, och därmed fler kunder som sedan genererar större försäljning. Det finns många sätt att använda sig av digital marknadsföring på, och Effektify är inte enbart en SEO-byrå, vi är en byrå som hjälper kunder med allt inom digital marknadsföring, både i Varberg och resterande Sverige.
     </p>
-   
- 
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-gray-100'>
-    <h1 class="text-2xl uppercase pb-10">
-    Hur fungerar SEO i Varberg?
-    </h1>
+    `}
+    mainTitle2={"Hur fungerar SEO i Varberg?"}
+    mainDesc2={`
     <p class="text-left pb-5 text-sm">
     SEO står för det engelska uttrycket Search Engine Optimization, vilket översatt till svenska är sökmotoroptimering. SEO/sökmotoroptimering består av många olika delar och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på tjänster eller produkter som du erbjuder. Enkelt förklarat innebär SEO att man optimerar en webbplats för att få den att synas på Google och andra sökmotorer. 
     <a href="https://effektify.com/seo-sokmotoroptimering/" class="text-rose-300 underline" title="Sökmotoroptimering"> SEO består av många olika delar </a>
     och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på tjänster eller produkter som du erbjuder. 
     </p>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats2} alt="" />
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats3} alt="" />
+    `}
+    underImage2={`
     <h1 class="text-xl uppercase pb-10">
     Sökmotoroptimering
     </h1>
@@ -60,23 +44,9 @@ export default function SEOVarberg() {
     <p class="text-left pb-5 text-sm">
     SEO består huvudsakligen av tre delar, analys av sökord, on-page-optimering och off-page-optimering. En analys av sökord är viktigt för att kunna finna de specifika sökfraser som är relevanta för dig och som gynnar dig och din verksamhet. Vid denna typ av analys kollar man på konkurrenter, antal sökningar på sökordet i fråga, sökintentionen m.m. Det är viktigt att konkurrensen inte är för hög men att det fortfarande är många sökningar på sökfrasen för att det ska vara gynnsamt. On-page-optimering är också en viktig del av SEO, det är det arbete som utförs på din hemsida. On-page innebär optimering av hemsidans innehåll, bland annat texter, rubriker, meta-titlar och meta-beskrivningar på hemsidan. Tanken är att optimera dessa utifrån de utvalda sökorden. Off-page är det arbete inom SEO som görs utanför hemsidan, så som att skicka länkar till din hemsida. Dessa tre delar utgör en framgångsrik SEO-strategi.
     </p>
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-white bg-rose-300 pb-20'>
-    <h1 class="text-lg uppercase pb-10">
-    <a href="/seo-sokmotoroptimering/">Vill du veta mer om SEO?</a>
-    </h1>
-    <a class="vidButton" href='/seo-sokmotoroptimering/'>Klicka Här</a>
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <h1 class="text-2xl uppercase pb-10">
-    Google utvecklas ständigt – därför måste också SEO göra det
-    </h1>
+    `}
+    mainTitle3={"Google utvecklas ständigt – därför måste också SEO göra det"}
+    mainDesc3={`
     <p class="text-left pb-10 text-sm">
     Det är ett faktum att företag som satsar på SEO ser en positiv skillnad på resultaten efter att denna strategi har implementerats och efter att man börjat optimera webbplatsen i fråga. Arbetet med sökmotoroptimering utvecklas och ändras ständigt eftersom Google gör det, sökmotorn uppdateras ofta och därför är det av stor vikt att hänga med. Detta innebär att även företag som arbetar med sökmotoroptimering bör se över sin strategi regelbundet för att fortsätta vara aktuella. Vi på Effektify anser det vara av stor betydelse att hålla oss uppdaterade och att ständigt utvecklas inom sökmotoroptimering för att kunna erbjuda våra kunder bästa möjliga service. 
     </p>
@@ -91,15 +61,11 @@ export default function SEOVarberg() {
     <p class="text-left pb-10 text-sm">
     En viktig del av lokal SEO är att tydliggöra var du befinner dig och i vilka orter du verkar på din hemsida, både för dina kunders skull, men också för Google så att sökmotorn anser din hemsida vara relevant när den gör en analys av den. För att lyckas med lokal SEO behövs det även tas hänsyn till vissa tekniska åtgärder bakom hemsidan. Detta gör det lättare för Google att begripa vilka geografiska områden sökmotorn ska förknippa dig och ditt företag med.
     </p>
-    </div>
-
-    </section>
-
-
-    <ContactForm/>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
+    `}
+    seoLink={"/seo-sokmotoroptimering/"}
+    seoLinkText={"Vill du veta mer om SEO?"}
+    seoLinkButton={"Klicka här"}
+    underButtonDesc={`
     <h1 class="text-2xl uppercase pb-10">
     Google My Business (GMB)
     </h1>
@@ -109,10 +75,9 @@ export default function SEOVarberg() {
     <p class="text-left pb-10 text-sm">
     En ytterligare fördel, förutom att synas i Googles organiska sökresultat, är att om ditt företag är registrerat på Google My Business då kan det även synas i Google Maps när någon söker efter den typ av företag som du driver. Exempelvis om någon söker på ”frisör” + en specifik plats så visas lokala företag som är registrerade i tjänsten upp på kartan. Detta gör att du får mer trafik till din webbplats, vilket i sin tur leder till högre försäljning.
     </p>
-
-    <h1 class="text-2xl uppercase pb-10">
-    Om Varberg
-    </h1>
+    `}
+    endTitle={"Om Varberg"}
+    endDesc={`
     <p class="text-left pb-5 text-sm">
     Varberg är en tätort i mellersta Halland, och ligger på västkusten. I slutet av år 2020 var det  
     <a href="https://www.varberg.se/kommunpolitik/kommunfakta/befolkning.4.12d3b98d13f7ba71e5a9c0.html)Varberg" class="text-rose-300 underline" title="Läs mer om Varberg här" rel="nofollow noopener" target="_blank"> 65 397 invånare </a>
@@ -124,8 +89,19 @@ export default function SEOVarberg() {
     <p class="text-left pb-5 text-sm">
     Har du ett företag i Varberg och vill att nya kunder ska hitta din hemsida? Vi på Effektify hjälper dig gärna! Vi hjälper företag i hela Sverige att förbättra sina resultat och uppnå sina mål genom digital marknadsföring, våra tjänster inkluderar allt från SEO och Google Ads till att bygga hemsidor. Kontakta oss för hjälp med digitala lösningar till ditt företag i Varberg. SEO kan vara krångligt, och därför finns vi!
     </p>
-    </div>
-    </section>
+    `}
+    />
+
+<ContactForm 
+    title={"Vad kul att du har hittat till oss!"}
+    desc={"Har du frågor eller är redo att starta din digitala resa redan nu, är du välkommen att fylla i några rader eller maila oss på "}
+    company={"Företag"}
+    name={"Namn"}
+    phone={"Telefon"}
+    email={"E-post"}
+    text={"Vad kan vi hjälpa dig med?"}
+    buttonText={"Skicka"}
+  />
     </>
 
   )
