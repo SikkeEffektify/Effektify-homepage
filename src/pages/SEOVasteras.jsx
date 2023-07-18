7a6fd4196e9600615bc239eecf24979ed3df86ba
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-18 15:51:06 +0200

    seovasteras

--- src/pages/SEOVasteras.jsx
+++ src/pages/SEOVasteras.jsx
@@ -4,10 +4,94 @@ import stats from '../assets/Effektify-hjalper-foretag-med-lokal-SEO.webp'
 import stats2 from '../assets/Effektify-hjalper-foretag-med-lokal-sokmotoroptimering.webp'
 import stats3 from '../assets/Effektify-hjalper-dig-med-SEO-lokalt.webp'
 import ContactForm from '../components/ContactForm'
+import SEOComponent from '../components/SEOComponent'
 
 export default function SEOVasteras() {
   return (
     <>
+
+    <SEOComponent 
+    bg={bg}
+    mainTitle1={"Seo Västerås"}
+    mainDesc1={`
+    <p class='text-left text-lg text-white'>
+    Behöver du hjälp med SEO i Varberg? Kontakta oss på Effektify! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Varberg kan vi hjälpa dig synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO och vi är det givna valet för dig som söker efter en byrå som kan hjälpa dig med optimering av din hemsida så att den syns bättre på de olika sökmotorerna. 
+    </p>   
+    `}
+    underImage1={`
+    <p class="text-left pt-5 text-base pb-5">
+    Det är ingen nyhet att synlighet skapar möjligheter för företag – både genom traditionell marknadsföring och digital marknadsföring. Att öka din digitala synlighet ökar också dina chanser att få fler besökare till din hemsida, och därmed fler kunder som sedan genererar större försäljning. Det finns många sätt att använda sig av digital marknadsföring på, och Effektify är inte enbart en SEO-byrå, vi är en byrå som hjälper kunder med allt inom digital marknadsföring, både i Varberg och resterande Sverige.
+    </p>
+    `}
+    mainTitle2={"Hur fungerar SEO i Varberg?"}
+    mainDesc2={`
+    <p class="text-left pb-5 text-sm">
+    SEO står för det engelska uttrycket Search Engine Optimization, vilket översatt till svenska är sökmotoroptimering. SEO/sökmotoroptimering består av många olika delar och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på tjänster eller produkter som du erbjuder. Enkelt förklarat innebär SEO att man optimerar en webbplats för att få den att synas på Google och andra sökmotorer. 
+    <a href="https://effektify.com/seo-sokmotoroptimering/" class="text-rose-300 underline" title="Sökmotoroptimering"> SEO består av många olika delar </a>
+    och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på tjänster eller produkter som du erbjuder. 
+    </p>
+    `}
+    // fix
+    underImage2={`
+    <h1 class="text-xl uppercase pb-10">
+    Sökmotoroptimering
+    </h1>
+    <p class="text-left pb-5 text-sm">
+    Syftet med SEO är att synas så högt upp på Google (och andra sökmotorer) som möjligt på sökord som är relevanta för dig och ditt företag. 
+    </p>
+    <p class="text-left pb-5 text-sm">
+    Google analyserar faktorer så som huruvida din webbplats är relevant och hur den är uppbyggd, hur många, samt vilka som länkar till din webbplats och det innehåll du har på din webbplats för att sedan avgöra vilken placering du hamnar på.
+    </p>
+    <p class="text-left pb-5 text-sm">
+    SEO består huvudsakligen av tre delar, analys av sökord, on-page-optimering och off-page-optimering. En analys av sökord är viktigt för att kunna finna de specifika sökfraser som är relevanta för dig och som gynnar dig och din verksamhet. Vid denna typ av analys kollar man på konkurrenter, antal sökningar på sökordet i fråga, sökintentionen m.m. Det är viktigt att konkurrensen inte är för hög men att det fortfarande är många sökningar på sökfrasen för att det ska vara gynnsamt. On-page-optimering är också en viktig del av SEO, det är det arbete som utförs på din hemsida. On-page innebär optimering av hemsidans innehåll, bland annat texter, rubriker, meta-titlar och meta-beskrivningar på hemsidan. Tanken är att optimera dessa utifrån de utvalda sökorden. Off-page är det arbete inom SEO som görs utanför hemsidan, så som att skicka länkar till din hemsida. Dessa tre delar utgör en framgångsrik SEO-strategi.
+    </p>
+    `}
+    mainTitle3={"Google utvecklas ständigt – därför måste också SEO göra det"}
+    mainDesc3={`
+    <p class="text-left pb-10 text-sm">
+    Det är ett faktum att företag som satsar på SEO ser en positiv skillnad på resultaten efter att denna strategi har implementerats och efter att man börjat optimera webbplatsen i fråga. Arbetet med sökmotoroptimering utvecklas och ändras ständigt eftersom Google gör det, sökmotorn uppdateras ofta och därför är det av stor vikt att hänga med. Detta innebär att även företag som arbetar med sökmotoroptimering bör se över sin strategi regelbundet för att fortsätta vara aktuella. Vi på Effektify anser det vara av stor betydelse att hålla oss uppdaterade och att ständigt utvecklas inom sökmotoroptimering för att kunna erbjuda våra kunder bästa möjliga service. 
+    </p>
+
+
+    <h1 class="text-2xl uppercase pb-10">
+    Lokal SEO – Varberg
+    </h1>
+    <p class="text-left pb-5 text-sm">
+    Lokal SEO, eller lokal sökmotoroptimering som det också kallas, skiljer sig något från SEO på nationell nivå. Den största skillnaden är konkurrensen, den är betydligt större på nationell nivå. För många branscher är det relevant att enbart synas lokalt online då deras tjänster eller produkter är avsedda att säljas lokalt. Om du exempelvis behöver hjälp av en rörmokare så kommer du troligtvis söka på ”rörmokare” plus din stad eftersom det är vad som är relevant för dig.
+    </p>
+    <p class="text-left pb-10 text-sm">
+    En viktig del av lokal SEO är att tydliggöra var du befinner dig och i vilka orter du verkar på din hemsida, både för dina kunders skull, men också för Google så att sökmotorn anser din hemsida vara relevant när den gör en analys av den. För att lyckas med lokal SEO behövs det även tas hänsyn till vissa tekniska åtgärder bakom hemsidan. Detta gör det lättare för Google att begripa vilka geografiska områden sökmotorn ska förknippa dig och ditt företag med.
+    </p>
+    `}
+    seoLink={"/seo-sokmotoroptimering/"}
+    seoLinkText={"Vill du veta mer om SEO?"}
+    seoLinkButton={"Klicka här"}
+    underButtonDesc={`
+    <h1 class="text-2xl uppercase pb-10">
+    Google My Business (GMB)
+    </h1>
+    <p class="text-left pb-5 text-sm">
+    Om du driver ett lokalt företag som verkar inom ett avgränsat geografiskt område är det viktigt för dig att finnas på Google My Business. Det är viktigt då Google My Business är det resultat som Google visar om du söker efter lokala företag. Google visar det som en lista av de olika relevanta lokala företagen ovanför de organiska sökresultaten. Tjänsten är kostnadsfri, och dessutom fungerar den även som ett skyltfönster för ditt företag där du kan avgöra hur du vill presentera ditt företag. Du kan välja att lägga till telefonnummer, adress och öppettider exempelvis.
+    </p>
+    <p class="text-left pb-10 text-sm">
+    En ytterligare fördel, förutom att synas i Googles organiska sökresultat, är att om ditt företag är registrerat på Google My Business då kan det även synas i Google Maps när någon söker efter den typ av företag som du driver. Exempelvis om någon söker på ”frisör” + en specifik plats så visas lokala företag som är registrerade i tjänsten upp på kartan. Detta gör att du får mer trafik till din webbplats, vilket i sin tur leder till högre försäljning.
+    </p>
+    `}
+    endTitle={"Om Varberg"}
+    endDesc={`
+    <p class="text-left pb-5 text-sm">
+    Varberg är en tätort i mellersta Halland, och ligger på västkusten. I slutet av år 2020 var det  
+    <a href="https://www.varberg.se/kommunpolitik/kommunfakta/befolkning.4.12d3b98d13f7ba71e5a9c0.html)Varberg" class="text-rose-300 underline" title="Läs mer om Varberg här" rel="nofollow noopener" target="_blank"> 65 397 invånare </a>
+    i Varberg Kommun. Varberg är en turist- och handelsstad tack vare att det är beläget på västkusten.
+    </p>
+    <p class="text-left pb-5 text-sm">
+    Varberg kommun har en vision om att vara västkustens kreativa mittpunkt, och det är något som har tagits fram i nära samarbete med företag, invånare, politiker och tjänstemän. Varberg kommun ser denna vision som en ledstjärna vars syfte är att hjälpa alla gå åt samma håll, och de har även skapat fyra ledord som beskriver hur man ska jobba för att lyckas. Ledorden är nytänkande, kunskap, framåtanda och mod. Utöver detta har även två förhållningssätt tagits fram, hållbarhet och delaktighet. Hållbarhet handlar om hur man ska tillgodose dagens behov utan att äventyra nästa generations möjligheter att ekonomiskt, socialt och ekologiskt tillgodose sina behov. Delaktighet handlar om att ha ett inkluderande förhållningssätt och ett öppet klimat där mångfald, inflytande och omtanke står i fokus.
+    </p>
+    <p class="text-left pb-5 text-sm">
+    Har du ett företag i Varberg och vill att nya kunder ska hitta din hemsida? Vi på Effektify hjälper dig gärna! Vi hjälper företag i hela Sverige att förbättra sina resultat och uppnå sina mål genom digital marknadsföring, våra tjänster inkluderar allt från SEO och Google Ads till att bygga hemsidor. Kontakta oss för hjälp med digitala lösningar till ditt företag i Varberg. SEO kan vara krångligt, och därför finns vi!
+    </p>
+    `}
+    />
     <section>
    <div className='hero '>
     <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg} />
