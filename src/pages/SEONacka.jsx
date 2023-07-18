6d88bc5663dc445c8f8c1b868efeb7a4b21fa4a7
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-18 09:23:16 +0200

    seonack

--- src/pages/SEONacka.jsx
+++ src/pages/SEONacka.jsx
@@ -4,25 +4,22 @@ import stats from '../assets/Effektify-hjalper-foretag-med-lokal-SEO.webp'
 import stats2 from '../assets/Effektify-hjalper-foretag-med-lokal-sokmotoroptimering.webp'
 import stats3 from '../assets/Effektify-hjalper-dig-med-SEO-lokalt.webp'
 import ContactForm from '../components/ContactForm'
+import SEOComponent from '../components/SEOComponent'
 
 export default function SEONacka() {
   return (
     <>
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/60">
-        <h1 class='text-5xl pl-4 text-left uppercase text-white pt-10 pb-10'>Seo Nacka<span class="text-rose-300">.</span></h1>
-        <p class='text-left text-lg text-white'>
-        Genom att öka din synlighet på nätet ökar du dina chanser att få mer trafik till din hemsida. Med fler kunder som besöker dig ökar du din försäljning och det är vårt jobb som SEO-byrå att se till att ditt företag i Nacka syns bättre än dina konkurrenters. Det finns många sätt att använda sig av digital marknadsföring men det måste göras på rätt sätt för att det ska effekt.
-        </p>   
-    </div>
-   </div>
-    </section>
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats} alt="" />
+    <SEOComponent 
+    bg={bg}
+
+    mainTitle1={"Seo Nacka"}
+    mainDesc1={`
+    <p class='text-left text-lg text-white'>
+        Genom att öka din synlighet på nätet ökar du dina chanser att få mer trafik till din hemsida. Med fler kunder som besöker dig ökar du din försäljning och det är vårt jobb som SEO-byrå att se till att ditt företag i Nacka syns bättre än dina konkurrenters. Det finns många sätt att använda sig av digital marknadsföring men det måste göras på rätt sätt för att det ska effekt.
+    </p>   
+    `}
+    underImage1={`
     <p class="text-left pt-5 text-base pb-10">
     Vårt mål som SEO-byrå är att hjälpa företag i Nacka och resten av Sverige med allt inom digital marknadsföring och vi har bred kunskap när det kommer till allt från sökmotoroptimering/SEO och hemsidor till 
     <a href="https://effektify.com/google-ads/" class="text-rose-300" title="Google Ads"> annonsering med Google Ads </a>
@@ -34,14 +31,10 @@ export default function SEONacka() {
     På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Nacka kan vi hjälpa dig att synas för potentiella kunder som söker efter de produkter eller tjänster som du erbjuder. Vårt team har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO och hjälper alla som vill ranka högt på Googles organiska sökresultat med digital marknadsföring. Vi arbetar löpande för att skapa ett bra förhållande mellan oss och kunden, såväl som mellan kund och sökmotorerna.
     </p>
     </p>
-    </div>
-    </section>
+    `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-gray-100'>
-    <h1 class="text-2xl uppercase pb-10">
-    Så vad innebär SEO i Nacka?
-    </h1>
+    mainTitle2={"Så vad innebär SEO i Nacka?"}
+    mainDesc2={`
     <p class="text-left pb-5 text-sm">
     SEO kommer från det engelska uttrycket 
     <em> Search Engine Optimization </em>
@@ -55,14 +48,8 @@ export default function SEONacka() {
     <p class="text-left pb-5 text-sm">
     SEO består huvudsakligen av tre delar; en analys av sökord, on-page och off-page optimering, vilka alla är av hög vikt för din ranking på sökmotorerna.
     </p>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats2} alt="" />
-
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats3} alt="" />
+    `}
+    underImage2={`
     <h1 class="text-xl uppercase pt-5 pb-10">
     Nå ett bra resultat med SEO
     </h1>
@@ -73,25 +60,9 @@ export default function SEONacka() {
     <p class="text-left pb-10 text-sm">
     Vi på Effektify vet hur viktigt det är att hålla sig uppdaterad och att utvecklas inom sökmotoroptimering och håller därför arbetet löpande för att kunna erbjuda våra kunder bästa möjliga service och för att kunna erbjuda en strategi som lämpar sig för just din digitala marksnadsföring. Våra experter utbildas ständigt inom varje del av SEO och annonsering för att alltid kunna ge våra kunder det som krävs för att lyckas hålla sig relevanta och ranka bra på sökmotorerna.
     </p>
-
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-white bg-rose-300 pb-20'>
-    <h1 class="text-lg uppercase pb-10">
-    <a href="/seo-sokmotoroptimering/">Vill du veta mer om SEO?</a>
-    </h1>
-    <a class="vidButton" href='/seo-sokmotoroptimering/'>Klicka Här</a>
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-
-    <h1 class="text-2xl uppercase pb-10">
-    Lokal SEO – Nacka
-    </h1>
+    `}
+    mainTitle3={"Lokal SEO – Nacka"}
+    mainDesc3={`
     <p class="text-left pb-5 text-sm">
     Lokal SEO, eller lokal sökmotoroptimering som det också kallas, skiljer sig något från SEO på nationell nivå. Den stora skillnaden är konkurrensen som är betydligt större på nationell nivå. För många branscher är det relevant att enbart synas lokalt online då deras tjänster eller produkter är avsedda att säljas lokalt. Söker du efter en bilmekaniker så kommer du troligtvis söka på ”bilmekaniker” tillsammans med staden du befinner dig i eftersom det är just vad som är relevant för dig. Du är troligtvis inte på jakt efter en bilmekaniker i Malmö när du bor i Nacka.
     </p>
@@ -101,17 +72,12 @@ export default function SEONacka() {
     <p class="text-left pb-10 text-sm">
     Vanligtvis kräver det betydligt mindre arbete för företag som vill synas lokalt då konkurrensen begränsas i jämförelse med företag som är verksamma över hela landet. Detta har naturligtvis även att göra med vilken stad du vill synas lokalt på, då konkurrensen naturligtvis är betydligt högre i städer som Stockholm, Göteborg eller Malmö, till skillnad från mindre städer.
     </p>
+    `}
 
-    </div>
-
-    </section>
-
-
-    <ContactForm/>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-
+    seoLink={"/seo-sokmotoroptimering/"}
+    seoLinkText={"Vill du veta mer om SEO?"}
+    seoLinkButton={"Klicka här"}
+    underButtonDesc={`
     <h1 class="text-2xl uppercase pb-10">
     Google My Business (GMB)
     </h1>
@@ -121,22 +87,32 @@ export default function SEONacka() {
     <p class="text-left pb-10 text-sm">
     En ytterligare fördel, förutom att synas i Googles organiska sökresultat, är att om ditt företag är registrerat på Google My Business då kan det även synas i Google Maps när någon söker efter den typ av företag som du driver. Exempelvis om någon söker på ”frisör” + en specifik plats så visas lokala företag som är registrerade i tjänsten upp på kartan. Detta gör att du får mer trafik till din webbplats, vilket i sin tur leder till högre försäljning.
     </p>
-    
-    <h1 class="text-2xl uppercase pb-10">
-    Om Nacka
-    </h1>
+    `}
+
+    endTitle={"Om Nacka"}
+    endDesc={`
     <p class="text-left pb-5 text-sm">
     Nacka är en kommun i Stockholms län med mängder av fina naturreservat. Fram till början av 1990-talet fanns många stora industriföretag i kommunen, men numera består näringslivet av service och handel. I Nacka kommun finns två stora köpcentrum; Sickla köpkvarter och Nacka forum.
-   </p>
+    </p>
     <p class="text-left pb-5 text-sm">
     Här hittar du den populära guidade turen genom Atlas Copros gruva där du under drygt 1,5 timmar kan uppleva utvecklingen av den industriella revolutionen. Sveriges första slagfältsmuseum, Historiearvsmuseet, hittar du också i Nacka. Platsen berättar en spännande historia med hjälp av bilder och filmer och är värt ett besök om du inte besökt den tidigare. Även Stockholms Läns Museum ligger i Nacka kommun och erbjuder nyfikna besökare historian om länet.
     </p>
     <p class="text-left pb-10 text-sm">
     Är du på jakt efter roliga aktiviteter hittar du i Nacka allt från golfbanor och badplatser till vandringar i fina miljöer och paddling. Här finns allt för att du ska trivas med naturen nära till hands. Ett av Sveriges äldsta bevarade nöjen under sommaren, Svindervik, är en exklusiv föregångare till dagens sommarstugor. Här kan du spendera dagen på en historisk plats som sträcker sig så långt som från 1700-talet.
     </p>
+    `}
+    />
 
-    </div>
-    </section>
+<ContactForm 
+    title={"Vad kul att du har hittat till oss!"}
+    desc={"Har du frågor eller är redo att starta din digitala resa redan nu, är du välkommen att fylla i några rader eller maila oss på "}
+    company={"Företag"}
+    name={"Namn"}
+    phone={"Telefon"}
+    email={"E-post"}
+    text={"Vad kan vi hjälpa dig med?"}
+    buttonText={"Skicka"}
+  />
     </>
 
   )
