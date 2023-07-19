07f1cb180731afbe3c6e7a4f2eaf71fe9eefc8f3
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-19 09:54:49 +0200

    seoorebro

--- src/pages/SEOOrebro.jsx
+++ src/pages/SEOOrebro.jsx
@@ -4,82 +4,57 @@ import stats from '../assets/Effektify-hjalper-foretag-med-lokal-SEO.webp'
 import stats2 from '../assets/Effektify-hjalper-foretag-med-lokal-sokmotoroptimering.webp'
 import stats3 from '../assets/Effektify-hjalper-dig-med-SEO-lokalt.webp'
 import ContactForm from '../components/ContactForm'
+import SEOComponent from '../components/SEOComponent'
 
 export default function SEOOrebro() {
   return (
     <>
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/60">
-        <h1 class='text-5xl pl-4 text-left uppercase text-white pt-5 pb-10'>Seo Örebro<span class="text-rose-300">.</span></h1>
-        <p class='text-left text-base text-white'>
-        Behöver du hjälp med SEO i Örebro? SEO är komplicerat, därför finns vår byrå! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Örebro kan vi hjälpa dig synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder på din e-handel. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO. Örebro är bara en av alla städer som vi verkar i, kontakta oss för hjälp med att öka din trafik online och bästa möjliga ranking på sökmotorerna. 
-        </p>   
-    </div>
-   </div>
-    </section>
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats} alt="" />
-    <p class="text-left pt-5 text-base pb-10">
+  <SEOComponent 
+  bg={bg}
+  mainTitle1={"Seo Örebro"}
+  mainDesc1={`
+  <p class='text-left text-base text-white'>
+    Behöver du hjälp med SEO i Örebro? SEO är komplicerat, därför finns vår byrå! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Örebro kan vi hjälpa dig synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder på din e-handel. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO. Örebro är bara en av alla städer som vi verkar i, kontakta oss för hjälp med att öka din trafik online och bästa möjliga ranking på sökmotorerna. 
+  </p>   
+  `}
+  underImage1={`
+  <p class="text-left pt-5 text-base pb-10">
     Det är ingen nyhet att synlighet skapar möjligheter för företag – både genom traditionell marknadsföring och digital marknadsföring. Att öka din synlighet online ökar också dina chanser att få fler besökare till din webbplats, och därmed fler kunder som sedan genererar större försäljning. Det finns många sätt att använda sig av digital marknadsföring på, och Effektify är inte enbart en SEO-byrå, vi hjälper våra kunder med allt inom digital marknadsföring, så som att bygga hemsidor och Google Ads, både i Örebro och resterande Sverige. 
-    </p>
-    </div>
-    </section>
+  </p>
+  `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-gray-100'>
-    <h1 class="text-2xl uppercase pb-10">
-    Hur fungerar SEO i Örebro?
-    </h1>
-    <p class="text-left pb-5 text-sm">
-    <a href="https://effektify.com/seo-sokmotoroptimering/" class="text-rose-300" title="SEO">SEO </a>
-    är en förkortning av det engelska uttrycket Search Engine Optimization, vilket översatt till svenska är sökmotoroptimering. SEO/sökmotoroptimering består av många olika delar och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på tjänster eller produkter som du erbjuder. Enkelt förklarat innebär SEO att optimera webbplatsen så att den syns bland sökresultaten på de olika sökmotorerna. Syftet med SEO är att synas så högt upp på Google (och andra sökmotorerna) som möjligt på sökord som är relevanta för dig och ditt företag. Sökmotorer analyserar faktorer så som huruvida din webbplats är relevant och hur den är uppbyggd, hur många, samt vilka som länkar till din webbplats och det innehåll du har på din webbplats för att sedan avgöra vilken placering du hamnar på.
-    </p>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats2} alt="" />
+  mainTitle2={"Hur fungerar SEO i Örebro?"}
+  mainDesc2={`
+  <p class="text-left pb-5 text-sm">
+  <a href="https://effektify.com/seo-sokmotoroptimering/" class="text-rose-300" title="SEO">SEO </a>
+  är en förkortning av det engelska uttrycket Search Engine Optimization, vilket översatt till svenska är sökmotoroptimering. SEO/sökmotoroptimering består av många olika delar och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på tjänster eller produkter som du erbjuder. Enkelt förklarat innebär SEO att optimera webbplatsen så att den syns bland sökresultaten på de olika sökmotorerna. Syftet med SEO är att synas så högt upp på Google (och andra sökmotorerna) som möjligt på sökord som är relevanta för dig och ditt företag. Sökmotorer analyserar faktorer så som huruvida din webbplats är relevant och hur den är uppbyggd, hur många, samt vilka som länkar till din webbplats och det innehåll du har på din webbplats för att sedan avgöra vilken placering du hamnar på.
+  </p>
+  `}
 
-    </div>
-    </section>
+  underImage2={`
+  <h1 class="text-xl uppercase pb-10">
+  Sökmotoroptimering
+  </h1>
+  <p class="text-left pb-5 text-sm">
+  Syftet med 
+  <a href="https://effektify.com/seo-sokmotoroptimering/" class="text-rose-300" title="SEO"> SEO </a>
+  är att synas så högt upp på Google (och andra sökmotorer) som möjligt på sökord som är relevanta för dig och ditt företag. 
+  </p>
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats3} alt="" />
-    <h1 class="text-xl uppercase pb-10">
-    Sökmotoroptimering
-    </h1>
-    <p class="text-left pb-5 text-sm">
-    Syftet med 
-    <a href="https://effektify.com/seo-sokmotoroptimering/" class="text-rose-300" title="SEO"> SEO </a>
-    är att synas så högt upp på Google (och andra sökmotorer) som möjligt på sökord som är relevanta för dig och ditt företag. 
-    </p>
-  
-    <p class="text-left pb-5 text-sm">
-    Google analyserar faktorer så som huruvida din webbplats är relevant och hur den är uppbyggd, hur många, samt vilka som länkar till din webbplats och det innehåll du har på din webbplats för att sedan avgöra vilken placering du hamnar på.
-    </p>
-    <p class="text-left pb-10 text-sm">
-    SEO består huvudsakligen av tre delar, analys av sökord, 
-    <a href="https://effektify.com/on-page-seo/" class="text-rose-300" title="on-page seo"> on-page </a>
-    och off-page. En analys av sökord är viktigt för att kunna finna de specifika sökfraser som är relevanta för dig och som gynnar dig och din verksamhet. Vid denna typ av analys kollar man på konkurrenter, antal sökningar på sökordet i fråga, huruvida sökintentionen är bra, om sökordet når din målgrupp m.m. Det är viktigt att konkurrensen inte är för hög men att det fortfarande är många sökningar på sökfrasen för att det ska vara gynnsamt att fokusera på sökordet i fråga. On-page-optimering är också en viktig del av sökmotoroptimering, det är det arbete som utförs på din hemsida. On-page innebär optimering av hemsidans innehåll, bland annat texter, rubriker, meta-titlar och meta-beskrivningar på hemsidan. Tanken är att optimera detta innehåll utifrån de utvalda sökorden. Off-page är det arbete inom sökmotoroptimering som görs utanför hemsidan, så som att skicka rätt och relevanta länkar till din hemsida. Dessa tre delar utgör en framgångsrik SEO-strategi. 
-    </p>
-    </div>
-    </section>
+  <p class="text-left pb-5 text-sm">
+  Google analyserar faktorer så som huruvida din webbplats är relevant och hur den är uppbyggd, hur många, samt vilka som länkar till din webbplats och det innehåll du har på din webbplats för att sedan avgöra vilken placering du hamnar på.
+  </p>
+  <p class="text-left pb-10 text-sm">
+  SEO består huvudsakligen av tre delar, analys av sökord, 
+  <a href="https://effektify.com/on-page-seo/" class="text-rose-300" title="on-page seo"> on-page </a>
+  och off-page. En analys av sökord är viktigt för att kunna finna de specifika sökfraser som är relevanta för dig och som gynnar dig och din verksamhet. Vid denna typ av analys kollar man på konkurrenter, antal sökningar på sökordet i fråga, huruvida sökintentionen är bra, om sökordet når din målgrupp m.m. Det är viktigt att konkurrensen inte är för hög men att det fortfarande är många sökningar på sökfrasen för att det ska vara gynnsamt att fokusera på sökordet i fråga. On-page-optimering är också en viktig del av sökmotoroptimering, det är det arbete som utförs på din hemsida. On-page innebär optimering av hemsidans innehåll, bland annat texter, rubriker, meta-titlar och meta-beskrivningar på hemsidan. Tanken är att optimera detta innehåll utifrån de utvalda sökorden. Off-page är det arbete inom sökmotoroptimering som görs utanför hemsidan, så som att skicka rätt och relevanta länkar till din hemsida. Dessa tre delar utgör en framgångsrik SEO-strategi. 
+  </p>
+  `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-white bg-rose-300 pb-20'>
-    <h1 class="text-lg uppercase pb-10">
-    <a href="/seo-sokmotoroptimering/">Vill du veta mer om SEO?</a>
-    </h1>
-    <a class="vidButton" href='/seo-sokmotoroptimering/'>Klicka Här</a>
-    </div>
-    </section>
+  mainTitle3={"Google utvecklas ständigt – därför måste också SEO göra det"}
+  mainDesc3={`
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <h1 class="text-2xl uppercase pb-10">
-    Google utvecklas ständigt – därför måste också SEO göra det
-    </h1>
     <p class="text-left pb-10 text-sm">
     Det är ett faktum att företag som satsar på sökmotoroptimering ser en positiv skillnad på resultaten efter att denna strategi har implementerats och efter att man börjat optimera hemsidan i fråga. Arbetet med sökmotoroptimering utvecklas och ändras ständigt eftersom Google gör det, sökmotorn uppdateras ofta och därför är det av stor vikt att hänga med. Detta innebär att även företag som arbetar med sökmotoroptimering bör se över sin strategi regelbundet för att fortsätta vara aktuella. Vi på Effektify anser det vara av stor betydelse att hålla oss uppdaterade och att ständigt utvecklas inom sökmotoroptimering för att kunna erbjuda våra kunder bästa möjliga service och rådgivning. 
     </p>
@@ -93,29 +68,26 @@ export default function SEOOrebro() {
     <p class="text-left pb-10 text-sm">
     En viktig del av lokal SEO är att tydliggöra var du befinner dig och i vilka orter du verkar på din hemsida, både för dina kunders skull, men också för Google så att sökmotorn anser din hemsida vara relevant. För att lyckas med lokal SEO behövs det även tas hänsyn till vissa tekniska åtgärder bakom hemsidan. Detta gör det lättare för Google att begripa vilka geografiska områden sökmotorn ska förknippa dig och ditt företag med.
     </p>
+  `}
 
-    </div>
-
-    </section>
-
-
-    <ContactForm/>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <h1 class="text-2xl uppercase pb-10">
+  seoLink={"/seo-sokmotoroptimering/"}
+  seoLinkText={"Vill du veta mer om SEO?"}
+  seoLinkButton={"Klicka här"}
+  underButtonDesc={`
+  <h1 class="text-2xl uppercase pb-10">
     Google My Business (GMB)
-    </h1>
+  </h1>
     <p class="text-left pb-5 text-sm">
     Om du driver ett lokalt företag som verkar inom ett avgränsat geografiskt område är det viktigt för dig att finnas på Google My Business. Det är viktigt då Google My Business är det resultat som Google visar om du söker efter lokala företag. Google visar det som en lista av de olika relevanta lokala företagen ovanför de organiska sökresultaten. Tjänsten är kostnadsfri, och dessutom fungerar den även som ett skyltfönster för ditt företag där du kan avgöra hur du vill presentera ditt företag. Du kan välja att lägga till telefonnummer, adress och öppettider exempelvis.
-    </p>
+  </p>
     <p class="text-left pb-10 text-sm">
     En ytterligare fördel, förutom att synas i Googles organiska sökresultat, är att om ditt företag är registrerat på Google My Business då kan det även synas i Google Maps när någon söker efter den typ av företag som du driver. Exempelvis om någon söker på ”frisör” + en specifik plats så visas lokala företag som är registrerade i tjänsten upp på kartan. Detta gör att du får mer trafik till din webbplats, vilket i sin tur leder till högre försäljning.
-    </p>
-    
-    <h1 class="text-2xl uppercase pb-10">
-    Om Örebro
-    </h1>
+  </p>
+  `}
+  
+
+  endTitle={"Om Örebro"}
+  endDesc={`
     <p class="text-left pb-5 text-sm">
     Örebro är en tätort i Närke, och i slutet av år 2020 bodde det
     <a href="https://www.orebro.se/fordjupning/fordjupning/fakta-statistik-priser--utmarkelser/statistik/befolkningsprognoser.html#" class='text-rose-300' title="Läs mer om Örebro här" rel="nofollow noopener" target="_blank"> 156 381 invånare </a>
@@ -132,9 +104,19 @@ export default function SEOOrebro() {
     Har du ett företag i Örebro och vill öka din digitala trafik? Vi på Effektify hjälper dig gärna! Vi hjälper företag i hela Sverige att förbättra sina resultat, uppnå sina mål och få så bra ranking som möjligt genom digital marknadsföring. Våra tjänster inkluderar allt från SEO och Google Ads till att bygga hemsidor. Kontakta oss för hjälp med digitala lösningar och för att få så bra resultat som möjligt.
     </p>
 
+  `}
+  />
 
-    </div>
-    </section>
+<ContactForm 
+    title={"Vad kul att du har hittat till oss!"}
+    desc={"Har du frågor eller är redo att starta din digitala resa redan nu, är du välkommen att fylla i några rader eller maila oss på "}
+    company={"Företag"}
+    name={"Namn"}
+    phone={"Telefon"}
+    email={"E-post"}
+    text={"Vad kan vi hjälpa dig med?"}
+    buttonText={"Skicka"}
+  />
     </>
 
   )
