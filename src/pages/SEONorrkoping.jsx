c05ba497d6866aa4908355973ba5d32fce094c01
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-19 11:22:01 +0200

    seonorrkoping

--- src/pages/SEONorrkoping.jsx
+++ src/pages/SEONorrkoping.jsx
@@ -4,48 +4,36 @@ import stats from '../assets/Effektify-hjalper-foretag-med-lokal-SEO.webp'
 import stats2 from '../assets/Effektify-hjalper-foretag-med-lokal-sokmotoroptimering.webp'
 import stats3 from '../assets/Effektify-hjalper-dig-med-SEO-lokalt.webp'
 import ContactForm from '../components/ContactForm'
+import SEOComponent from '../components/SEOComponent'
 
 export default function SEONorrkoping() {
   return (
     <>
-    <section>
-   <div className='hero '>
-    <img class="min-w-full min-h-full absolute object-cover hero-image" src={bg} />
-    <div class="vidContent2 hero-image-text pb-40  bg-black/60">
-        <h1 class='text-5xl pl-4 text-left uppercase text-white pt-5 pb-10'>Seo Norrköping<span class="text-rose-300">.</span></h1>
-        <p class='text-left text-base text-white'>
-        Behöver du hjälp med SEO i Norrköping? SEO kan vara krångligt, kontakta därför vår SEO-byrå, Effektify! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Norrköping kan vi hjälpa dig och din verksamhet synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO och vi är det givna valet för dig som letar efter en SEO-byrå som kan hjälpa dig med optimering av din webbplats så att den syns bättre på de olika sökmotorerna.Vi har lång erfarenhet av att hjälpa kunder med att förbättra deras hemsidor och dess synlighet på Google (och andra sökmotorer). 
-        </p>   
-    </div>
-   </div>
-    </section>
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats} alt="" />
+    <SEOComponent 
+    bg={bg}
+    mainTitle1={"Seo Norrköping"}
+    mainDesc1={`
+    <p class='text-left text-base text-white'>
+    Behöver du hjälp med SEO i Norrköping? SEO kan vara krångligt, kontakta därför vår SEO-byrå, Effektify! På samma sätt som du lyckades hitta vår hemsida när du sökte efter SEO Norrköping kan vi hjälpa dig och din verksamhet synas när dina potentiella kunder söker efter produkter eller tjänster som du erbjuder. Vi har lång erfarenhet och bred kunskap inom sökmotoroptimering/SEO och vi är det givna valet för dig som letar efter en SEO-byrå som kan hjälpa dig med optimering av din webbplats så att den syns bättre på de olika sökmotorerna.Vi har lång erfarenhet av att hjälpa kunder med att förbättra deras hemsidor och dess synlighet på Google (och andra sökmotorer). 
+    </p>   
+    `}
+
+    underImage1={`
     <p class="text-left pt-5 text-base pb-10">
     Det är ingen nyhet att synlighet skapar möjligheter för företag – både genom traditionell marknadsföring och digital marknadsföring. Att öka din digitala synlighet ökar också dina chanser att få fler besökare till din webbplats, och därmed fler kunder som sedan genererar större försäljning. Det finns många sätt att arbeta med digital marknadsföring på, och Effektify är inte enbart en SEO-byrå, vi är en webbyrå som arbetar med allt inom digital marknadsföring, både i Norrköping och resterande Sverige. Kontakta oss så hjälper vi dig nå de resultat du önskar för din webbplats.
     </p>
-    </div>
-    </section>
+    `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-gray-100'>
-    <h1 class="text-2xl uppercase pb-10">
-    Hur fungerar SEO i Norrköping?
-    </h1>
+    mainTitle2={"Hur fungerar SEO i Norrköping?"}
+    mainDesc2={`
     <p class="text-left pb-5 text-sm">
     <a href="https://effektify.com/seo-sokmotoroptimering/" class="text-rose-300" title="SEO">SEO </a>
     är en förkortning av det engelska uttrycket Search Engine Optimization, vilket översatt till svenska är sökmotoroptimering. SEO/sökmotoroptimering består av många olika delar och är en viktig faktor för dig som vill att dina potentiella kunder ska hitta din webbplats när de gör sökningar på tjänster eller produkter som du erbjuder. Enkelt förklarat innebär SEO att optimera webbplatsen så att den syns bland sökresultaten på de olika sökmotorerna. Syftet med SEO är att synas så högt upp på Google (och andra sökmotorerna) som möjligt på sökord som är relevanta för dig och ditt företag. Sökmotorer analyserar faktorer så som huruvida din webbplats är relevant och hur den är uppbyggd, hur många, samt vilka som länkar till din webbplats och det innehåll du har på din webbplats för att sedan avgöra vilken placering du hamnar på.
     </p>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats2} alt="" />
+    `}
 
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <img class="mx-auto pt-5 drop-shadow-lg pb-10" src={stats3} alt="" />
+    underImage2={`
     <h1 class="text-xl uppercase pb-10">
     Sökmotoroptimering
     </h1>
@@ -63,23 +51,10 @@ export default function SEONorrkoping() {
     <a href="https://effektify.com/on-page-seo/" class="text-rose-300" title="on-page seo"> on-page </a>
     och off-page. En analys av sökord är viktigt för att kunna finna de specifika sökfraser som är relevanta för dig och som gynnar dig och din verksamhet. Vid denna typ av analys kollar man på konkurrenter, antal sökningar på sökordet i fråga, huruvida sökintentionen är bra, om sökordet når din målgrupp m.m. Det är viktigt att konkurrensen inte är för hög men att det fortfarande är många sökningar på sökfrasen för att det ska vara gynnsamt att fokusera på sökordet i fråga. On-page-optimering är också en viktig del av sökmotoroptimering, det är det arbete som utförs på din hemsida. On-page innebär optimering av hemsidans innehåll, bland annat texter, rubriker, meta-titlar och meta-beskrivningar på hemsidan. Tanken är att optimera detta innehåll utifrån de utvalda sökorden. Off-page är det arbete inom sökmotoroptimering som görs utanför hemsidan, så som att skicka rätt och relevanta länkar till din hemsida. Dessa tre delar utgör en framgångsrik SEO-strategi. 
     </p>
-    </div>
-    </section>
+    `}
 
-    <section>
-    <div class='pt-20 heading-container text-center text-white bg-rose-300 pb-20'>
-    <h1 class="text-lg uppercase pb-10">
-    <a href="/seo-sokmotoroptimering/">Vill du veta mer om SEO?</a>
-    </h1>
-    <a class="vidButton" href='/seo-sokmotoroptimering/'>Klicka Här</a>
-    </div>
-    </section>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
-    <h1 class="text-2xl uppercase pb-10">
-    Google utvecklas ständigt – därför måste också SEO göra det
-    </h1>
+    mainTitle3={"Google utvecklas ständigt – därför måste också SEO göra det"}
+    mainDesc3={`
     <p class="text-left pb-10 text-sm">
     Det är ett faktum att företag som satsar på sökmotoroptimering ser en positiv skillnad på resultaten efter att denna strategi har implementerats och efter att man börjat optimera hemsidan i fråga. Arbetet med sökmotoroptimering utvecklas och ändras ständigt eftersom Google gör det, sökmotorn uppdateras ofta och därför är det av stor vikt att hänga med. Detta innebär att även företag som arbetar med sökmotoroptimering bör se över sin strategi regelbundet för att fortsätta vara aktuella. Vi på Effektify anser det vara av stor betydelse att hålla oss uppdaterade och att ständigt utvecklas inom sökmotoroptimering för att kunna erbjuda våra kunder bästa möjliga service och rådgivning. 
     </p>
@@ -101,16 +76,13 @@ export default function SEONorrkoping() {
     <p class="text-left pb-10 text-sm">
     Innehållet på din hemsida är en viktig del av SEO då det ska vara relevant för att driva digital trafik till din hemsida, men samtidigt måste man också arbeta med att implementera relevanta sökord i texten för att få hemsidan att ranka högre. Det är även viktigt att arbeta utifrån sin målgrupp och se till att webbplatsen är riktad mot den. Det är mycket att tänka på, därför hjälper vi dig gärna med detta och alla andra delar av sökmotoroptimering i Norrköping.
     </p>
+    `}
 
-    </div>
+    seoLink={"/seo-sokmotoroptimering/"}
+    seoLinkText={"Vill du veta mer om SEO?"}
+    seoLinkButton={"Klicka här"}
 
-    </section>
-
-
-    <ContactForm/>
-
-    <section>
-    <div class='pt-20 heading-container text-center text-black bg-white'>
+    underButtonDesc={`
     <h1 class="text-2xl uppercase pb-10">
     Google My Business (GMB)
     </h1>
@@ -121,9 +93,11 @@ export default function SEONorrkoping() {
     En ytterligare fördel, förutom att synas i Googles organiska sökresultat, är att om ditt företag är registrerat på Google My Business då kan det även synas i Google Maps när någon söker efter den typ av företag som du driver. Exempelvis om någon söker på ”frisör” + en specifik plats så visas lokala företag som är registrerade i tjänsten upp på kartan. Detta gör att du får mer trafik till din webbplats, vilket i sin tur leder till högre försäljning.
     </p>
     
-    <h1 class="text-2xl uppercase pb-10">
-    Om Norrköping
-    </h1>
+    `}
+
+
+    endTitle={"Om Norrköping"}
+    endDesc={`
     <p class="text-left pb-5 text-sm">
     Norrköping är en tätort i Östergötland, och invånarantalet i staden uppgick till
     <a href="https://www.norrkoping.se/organisation/statistik/statistik-befolkning/befolkningsutveckling-norrkoping-manadsdata" class='text-rose-300' title="Läs mer om Norrköping här" rel="nofollow noopener" target="_blank"> 143 640 </a>
@@ -139,10 +113,19 @@ export default function SEONorrkoping() {
     <p class="text-left pb-10 text-sm">
     Har du ett företag i Norrköping och vill att nya kunder ska hitta din hemsida? Vår webbyrå hjälper dig gärna med sökmotoroptimering i Norrköping! Vi hjälper företag i hela Sverige att förbättra sina resultat och uppnå sina mål genom digital marknadsföring, våra tjänster inkluderar allt från SEO och Google Ads till att bygga hemsidor. Kontakta oss för hjälp med digitala lösningar till ditt företag i Norrköping. Sökmotoroptimering/SEO kan vara krångligt, låt därför oss utföra det arbetet åt dig!
     </p>
+    `}
+    />
 
-
-    </div>
-    </section>
+<ContactForm 
+    title={"Vad kul att du har hittat till oss!"}
+    desc={"Har du frågor eller är redo att starta din digitala resa redan nu, är du välkommen att fylla i några rader eller maila oss på "}
+    company={"Företag"}
+    name={"Namn"}
+    phone={"Telefon"}
+    email={"E-post"}
+    text={"Vad kan vi hjälpa dig med?"}
+    buttonText={"Skicka"}
+  />
     </>
 
   )
