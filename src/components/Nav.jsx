8035183a8c30526e7cae91a9e88f7dad67815f50
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-20 15:34:17 +0200

    NavbarDropdown progress

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -19,7 +19,7 @@ const Nav = () => {
        <img src={burgericon} alt="" />
     </button>
    
-<div id="dropdown" class="z-10 hidden divide-y rounded-lg shadow  navbarDrop">
+<div id="dropdown" class="z-10 hidden rounded-lg shadow navbarDrop">
     <ul class="py-10 text-sm  text-white" aria-labelledby="dropdownDefaultButton">
         <h1 className='text-rose-300 text-2xl px-4 pb-1 '><span className='text-white'>Search</span>.</h1>
         <hr class="w-48 h-0.5 m-2 bg-gray-100 border-0 rounded dark:bg-gray-700" ></hr>
@@ -28,6 +28,36 @@ const Nav = () => {
       </li>
       <li class="pt-3">
         <a href="#" class="block px-4 py-2 text-base"><i class="fa-solid fa-chart-column text-sky-400 pr-2"></i>Google ads</a>
+      </li>
+    </ul>
+    <ul class="pb-5 text-sm  text-white" aria-labelledby="dropdownDefaultButton">
+        <h1 className='text-rose-300 text-2xl px-4 pb-1 '><span className='text-white'>Paid social</span>.</h1>
+        <hr class="w-48 h-0.5 m-2 bg-gray-100 border-0 rounded dark:bg-gray-700" ></hr>
+      <li class="pt-3">
+        <a href="#" class="block px-4 py-2 text-base "><i class="fa-brands fa-facebook text-blue-400 pr-2"></i>Facebook Ads</a>
+      </li>
+      <li class="pt-3">
+        <a href="#" class="block px-4 py-2 text-base "><i class="fa-brands fa-instagram text-pink-300 pr-2"></i>Instagram Ads</a>
+      </li>
+      <li class="pt-3">
+        <a href="#" class="block px-4 py-2 text-base "><i class="fa-brands fa-linkedin text-blue-500 pr-2"></i>Linkedin Ads</a>
+      </li>
+      <li class="pt-3">
+        <a href="#" class="block px-4 py-2 text-base "><i class="fa-brands fa-youtube text-red-500 pr-1"></i>Youtube Ads</a>
+      </li>
+      <li class="pt-3">
+        <a href="#" class="block px-4 py-2 text-base "><i class="fa-brands fa-tiktok text-white pr-2"></i>Tiktok Ads</a>
+      </li>
+     
+    </ul>
+    <ul class="pb-5 text-sm  text-white" aria-labelledby="dropdownDefaultButton">
+        <h1 className='text-rose-300 text-2xl px-4 pb-1 '><span className='text-white'>WEB & E-COM</span>.</h1>
+        <hr class="w-48 h-0.5 m-2 bg-gray-100 border-0 rounded dark:bg-gray-700" ></hr>
+      <li class="pt-3">
+        <a href="#" class="block px-4 py-2 text-base "><i class="fa-brands fa-wordpress text-sky-600 pr-2"></i>Wordpress</a>
+      </li>
+      <li class="pt-3">
+        <a href="#" class="block px-4 py-2 text-base"><i class="fa-brands fa-shopify text-green-400 pr-2"></i>Shopify</a>
       </li>
     </ul>
 </div>
