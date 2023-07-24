54ee558b95f9425388c4ab423d1d22ecede7e167
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-24 11:58:51 +0200

    repsonsive progress

--- src/components/SEOComponent.jsx
+++ src/components/SEOComponent.jsx
@@ -10,8 +10,8 @@ export default function SEOComponent({mainTitle1, mainDesc1, underImage1, mainTi
    <div className='hero '>
     
     <img class=" min-w-full min-h-full absolute object-cover  hero-image" src={bg} />
-    <div class="vidContent2 hero-image-text bg-black/60  ">
-        <h1 class='text-5xl pl-4 text-left uppercase text-white pt-20  pb-10'>{mainTitle1}<span class="text-rose-300">.</span></h1>
+    <div class="vidContentSEO hero-image-text bg-black/60 md:pb-40">
+        <h1 class='text-5xl pl-4 text-left uppercase text-white pb-10'>{mainTitle1}<span class="text-rose-300">.</span></h1>
          <div dangerouslySetInnerHTML={{__html: mainDesc1}}></div>
         
          
