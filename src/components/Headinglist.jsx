f8bc0f1ddc53276dd8bdee4206a04bfed1d097e8
Mattias Sikmar <mattias.sikmar@effektify.com>
2023-07-26 10:52:23 +0200

    glitch fix

--- src/components/Headinglist.jsx
+++ src/components/Headinglist.jsx
@@ -30,109 +30,84 @@ export const Headinglist = (
 
 
     <>
-    <div class='pt-20 heading-container text-center text-white'>
-      <p class='uppercase text-xs pt-5'>{smallTitle}</p>
-    <h1 class="text-xl pt-5 pb-10">{mainTitle}</h1>
-    </div>
-    
+     <section class="py-20 heading-container"> 
+
+<h1 class="text-lg text-white text-center pt-10 uppercase pb-5">För dig som vill veta mer om sökmotoroptimering</h1>
+<p class="text-white text-center pb-5" >Vanliga frågor om SEO i Halmstad</p>
+
 <div id="accordion-flush" data-accordion="collapse " class='pb-10' >
-  <h2 id="accordion-flush-heading-1" class="button-no-back">
-    <button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-1" aria-expanded="false" aria-controls="accordion-flush-body-1">
-      <span class='uppercase md:pl-2 '>{title1}</span> 
-      <i class="fa-solid fa-angles-down max-md:pr-2 md:pr-3 text-rose-300"></i>
-    </button>
-  </h2>
-  <div id="accordion-flush-body-1" class="hidden" aria-labelledby="accordion-flush-heading-1">
-    <div class="py-5 border-b md:pl-2 " dangerouslySetInnerHTML={{__html: desc1}}>
-    </div>
-  </div> 
-  <h2 id="accordion-flush-heading-2" class="button-no-back">
-    <button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-2" aria-expanded="false" aria-controls="accordion-flush-body-2">
-      <span class='uppercase md:pl-2 '>{title2}</span>
-      <i class="fa-solid fa-angles-down max-md:pr-2 md:pr-3 text-rose-300"></i>
-    </button>
-  </h2>
-  <div id="accordion-flush-body-2" class="hidden" aria-labelledby="accordion-flush-heading-2">
-    <div class="py-5 border-b md:pl-2 " dangerouslySetInnerHTML={{__html: desc2}}>
-    </div>
-  </div> 
-  <h2 id="accordion-flush-heading-3" class="button-no-back">
-    <button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-3" aria-expanded="false" aria-controls="accordion-flush-body-3">
-      <span class='uppercase md:pl-2 '>{title3}</span>
-      <i class="fa-solid fa-angles-down max-md:pr-2 md:pr-3 text-rose-300"></i>
-    </button>
-  </h2>
-  <div id="accordion-flush-body-3" class="hidden" aria-labelledby="accordion-flush-heading-3">
-    <div class="py-5 border-b md:pl-2 " dangerouslySetInnerHTML={{__html: desc3}}>
-    </div>
-  </div> 
-  <h2 id="accordion-flush-heading-4" class="button-no-back">
-    <button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-4" aria-expanded="false" aria-controls="accordion-flush-body-4">
-      <span class='uppercase md:pl-2'>{title4}</span>
-      <i class="fa-solid fa-angles-down max-md:pr-2 md:pr-3 text-rose-300"></i>
-    </button>
-  </h2>
-  <div id="accordion-flush-body-4" class="hidden" aria-labelledby="accordion-flush-heading-4">
-    <div class="py-5 border-b text-white md:pl-2" dangerouslySetInnerHTML={{__html: desc4}}>
-      
-    </div>
-  </div> 
-  <h2 id="accordion-flush-heading-5" class="button-no-back">
-    <button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-5" aria-expanded="false" aria-controls="accordion-flush-body-5">
-      <span class='uppercase md:pl-2'>{title5}</span>
-      <i class="fa-solid fa-angles-down max-md:pr-2 md:pr-3 text-rose-300"></i>
-    </button>
-  </h2>
-  <div id="accordion-flush-body-5 md:pl-2" class="hidden" aria-labelledby="accordion-flush-heading-5">
-    <div class="py-5 border-b md:pl-2" dangerouslySetInnerHTML={{__html: desc5}}>
-      
-    </div>
-  </div> 
-  <h2 id="accordion-flush-heading-6" class="button-no-back">
-    <button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-6" aria-expanded="false" aria-controls="accordion-flush-body-6">
-      <span class='uppercase md:pl-2 '>{title6}</span>
-      <i class="fa-solid fa-angles-down max-md:pr-2 md:pr-3 text-rose-300"></i>
-    </button>
-  </h2>
-  <div id="accordion-flush-body-6" class="hidden" aria-labelledby="accordion-flush-heading-6">
-    <div class="py-5 border-b md:pl-2" dangerouslySetInnerHTML={{__html: desc6}}>
-      
-    </div>
-  </div> 
-  <h2 id="accordion-flush-heading-7" class="button-no-back">
-    <button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-7" aria-expanded="false" aria-controls="accordion-flush-body-7">
-      <span class='uppercase md:pl-2'>{title7}</span>
-      <i class="fa-solid fa-angles-down max-md:pr-2 md:pr-3 text-rose-300"></i>
-    </button>
-  </h2>
-  <div id="accordion-flush-body-7" class="hidden" aria-labelledby="accordion-flush-heading-7">
-    <div class="py-5 border-b md:pl-2" dangerouslySetInnerHTML={{__html: desc7}}>
-     
-    </div>
-  </div> 
-  <h2 id="accordion-flush-heading-8" class="button-no-back">
-    <button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-8" aria-expanded="false" aria-controls="accordion-flush-body-8">
-      <span class='uppercase md:pl-2'>{title8}</span>
-      <i class="fa-solid fa-angles-down max-md:pr-2 md:pr-3 text-rose-300"></i>
-    </button>
-  </h2>
-  <div id="accordion-flush-body-8" class="hidden" aria-labelledby="accordion-flush-heading-8">
-    <div class="py-5 border-b md:pl-2" dangerouslySetInnerHTML={{__html: desc8}}>
-    
-    </div>
-  </div> 
-  <h2 id="accordion-flush-heading-9" class="button-no-back">
-    <button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-9" aria-expanded="false" aria-controls="accordion-flush-body-9">
-      <span class='uppercase md:pl-2 '>{title9}</span>
-      <i class="fa-solid fa-angles-down max-md:pr-2 md:pr-3 text-rose-300"></i>
-    </button>
-  </h2>
-  <div id="accordion-flush-body-9" class="hidden" aria-labelledby="accordion-flush-heading-9">
-    <div class="py-5 border-b md:pl-2" dangerouslySetInnerHTML={{__html: desc9}}>
-     
-    </div>
-  </div> 
+<h2 id="accordion-flush-heading-1" class="button-no-back">
+<button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-1" aria-expanded="false" aria-controls="accordion-flush-body-1">
+  <span class='uppercase'>Vad är lokal SEO- Halmstad?</span>
+  <i class="fa-solid fa-angles-down pr-2 text-rose-300"></i>
+</button>
+</h2>
+<div id="accordion-flush-body-1" class="hidden" aria-labelledby="accordion-flush-heading-1">
+<div class="py-5 border-b">
+  <p class="mb-2 text-sm tracking-wider  text-white">
+  Sökmotoroptimering (SEO) står för Search Engine Optimization och handlar om hur man optimerar en webbplats för att den ska rankas så högt som möjligt i sökresultatet. Om din webbplats är SEO-optimerad för Halmstad kommer fler lokala kunder att ha större chans att hitta till din hemsida.
+  </p> 
 </div>
+</div> 
+<h2 id="accordion-flush-heading-2" class="button-no-back">
+<button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-2" aria-expanded="false" aria-controls="accordion-flush-body-2">
+  <span class='uppercase'>Lokal SEO analys- Halmstad?</span>
+  <i class="fa-solid fa-angles-down pr-2 text-rose-300"></i>
+</button>
+</h2>
+<div id="accordion-flush-body-2" class="hidden" aria-labelledby="accordion-flush-heading-2">
+<div class="py-5 border-b">
+  <p class="mb-2 text-sm tracking-wider  text-white">
+  När man gör en analys som är lokal är det bra att fokusera på vad användarens intentioner med sökningen är. Om du har en firma som utför service av värmepumpar i Halmstad, vill man inte synas på sökresultat för människor som exempelvis bor i Stockholm. Man behöver inrikta sig till en lokal kundgrupp.
+  </p>
+</div>
+</div> 
+<h2 id="accordion-flush-heading-3" class="button-no-back">
+<button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-3" aria-expanded="false" aria-controls="accordion-flush-body-3">
+  <span class='uppercase'>Hur gör jag för att synas i det lokala sökresultatet i Halmstad?</span>
+  <i class="fa-solid fa-angles-down pr-2 text-rose-300"></i>
+</button>
+</h2>
+<div id="accordion-flush-body-3" class="hidden" aria-labelledby="accordion-flush-heading-3">
+<div class="py-5 border-b">
+  <p class="mb-2 text-sm tracking-wider  text-white">
+  För att synas i lokala sökresultat behöver du inrikta dig för ett specifikt lokalt område. Om du har en firma som utför service av värmepumpar i Halmstad gör man det med sökord som exempelvis ”service värmepump Halmstad”.
+  </p>
+</div>
+</div> 
+<h2 id="accordion-flush-heading-4" class="button-no-back">
+<button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-4" aria-expanded="false" aria-controls="accordion-flush-body-4">
+  <span class='uppercase'>Vad påverkar min rankning i Halmstad?</span>
+  <i class="fa-solid fa-angles-down pr-2 text-rose-300"></i>
+</button>
+</h2>
+<div id="accordion-flush-body-4" class="hidden" aria-labelledby="accordion-flush-heading-4">
+<div class="py-5 border-b text-white">
+  <p class="mb-2 text-sm tracking-wider  text-white">
+  Har du för ”breda” sökord som syns över hela Sverige istället för mer lokala sökord rankas du lägre för just din stad. Dina sökord behöver vara mer lokalt anpassade och då rankas man högre i sin stad.
+  </p>
+
+</div>
+</div> 
+<h2 id="accordion-flush-heading-5" class="button-no-back">
+<button type="button" class="flex button-no-back items-center justify-between w-full py-5 font-medium text-left text-white" data-accordion-target="#accordion-flush-body-5" aria-expanded="false" aria-controls="accordion-flush-body-5">
+  <span class='uppercase'>Vad bör man tänka på inför en lokal SEO analys i Halmstad?</span>
+  <i class="fa-solid fa-angles-down pr-2 text-rose-300"></i>
+</button>
+</h2>
+<div id="accordion-flush-body-5" class="hidden" aria-labelledby="accordion-flush-heading-5">
+<div class="py-5 border-b">
+  <p class="mb-2 tracking-wider text-sm text-white">
+  När man vill synas på lokala sökresultat är det bra att på förhand fundera över vilka geografiska områden man vill synas på och om ni har några fördelar jämfört med era lokala konkurrenter. Ska man synas lokalt behöver man inrikta sina sökord. Om du har en frisörsalong i Halmstad, vill du inte synas på sökresultat för personer i Umeå. Välj då att inrikta dig på sökord som innehar den ort du vill synas i.
+  </p>
+</div>
+</div> 
+
+
+</div>
+
+</section>
+
   </>
   )
 }
